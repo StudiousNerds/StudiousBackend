ccac9ce57a1ff0142c6ebf16245368026fb4f1bc
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-23 16:07:32 +0900

    feat: 평점 엔티티 구현
    평점 엔티티 개발
    
    Related to: #2, #3, #4

---
+++ src/main/java/nerds/studiousTestProject/review/entity/Grade.java
@@ -0,0 +1,33 @@
+package nerds.studiousTestProject.review.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Grade {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Integer cleanliness;
+
+    private Integer deafening;
+
+    @Column(name = "fixtures_status")
+    private Integer fixturesStatus;
+
+    @Column(name = "is_recommended")
+    private Boolean isRecommended;
+
+    private Double total;
+}
