6e377ea290bcac67c2bb22c1a0b7aacb512210cd
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-25 03:41:41 +0900

    feat: 사진 레포지토리 구현
    1. 사진 레포지토리 생성
    2. 리뷰id를 통해 사진 정보를 가져오는 메소드 개발
    
    Related to: #2, #3, #4

---
+++ src/main/java/nerds/studiousTestProject/photo/repository/SubPhotoRepository.java
@@ -0,0 +1,10 @@
+package nerds.studiousTestProject.photo.repository;
+
+import nerds.studiousTestProject.photo.entity.SubPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SubPhotoRepository extends JpaRepository<SubPhoto, Long> {
+    List<SubPhoto> findAllByReviewId(Long reviewId);
+}
