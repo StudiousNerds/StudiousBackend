7d2de8d38504373e1b50f34c1007610ad5241f66
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-25 03:35:54 +0900

    feat: 룸 편의시설을 찾는 레포지토리 메소드 구현
    스터디룸의 모든 편의시설을 List로 반환하는 메소드 개발
    
    Related to: #2, #3, #4

--- src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepository.java
+++ src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepository.java
@@ -13,4 +13,10 @@ public interface ConvenienceRepository extends JpaRepository<Convenience, Long> 
             "where c.studycafe = :studycafeId " +
             "and c.room is null", nativeQuery = true)
     List<String> findByStudycafeId(@Param("studycafeId") Long studycafeId);
+
+    @Query(value = "select c.name " +
+            "from Convenience c " +
+            "where c.studycafe = :studycafeId " +
+            "and c.room = :roomId", nativeQuery = true)
+    List<String> findByStudycafeIdAndRoomId(@Param("studycafeId")Long studycafeId, @Param("roomId")Long roomId);
 }
