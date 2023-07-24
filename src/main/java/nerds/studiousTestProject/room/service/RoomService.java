b836a1d8bfb220a20611e8dddb8825e0b93b33ad
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-25 04:32:09 +0900

    feat: 날짜별 예약 가능한 시간 조회 기능 구현
    해당 날짜의 달(한 달)의 날짜마다 예약 가능한 시간을 배열에 담아서 반환하는 메소드 개발
    
    Related to: #2, #6

--- src/main/java/nerds/studiousTestProject/room/service/RoomService.java
+++ src/main/java/nerds/studiousTestProject/room/service/RoomService.java
@@ -1,6 +1,5 @@
 package nerds.studiousTestProject.room.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nerds.studiousTestProject.convenience.service.ConvenienceService;
@@ -9,17 +8,11 @@ import nerds.studiousTestProject.reservation.service.ReservationService;
 import nerds.studiousTestProject.room.dto.FindRoomResponse;
 import nerds.studiousTestProject.room.entity.Room;
 import nerds.studiousTestProject.room.repository.RoomRepository;
-import nerds.studiousTestProject.studycafe.service.StudycafeService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -40,18 +33,18 @@ public class RoomService {
 
         for (Room room : roomList) {
             rooms.add(FindRoomResponse.builder()
-                            .id(room.getId())
-                            .name(room.getName())
-                            .standCount(room.getStandCount())
-                            .minCount(room.getMinCount())
-                            .maxCount(room.getMaxCount())
-                            .price(room.getPrice())
-                            .type(room.getType())
-                            .minUsingTime(room.getMinUsingTime())
-                            .photos(subPhotoService.findRoomPhotos(room.getId()))
-                            .canReserveDatetime()
-                            .conveniences(convenienceService.getAllRoomConveniences(studycafeId, room.getId()))
-                            .build());
+                    .id(room.getId())
+                    .name(room.getName())
+                    .standCount(room.getStandCount())
+                    .minCount(room.getMinCount())
+                    .maxCount(room.getMaxCount())
+                    .price(room.getPrice())
+                    .type(room.getType())
+                    .minUsingTime(room.getMinUsingTime())
+                    .photos(subPhotoService.findRoomPhotos(room.getId()))
+                    .canReserveDatetime(getCanReserveDatetime(date, studycafeId, room.getId()))
+                    .conveniences(convenienceService.getAllRoomConveniences(studycafeId, room.getId()))
+                    .build());
         }
 
         return rooms;
@@ -71,5 +64,19 @@ public class RoomService {
         }
 
         return timeList;
+    }
+
+    public Map<String, Integer[]> getCanReserveDatetime(LocalDate date,Long studycafeId, Long roomId){
+        Integer oneMonth = date.lengthOfMonth();
+        LocalDate firstDayOfMonth = date.withDayOfMonth(1);
+        Map<String, Integer[]> reservationList = new ConcurrentHashMap<>();
+
+        for (int i = 0; i < oneMonth; i++){
+            Integer[] canReserveTime = getCanReserveTime(firstDayOfMonth, studycafeId, roomId);
+            reservationList.put(firstDayOfMonth.toString(), canReserveTime);
+            firstDayOfMonth.plusDays(1);
+        }
+
+        return reservationList;
     }
 }
