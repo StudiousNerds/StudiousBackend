219ffe9a7cb4d499fdbadcf3dcea2a75c26fb427
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-25 23:02:52 +0900

    rename: 메인페이지 스터디카페 조회 DTO 이름 변경
    1. 메인페이지의 스터디카페의 정보들을 담아서 반환하는 폼이 동일하여 이름을 좀 더 포용적인 것으로 바꿔야한다고 생각함
    2. 메인페이지의 스터디카페들을 반환하는 궁극적인 목적은 동일하여 이름을 `MainPageResponse` 로 변경
    
    Related to: #4

--- src/main/java/nerds/studiousTestProject/studycafe/dto/MainPageResponse.java
+++ src/main/java/nerds/studiousTestProject/studycafe/dto/MainPageResponse.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @Builder
-public class FindRecommendStudycafeResponse {
+public class MainPageResponse {
     private String cafeName;
     private String photo;
     private Integer accumRevCnt;
