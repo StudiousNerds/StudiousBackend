2c67101da854af0fcc4950a8b7fc8b74c9caa924
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-25 14:56:28 +0900

    feat: Room ENTITY 추가
    룸 entity 추가 입니다.
    
    Related to: #13

---
+++ src/main/java/nerds/studiousTestProject/room/entity/Room.java
@@ -0,0 +1,29 @@
+package nerds.studiousTestProject.room.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private int minHeadCount;
+    private int maxHeadCount;
+
+    private int price;
+    private String type;
+
+    private int minUsingTime;
+
+    @ManyToOne
+    @JoinColumn(name = "studycafe_id")
+    private Studycafe studycafe;
+}
