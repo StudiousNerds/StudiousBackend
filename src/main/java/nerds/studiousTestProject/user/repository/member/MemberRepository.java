c7e7dc2ac7f4242d7907a03ec4589b92badc5218
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-26 01:19:36 +0900

    feat: 북마크 조회 메소드 개발
    북마크 리스트가 비어있지 않은 멤버를 찾는 레포지토리 메소드 개발
    
    Related to: #8

--- src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
+++ src/main/java/nerds/studiousTestProject/user/repository/member/MemberRepository.java
@@ -3,6 +3,7 @@ package nerds.studiousTestProject.user.repository.member;
 import nerds.studiousTestProject.user.entity.member.Member;
 import nerds.studiousTestProject.user.entity.member.MemberType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +16,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByProviderIdAndType(Long providerId, MemberType type);
     boolean existsByEmailAndType(String email, MemberType type);
     boolean existsByPhoneNumber(String phoneNumber);
+    @Query(value = "select m from Member m where m.id = :id and m.bookmark is not empty", nativeQuery = true)
+    Optional<Member> findById(Long id);
 }
