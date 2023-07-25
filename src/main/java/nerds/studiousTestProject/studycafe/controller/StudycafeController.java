f2efb4c5018ab4519624b63347ec9994cefc2d94
kmw2378 <rlarla677@gmail.com>
2023-07-26 01:27:29 +0900

    feat: Controller 레이어 메소드에 페이징 관련 파라미터 추가

--- src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
+++ src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
@@ -9,6 +9,7 @@ import nerds.studiousTestProject.studycafe.dto.SearchResponse;
 import nerds.studiousTestProject.studycafe.dto.SortType;
 import nerds.studiousTestProject.studycafe.entity.hashtag.HashtagName;
 import nerds.studiousTestProject.studycafe.service.StudyCafeService;
+import nerds.studiousTestProject.studycafe.util.PageRequestConverter;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,21 +28,22 @@ public class StudycafeController {
     private final StudyCafeService studyCafeService;
 
     @GetMapping("/search")
-    public List<SearchResponse> search(@RequestParam Integer page,
-                                       @RequestParam String keyword,
-                                       @RequestParam @Nullable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date,
-                                       @RequestParam @Nullable Time startTime,
-                                       @RequestParam @Nullable Time endTime,
-                                       @RequestParam @Nullable Integer headCount,
-                                       @RequestParam @Nullable Integer minGrade,          // 최소 평점
-                                       @RequestParam @Nullable Integer maxGrade,          // 최대 평점
-                                       @RequestParam @Nullable Boolean eventInProgress,   // 이벤트 여부
-                                       @RequestParam @Nullable List<HashtagName> hashtags,     // 해시태그
-                                       @RequestParam @Nullable List<ConvenienceName> conveniences, // 편의 시설
-                                       @RequestParam @Nullable SortType sortType            // 정렬 기준
+    public List<SearchResponse> search(
+                                        @RequestParam Integer page,
+                                        @RequestParam @Nullable SortType sortType,       // 정렬 기준
+                                        @RequestParam String keyword,
+                                        @RequestParam @Nullable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date,
+                                        @RequestParam @Nullable Time startTime,
+                                        @RequestParam @Nullable Time endTime,
+                                        @RequestParam @Nullable Integer headCount,
+                                        @RequestParam @Nullable Integer minGrade,          // 최소 평점
+                                        @RequestParam @Nullable Integer maxGrade,          // 최대 평점
+                                        @RequestParam @Nullable Boolean eventInProgress,   // 이벤트 여부
+                                        @RequestParam @Nullable List<HashtagName> hashtags,     // 해시태그
+                                        @RequestParam @Nullable List<ConvenienceName> conveniences // 편의 시설
     ) {
         return studyCafeService.inquire(SearchRequest.builder()
-                .page(page)
+                .sortType(sortType)
                 .keyword(keyword)
                 .date(date)
                 .startTime(startTime)
@@ -52,8 +54,8 @@ public class StudycafeController {
                 .eventInProgress(eventInProgress)
                 .hashtags(hashtags)
                 .conveniences(conveniences)
-                .sortType(sortType)
-                .build()
+                .build(),
+                PageRequestConverter.of(page)
         );
     }
 }
