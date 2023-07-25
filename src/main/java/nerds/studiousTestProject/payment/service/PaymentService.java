0a57d86d3a7eb415dee838dc919cb580ac8fb910
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-26 03:35:32 +0900

    feat: 토스로 결제 승인 요청 및 반환 기능 추가
    결제 성공시 토스로 결제 승인을 요청하고 반환 받는 기능 추가입니다.
    
    Related to: #21

--- src/main/java/nerds/studiousTestProject/payment/service/PaymentService.java
+++ src/main/java/nerds/studiousTestProject/payment/service/PaymentService.java
@@ -1,26 +1,59 @@
 package nerds.studiousTestProject.payment.service;
 
 import lombok.RequiredArgsConstructor;
-import nerds.studiousTestProject.payment.dto.PaymentRequest;
-import nerds.studiousTestProject.payment.dto.PaymentResponse;
+import nerds.studiousTestProject.payment.dto.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.UUID;
+
+import static nerds.studiousTestProject.payment.PaymentConstant.*;
 
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
 public class PaymentService {
 
+    private final WebClient webClient;
     public PaymentResponse createPaymentResponse(PaymentRequest paymentRequest) {
         return PaymentResponse.builder()
                 .amount(paymentRequest.getReservation().getPrice())
                 .orderId(String.valueOf(UUID.randomUUID()))
                 .orderName(paymentRequest.getUser().getName())
-                .successUrl("http://localhost:8080/studious/payment/ssuccess")
+                .successUrl("http://localhost:8080/studious/payments/success")
                 .failUrl("http://localhost:8080/studious/payments/fail")
                 .build();
+    }
+
+    @Transactional
+    public Payment confirmPay(String orderId, String paymentKey, int amount) throws IOException, InterruptedException {
+        ConfirmRequest request = ConfirmRequest.builder()
+                .amount(amount)
+                .orderId(orderId)
+                .paymentKey(paymentKey)
+                .build();
+        String encodedAuth = Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes());
+        return webClient.method(HttpMethod.POST)
+                .uri(CONFIRM_URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Basic " + encodedAuth)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(Payment.class)
+                .block();
     }
 
 }
