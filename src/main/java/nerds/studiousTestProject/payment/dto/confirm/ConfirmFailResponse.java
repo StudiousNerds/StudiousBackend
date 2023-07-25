9e36a52c262fc706c93e183e5769bc7be70ee712
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-26 04:44:18 +0900

    feat: 결제 승인 실패 응답 DTO 추가
    결제 승인 실패 응답 DTO 추가입니다.
    
    Related to: #22

---
+++ src/main/java/nerds/studiousTestProject/payment/dto/confirm/ConfirmFailResponse.java
@@ -0,0 +1,15 @@
+package nerds.studiousTestProject.payment.dto.confirm;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConfirmFailResponse {
+    private String message;
+    private int statusCode;
+}
