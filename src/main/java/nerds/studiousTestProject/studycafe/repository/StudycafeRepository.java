1536b29480c74a36f22a2ae69008af361e43c039
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-25 22:45:15 +0900

    feat: 평점 높은 순으로 10개를 가져오는 메소드 구현
    평점 높은 순으로 정렬한 10개의 스터디카페를 List로 반환하는 메소드 개발
    
    Related to: #4

--- src/main/java/nerds/studiousTestProject/studycafe/repository/StudycafeRepository.java
+++ src/main/java/nerds/studiousTestProject/studycafe/repository/StudycafeRepository.java
@@ -3,8 +3,10 @@ package nerds.studiousTestProject.studycafe.repository;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudycafeRepository extends JpaRepository<Studycafe, Long> {
     Optional<Studycafe> findById(Long id);
+    List<Studycafe> findTop10ByOrderByTotalGardeDesc();
 }
