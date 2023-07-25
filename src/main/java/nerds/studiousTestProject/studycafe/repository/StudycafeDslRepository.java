6feab96ddc011d60ccafa48fd334b9ccf7db0fad
kmw2378 <rlarla677@gmail.com>
2023-07-26 00:17:43 +0900

    refactor: Room 엔티티와의 조인을 통해 인원 수 검색을 하도록 수정 - 서브쿼리가 나가는 대신 Join 절을 통해 인원 수 검색을 수행합니다. - 인원 수 값이 null인 경우 Join이 되지 않도록 구현했습니다.

--- src/main/java/nerds/studiousTestProject/studycafe/repository/StudycafeDslRepository.java
+++ src/main/java/nerds/studiousTestProject/studycafe/repository/StudycafeDslRepository.java
@@ -1,6 +1,7 @@
 package nerds.studiousTestProject.studycafe.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import nerds.studiousTestProject.studycafe.dto.QSearchResponse;
@@ -8,11 +9,9 @@ import nerds.studiousTestProject.studycafe.dto.SearchRequest;
 import nerds.studiousTestProject.studycafe.dto.SearchResponse;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Time;
-import java.util.Date;
 import java.util.List;
 
-import static io.jsonwebtoken.lang.Strings.hasText;
+import static nerds.studiousTestProject.room.entity.QRoom.room;
 import static nerds.studiousTestProject.studycafe.entity.QStudycafe.studycafe;
 
 @Repository
@@ -21,33 +20,44 @@ public class StudycafeDslRepository {
     private final JPAQueryFactory queryFactory;
 
     public List<SearchResponse> searchAll(SearchRequest searchRequest) {
-        return queryFactory
+        JPAQuery<SearchResponse> contentQuery = queryFactory
                 .select(
                         new QSearchResponse(
                                 studycafe.name,
                                 studycafe.photo,    // 사진 (추후 수정 예정)
                                 studycafe.accumReserveCount,
-                                studycafe.duration,    // 역까지 걸리는 시간
+                                studycafe.duration,
                                 studycafe.totalGrade
                         )
                 )
-                .from(studycafe)
+                .from(studycafe);
+
+        return getJoinedContentQuery(contentQuery, searchRequest)
                 .where(
                         headCountBetween(searchRequest.getHeadCount())
                 )
+                .groupBy(studycafe.id)
                 .fetch();
     }
 
+    public JPAQuery<SearchResponse> getJoinedContentQuery(JPAQuery<SearchResponse> query, SearchRequest searchRequest) {
+        if (searchRequest.getHeadCount() != null) {
+            query = query.leftJoin(studycafe.rooms, room);
+        }
+
+        return query;
+    }
+
     private BooleanExpression headCountBetween(Integer headCount) {
-        BooleanExpression minHeadCountGoe = minHeadCountGoe(headCount);
-        return minHeadCountGoe == null ? null : minHeadCountGoe.and(maxHeadCountLoe(headCount));
+        BooleanExpression minHeadCountLoe = minHeadCountLoe(headCount);
+        return minHeadCountLoe == null ? null : minHeadCountLoe.and(maxHeadCountGoe(headCount));
     }
 
-    private BooleanExpression minHeadCountGoe(Integer headCount) {
-        return headCount != null ? studycafe.rooms.any().minHeadCount.goe(headCount) : null;
+    private BooleanExpression minHeadCountLoe(Integer headCount) {
+        return headCount != null ? room.minHeadCount.loe(headCount) : null;
     }
 
-    private BooleanExpression maxHeadCountLoe(Integer headCount) {
-        return headCount != null ? studycafe.rooms.any().maxHeadCount.loe(headCount) : null;
+    private BooleanExpression maxHeadCountGoe(Integer headCount) {
+        return headCount != null ? room.maxHeadCount.goe(headCount) : null;
     }
 }
