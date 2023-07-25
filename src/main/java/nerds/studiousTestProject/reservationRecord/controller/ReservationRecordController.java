4e8b830dbe60cea1d49ce0ccc261c539d5a13d93
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-26 05:46:18 +0900

    feat: 결제 취소 기능 추가
    결제 취소 기능 추가입니다.
    
    Related to: #23

---
+++ src/main/java/nerds/studiousTestProject/reservationRecord/controller/ReservationRecordController.java
@@ -0,0 +1,26 @@
+package nerds.studiousTestProject.reservationRecord.controller;
+
+import lombok.RequiredArgsConstructor;
+import nerds.studiousTestProject.payment.dto.cancel.CancelRequest;
+import nerds.studiousTestProject.payment.dto.cancel.CancelResponse;
+import nerds.studiousTestProject.payment.service.PaymentService;
+import nerds.studiousTestProject.reservationRecord.service.ReservationRecordService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/studious")
+public class ReservationRecordController {
+
+    private final PaymentService paymentService;
+    private final ReservationRecordService reservationRecordService;
+
+    @GetMapping("{reservationRecordId}/cancel")
+    public List<CancelResponse> cancelReservation(@PathVariable Long reservationRecordId,
+                                                  @RequestBody CancelRequest cancelRequest){
+        reservationRecordService.cancel(reservationRecordId);
+        return paymentService.requestCancelToToss(cancelRequest);
+    }
+}
