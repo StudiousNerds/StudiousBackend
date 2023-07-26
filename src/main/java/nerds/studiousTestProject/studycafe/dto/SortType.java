f9076c16316e43f307ed414940939e5e6cf77a7f
kmw2378 <rlarla677@gmail.com>
2023-07-27 02:51:43 +0900

    feat: 정렬 값을 저장하는 열거체 추가

---
+++ src/main/java/nerds/studiousTestProject/studycafe/dto/SortType.java
@@ -0,0 +1,10 @@
+package nerds.studiousTestProject.studycafe.dto;
+
+public enum SortType {
+    GRADE_DESC,
+    GRADE_ASC,
+    RESERVATION_DESC,
+    RESERVATION_ASC;
+//    REVIEW_DESC,
+//    REVIEW_ASC;
+}
