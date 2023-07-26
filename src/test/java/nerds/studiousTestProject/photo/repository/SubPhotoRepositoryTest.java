10d74c58c0f61180a3252d959337907292b3ebbe
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-27 00:04:16 +0900

    test: 사진 레포지토리의 findAllByReviewId 테스트
    리뷰 id 주어졌을 때 해당 리뷰의 사진을 제대로 가져오는 지 테스트
    
    Related to: #2

---
+++ src/test/java/nerds/studiousTestProject/photo/repository/SubPhotoRepositoryTest.java
@@ -0,0 +1,39 @@
+package nerds.studiousTestProject.photo.repository;
+
+import nerds.studiousTestProject.photo.entity.SubPhoto;
+import nerds.studiousTestProject.review.entity.Review;
+import nerds.studiousTestProject.review.repository.ReviewRepository;
+import nerds.studiousTestProject.room.repository.RoomRepository;
+import nerds.studiousTestProject.studycafe.repository.StudycafeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SubPhotoRepositoryTest {
+    @Autowired
+    private SubPhotoRepository subPhotoRepository;
+    @Autowired
+    private StudycafeRepository studycafeRepository;
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    void findAllByReviewId() {
+        // given
+        Review review = reviewRepository.save(Review.builder().id(1L).build());
+        SubPhoto subPhoto = subPhotoRepository.save(SubPhoto.builder().review(review).url("www.spring.com").build());
+        SubPhoto subPhoto1 = subPhotoRepository.save(SubPhoto.builder().review(review).url("www.sub.com").build());
+        // when
+        List<SubPhoto> reviewPhotoList = subPhotoRepository.findAllByReviewId(review.getId());
+        // then
+        Assertions.assertThat(reviewPhotoList).contains(subPhoto, subPhoto1);
+    }
+}
