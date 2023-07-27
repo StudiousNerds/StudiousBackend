c2191303f643ea674fc517d74e9d348a9cc3772b
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-27 12:57:54 +0900

    feat: 북마크 등록 컨트롤러 구현
    Related to: #7

--- src/main/java/nerds/studiousTestProject/user/controller/MyPageMemberController.java
+++ src/main/java/nerds/studiousTestProject/user/controller/MyPageMemberController.java
@@ -2,36 +2,47 @@ package nerds.studiousTestProject.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.user.dto.bookmark.BookmarkReuqest;
 import nerds.studiousTestProject.user.dto.general.patch.PatchNicknameRequest;
 import nerds.studiousTestProject.user.dto.general.patch.PatchPasswordRequest;
 import nerds.studiousTestProject.user.dto.general.withdraw.WithdrawRequest;
+import nerds.studiousTestProject.user.service.bookmark.BookmarkService;
 import nerds.studiousTestProject.user.service.member.MemberService;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/studious/mypage/members")
+@RequestMapping("/studious/mypage")
 public class MyPageMemberController {
     private final MemberService memberService;
+    private final BookmarkService bookmarkService;
 
-    @PatchMapping("/nickname")
+    @PatchMapping("/members/nickname")
     public void patchNickname(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @RequestBody PatchNicknameRequest patchNicknameRequest) {
         memberService.replaceNickname(accessToken, patchNicknameRequest);
     }
 
-    @PatchMapping("/password")
+    @PatchMapping("/members/password")
     public void patchPassword(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @RequestBody PatchPasswordRequest patchPasswordRequest) {
         memberService.replacePassword(accessToken, patchPasswordRequest);
     }
-    @PostMapping("/withdraw")
+    @PostMapping("/members/withdraw")
     public void withdraw(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @RequestBody WithdrawRequest withdrawRequest) {
         memberService.deactivate(accessToken, withdrawRequest);
+    }
+
+    @PostMapping("/bookmarks/{pageNumber}")
+    public ResponseEntity<?> registerBookmark(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @RequestBody BookmarkReuqest bookmarkReuqest, @PathVariable("pageNumber") Integer pageNumber){
+        return bookmarkService.registerBookmark(accessToken, bookmarkReuqest);
     }
 }
