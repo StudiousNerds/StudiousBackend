25fb86b994dae69054da9eabc741d46f0e9ebc60
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-27 22:43:40 +0900

    feat: 메인페이지 스터디카페 조회 서비스 로직 구현
    Related to: #3, #4

--- src/main/java/nerds/studiousTestProject/studycafe/service/StudycafeService.java
+++ src/main/java/nerds/studiousTestProject/studycafe/service/StudycafeService.java
@@ -8,15 +8,16 @@ import nerds.studiousTestProject.hashtag.service.HashtagService;
 import nerds.studiousTestProject.photo.service.SubPhotoService;
 import nerds.studiousTestProject.review.service.ReviewService;
 import nerds.studiousTestProject.room.service.RoomService;
+import nerds.studiousTestProject.studycafe.dto.EventCafeResponse;
+import nerds.studiousTestProject.studycafe.dto.FindStudycafeRequest;
 import nerds.studiousTestProject.studycafe.dto.MainPageResponse;
 import nerds.studiousTestProject.studycafe.dto.FindStudycafeResponse;
+import nerds.studiousTestProject.studycafe.dto.RecommendCafeResponse;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
 import nerds.studiousTestProject.studycafe.repository.StudycafeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class StudycafeService {
     private final HashtagService hashtagService;
     private final ConvenienceService convenienceService;
 
-    public FindStudycafeResponse findByDate(Long id, LocalDate date, LocalTime startTime, LocalTime endTime, Integer headCount){
+    public FindStudycafeResponse findByDate(Long id, FindStudycafeRequest findStudycafeRequest){
         Studycafe studycafe = studycafeRepository.findById(id).orElseThrow(() -> new RuntimeException("No Such Studycafe"));
 
         return FindStudycafeResponse.builder()
@@ -48,7 +49,7 @@ public class StudycafeService {
                 .notification(studycafe.getNotificationInfo())
                 .refundPolicy(studycafe.getRefundPolicyInfo())
                 .notice(getNotice(id))
-                .rooms(roomService.getRooms(date, id))
+                .rooms(roomService.getRooms(findStudycafeRequest.getDate(), id))
                 .recommendationRate(reviewService.getAvgRecommendation(id))
                 .cleanliness(reviewService.getAvgCleanliness(id))
                 .deafening(reviewService.getAvgDeafening(id))
@@ -58,22 +59,19 @@ public class StudycafeService {
                 .build();
     }
 
-    public List<MainPageResponse> getRecommendStduycafes(){
-        List<Studycafe> topTenCafeList = studycafeRepository.findTop10ByOrderByTotalGradeDesc();
-        return getMainPageCafes(topTenCafeList);
+    public MainPageResponse getMainPage(){
+        List<RecommendCafeResponse> recommendStduycafes = getRecommendStduycafes();
+        List<EventCafeResponse> eventStudycafes = getEventStudycafes();
+        return MainPageResponse.builder().recommend(recommendStduycafes).event(eventStudycafes).build();
     }
 
-    public List<MainPageResponse> getEventStudycafes(){
-        List<Studycafe> topTenCafeList = studycafeRepository.findTop10ByOrderByCreatedDateDesc();
-        return getMainPageCafes(topTenCafeList);
-    }
+    public List<RecommendCafeResponse> getRecommendStduycafes(){
+        List<Studycafe> recommend = studycafeRepository.findTop2ByOrderByTotalGradeDesc();
+        List<RecommendCafeResponse> recommendCafeList = new ArrayList<>();
 
-    private List<MainPageResponse> getMainPageCafes(List<Studycafe> topTenCafeList) {
-        List<MainPageResponse> mainPageStudycafeList = new ArrayList<>();
-
-        for (Studycafe studycafe : topTenCafeList) {
+        for (Studycafe studycafe : recommend) {
             String[] cafePhotos = subPhotoService.findCafePhotos(studycafe.getId());
-            MainPageResponse foundStudycafe = MainPageResponse.builder()
+            RecommendCafeResponse foundStudycafe = RecommendCafeResponse.builder()
                     .cafeId(studycafe.getId())
                     .cafeName(studycafe.getName())
                     .photo(cafePhotos[0])
@@ -83,9 +81,31 @@ public class StudycafeService {
                     .grade(studycafe.getTotalGrade())
                     .hashtags(hashtagService.findHashtags(studycafe.getId()))
                     .build();
-            mainPageStudycafeList.add(foundStudycafe);
+            recommendCafeList.add(foundStudycafe);
         }
-        return mainPageStudycafeList;
+        return recommendCafeList;
+
+    }
+
+    public List<EventCafeResponse> getEventStudycafes(){
+        List<Studycafe> event = studycafeRepository.findTop2ByOrderByCreatedDateDesc();
+        List<EventCafeResponse> eventCafeList = new ArrayList<>();
+
+        for (Studycafe studycafe : event) {
+            String[] cafePhotos = subPhotoService.findCafePhotos(studycafe.getId());
+            EventCafeResponse foundStudycafe = EventCafeResponse.builder()
+                    .cafeId(studycafe.getId())
+                    .cafeName(studycafe.getName())
+                    .photo(cafePhotos[0])
+                    .accumRevCnt(studycafe.getAccumReserveCount())
+                    .distance(studycafe.getDuration())
+                    .nearestStation(studycafe.getNearestStation())
+                    .grade(studycafe.getTotalGrade())
+                    .hashtags(hashtagService.findHashtags(studycafe.getId()))
+                    .build();
+            eventCafeList.add(foundStudycafe);
+        }
+        return eventCafeList;
     }
 
     public Studycafe getStudyCafe(Long studycafeId){
@@ -95,61 +115,6 @@ public class StudycafeService {
     public Studycafe getStudyCafeByName(String cafeName){
         return studycafeRepository.findByName(cafeName).orElseThrow(() -> new EntityNotFoundException("No Such Cafe"));
     }
-
-//    public List<MainPageResponse> getRecommendStudycafe(){
-//        Map<Studycafe, Double> averageList = getStudycafeAvgGrade();
-//        Map<Studycafe, Double> topTenList = getTopTenStudycafeList(averageList);
-//        List<Studycafe> topTenCafeList = getTopTenStudycafesName(topTenList);
-//        List<MainPageResponse> recommendStudycafeList = getRecommendStudycafes(topTenList, topTenCafeList);
-//        return recommendStudycafeList;
-//    }
-//
-//
-//    private Map<Studycafe, Double> getStudycafeAvgGrade() {
-//        List<Studycafe> studycafeList = studycafeRepository.findAll();
-//        Map<Studycafe, Double> averageList = new HashMap<>();
-//        for (Studycafe studyCafe : studycafeList) {
-//            averageList.put(studyCafe, studyCafe.getTotalGarde());
-//        }
-//        return averageList;
-//    }
-//
-//    private Map<Studycafe, Double> getTopTenStudycafeList(Map<Studycafe, Double> averageList) {
-//        Map<Studycafe, Double> topTenList = new HashMap<>();
-//        List<Studycafe> keySetList = new ArrayList<>(averageList.keySet());
-//        Collections.sort(keySetList, (o1, o2) -> averageList.get(o2).compareTo(averageList.get(o1)));
-//        for (Studycafe key : keySetList) {
-//            topTenList.put(key, averageList.get(key));
-//        }
-//        return topTenList;
-//    }
-//
-//    private List<Studycafe> getTopTenStudycafesName(Map<Studycafe, Double> topTenList) {
-//        List<Studycafe> topTenCafeList = new ArrayList<>();
-//        for (Studycafe studycafe : topTenList.keySet()) {
-//            topTenCafeList.add(studycafe);
-//        }
-//        return topTenCafeList;
-//    }
-//
-//    private List<MainPageResponse> getRecommendStudycafes(Map<Studycafe, Double> topTenList, List<Studycafe> topTenCafeList) {
-//        List<MainPageResponse> recommendStudycafeList = new ArrayList<>();
-//        for (int i = 0; i < 10; i++) {
-//            Studycafe studycafe = topTenCafeList.get(i);
-//            String[] cafePhotos = subPhotoService.findCafePhotos(studycafe.getId());
-//            MainPageResponse foundStudycafe = MainPageResponse.builder()
-//                    .cafeName(studycafe.getName())
-//                    .photo(cafePhotos[0])
-//                    .accumRevCnt(studycafe.getAccumReserveCount())
-//                    .distance(studycafe.getDuration())
-//                    .grade(topTenList.get(studycafe))
-//                    .hashtags(hashtagService.findHashtags(studycafe.getId()))
-//                    .build();
-//            recommendStudycafeList.add(foundStudycafe);
-//        }
-//        return recommendStudycafeList;
-//    }
-
     public String[] getNotice(Long id){
         Studycafe studycafe = studycafeRepository.findById(id).orElseThrow(() -> new RuntimeException("No Such Studycafe"));
 
@@ -159,16 +124,4 @@ public class StudycafeService {
 
         return notices;
     }
-
-//    public LocalTime getOpenTime(Long studycafeId){
-//        Studycafe studycafe = studycafeRepository.findById(studycafeId).orElseThrow(() -> new RuntimeException("No Such Studycafe"));
-//
-//        return studycafe.getStartTime();
-//    }
-//
-//    public LocalTime getEndTime(Long studycafeId){
-//        Studycafe studycafe = studycafeRepository.findById(studycafeId).orElseThrow(() -> new RuntimeException("No Such Studycafe"));
-//
-//        return studycafe.getEndTime();
-//    }
 }
