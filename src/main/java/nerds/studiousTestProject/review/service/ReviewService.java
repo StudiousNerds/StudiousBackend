4317eb58d43dd012e5ec3a852f8aa8c9133044c5
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-28 00:41:47 +0900

    refactor: 모든 리뷰를 조회하는 메소드 수정
    쿼리로 해결하려고 하니 너무 복잡해져서 서비스단에서 조금 갈무리해주고 넘겨주는 걸로 수정함
    
    Related to: #2

--- src/main/java/nerds/studiousTestProject/review/service/ReviewService.java
+++ src/main/java/nerds/studiousTestProject/review/service/ReviewService.java
@@ -3,9 +3,13 @@ package nerds.studiousTestProject.review.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nerds.studiousTestProject.photo.service.SubPhotoService;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
+import nerds.studiousTestProject.reservation.repository.ReservationRecordRepository;
 import nerds.studiousTestProject.review.dto.FindReviewResponse;
 import nerds.studiousTestProject.review.entity.Review;
 import nerds.studiousTestProject.review.repository.ReviewRepository;
+import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.room.repository.RoomRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,17 +23,26 @@ import java.util.List;
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final SubPhotoService subPhotoService;
+    private final RoomRepository roomRepository;
+    private final ReservationRecordRepository reservationRecordRepository;
 
-    private List<Review> getReviewList(Long id) {
-        List<Review> reviewList = reviewRepository.findAllById(id);
+    private List<Review> getReviewList(Long studycafeId) {
+        List<ReservationRecord> recordList = findAllReservation(studycafeId);
+        List<Review> reviewList = new ArrayList<>();
+        for (ReservationRecord r : recordList) {
+            List<Review> reviews = reviewRepository.findAllById(r.getId());
+            for (int i = 0; i < reviews.size(); i++) {
+                reviewList.add(reviews.get(i));
+            }
+        }
         return reviewList;
     }
 
-    public List<FindReviewResponse> findAllReviews(Long id){
+    public List<FindReviewResponse> findAllReviews(Long id) {
         List<Review> reviewList = getReviewList(id);
         List<FindReviewResponse> reviewResponses = new ArrayList<>();
 
-        for (Review review :reviewList) {
+        for (Review review : reviewList) {
             reviewResponses.add(FindReviewResponse.builder()
                     .grade(review.getGrade().getTotal())
                     .email(review.getReservationRecord().getMember().getNickname())
@@ -42,63 +55,77 @@ public class ReviewService {
         return reviewResponses;
     }
 
+    public List<ReservationRecord> findAllReservation(Long studycafeId){
+        List<Room> roomList = roomRepository.findAllByStudycafeId(studycafeId);
+        List<ReservationRecord> reservationRecordList = new ArrayList<>();
+        for (Room r : roomList){
+            List<ReservationRecord> reservationRecords = reservationRecordRepository.findAllByRoomId(r.getId());
+            for (int i = 0; i < reservationRecords.size(); i++) {
+                reservationRecordList.add(reservationRecords.get(i));
+            }
+        }
+        return reservationRecordList;
+    }
+
     public Integer getAvgCleanliness(Long id){
         List<Review> reviewList = getReviewList(id);
-        Integer count = 0, sum = 0, result = 0;
+        Integer count = 0, sum = 0;
 
         for (Review review : reviewList){
             sum += review.getGrade().getCleanliness();
             count++;
         }
 
-        return result = sum / count;
+        return sum / count;
     }
 
     public Integer getAvgDeafening(Long id){
         List<Review> reviewList = getReviewList(id);
-        Integer count = 0, sum = 0, result = 0;
+        Integer count = 0, sum = 0;
 
         for (Review review : reviewList){
             sum += review.getGrade().getDeafening();
             count++;
         }
 
-        return result = sum / count;
+        return sum / count;
     }
 
     public Integer getAvgFixturesStatus(Long id){
         List<Review> reviewList = getReviewList(id);
-        Integer count = 0, sum = 0, result = 0;
+        Integer count = 0, sum = 0;
 
         for (Review review : reviewList){
             sum += review.getGrade().getFixturesStatus();
             count++;
         }
 
-        return result = sum / count;
+        return sum / count;
     }
 
     public Integer getAvgRecommendation(Long id){
         List<Review> reviewList = getReviewList(id);
-        Integer recommend = 0, count = 0, result = 0;
+        Integer recommend = 0, count = 0;
 
         for (Review review : reviewList){
-            recommend += review.getGrade().getFixturesStatus();
+            if(review.getGrade().getIsRecommended()){
+                recommend++;
+            }
             count++;
         }
 
-        return result = recommend / count * 100;
+        return recommend / count * 100;
     }
 
     public Double getAvgGrade(Long id){
         List<Review> reviewList = getReviewList(id);
         Integer count = 0;
-        Double sum = 0.0, result = 0.0;
+        Double sum = 0.0;
 
         for (Review review : reviewList){
             sum += review.getGrade().getTotal();
             count++;
         }
-        return  result = sum/ count;
+        return  sum/ count;
     }
 }
