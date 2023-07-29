841c6062c805595c36d03fa26947632c56e8893b
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-29 18:20:17 +0900

    test: room fixture
    최소 인원 4, 최대 인원 6의 방 fixture

---
+++ src/test/java/nerds/studiousTestProject/RoomFixture.java
@@ -0,0 +1,51 @@
+package nerds.studiousTestProject;
+
+import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.room.entity.Room.RoomBuilder;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
+
+public enum RoomFixture {
+
+    ROOM_FOUR_SIX(4,6);
+
+    private final int minHeadCount;
+
+    private final int maxHeadCount;
+
+    RoomFixture(int minHeadCount, int maxHeadCount) {
+        this.minHeadCount = minHeadCount;
+        this.maxHeadCount = maxHeadCount;
+    }
+
+
+    public Room 생성(){
+        return 기본_정보_생성().build();
+    }
+
+    public Room 생성(Long id){
+        return 기본_정보_생성()
+                .id(id)
+                .build();
+    }
+
+    public RoomBuilder 기본_정보_생성() {
+        return Room.builder()
+                .minHeadCount(this.minHeadCount)
+                .maxHeadCount(this.maxHeadCount);
+
+    }
+
+    public Room 스터디카페_생성(Studycafe studyCafe) {
+        return 기본_정보_생성()
+                .studycafe(studyCafe)
+                .build();
+    }
+
+    public Room 스터디카페_생성(Studycafe studyCafe, Long id){
+        return 기본_정보_생성()
+                .id(id)
+                .studycafe(studyCafe)
+                .build();
+    }
+
+}
