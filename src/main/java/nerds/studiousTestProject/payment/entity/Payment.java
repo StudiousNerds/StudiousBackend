4dd8c52a8159134f4fa4834dc913201f2bf9ad91
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-29 18:29:13 +0900

    refactor: 생성자 리팩토링

--- src/main/java/nerds/studiousTestProject/payment/entity/Payment.java
+++ src/main/java/nerds/studiousTestProject/payment/entity/Payment.java
@@ -3,27 +3,31 @@ package nerds.studiousTestProject.payment.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Entity
-@AllArgsConstructor
-@Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Payment {
 
     @Id
     @GeneratedValue
     private Long id;
-
     private String orderId;
     private String paymentKey;
     private String type;
     private String completeTime;
 
+    @Builder
+    public Payment(Long id, String orderId, String paymentKey, String type, String completeTime) {
+        this.id = id;
+        this.orderId = orderId;
+        this.paymentKey = paymentKey;
+        this.type = type;
+        this.completeTime = completeTime;
+    }
 }
