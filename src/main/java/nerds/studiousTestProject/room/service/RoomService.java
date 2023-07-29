c29e58d03a6955a8c89ebf85b6256a69f71a73c6
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-29 17:47:45 +0900

    feat: findById 예외 반환 기능 추가

--- src/main/java/nerds/studiousTestProject/room/service/RoomService.java
+++ src/main/java/nerds/studiousTestProject/room/service/RoomService.java
@@ -2,6 +2,7 @@ package nerds.studiousTestProject.room.service;
 
 import lombok.RequiredArgsConstructor;
 import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.exception.RoomNotFoundException;
 import nerds.studiousTestProject.room.repository.RoomRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class RoomService {
     private final RoomRepository roomRepository;
 
-    public Room findById(Long roomId){
-        return roomRepository.findRoomById(roomId);
+    public Room findRoomById(Long roomId){
+        return roomRepository.findById(roomId)
+                .orElseThrow(RoomNotFoundException::new);
     }
 }
