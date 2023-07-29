be50741f4096d9650217e3741a524d7ea27d5988
rlfrkdms1 <rlfrkdms1@naver.com>
2023-07-29 18:19:04 +0900

    test: repository test 어노테이션

---
+++ src/test/java/nerds/studiousTestProject/RepositoryTest.java
@@ -0,0 +1,17 @@
+package nerds.studiousTestProject;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@DataJpaTest(showSql = false)
+public @interface RepositoryTest {
+
+}
