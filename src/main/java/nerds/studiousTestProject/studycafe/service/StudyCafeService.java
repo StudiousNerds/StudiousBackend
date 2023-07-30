93c71fa041c379649eceecb074586bf9887ec21d
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-30 10:45:11 +0900

    fix: 충돌 해결

--- src/main/java/nerds/studiousTestProject/studycafe/service/StudyCafeService.java
+++ src/main/java/nerds/studiousTestProject/studycafe/service/StudyCafeService.java
@@ -1,21 +1,41 @@
 package nerds.studiousTestProject.studycafe.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nerds.studiousTestProject.common.exception.BadRequestException;
 import nerds.studiousTestProject.common.exception.ErrorCode;
+import nerds.studiousTestProject.convenience.service.ConvenienceService;
+import nerds.studiousTestProject.hashtag.service.HashtagService;
+import nerds.studiousTestProject.photo.service.SubPhotoService;
+import nerds.studiousTestProject.review.service.ReviewService;
+import nerds.studiousTestProject.room.service.RoomService;
+import nerds.studiousTestProject.studycafe.dto.FindStudycafeResponse;
+import nerds.studiousTestProject.studycafe.dto.MainPageResponse;
 import nerds.studiousTestProject.studycafe.dto.SearchRequest;
 import nerds.studiousTestProject.studycafe.dto.SearchResponse;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
 import nerds.studiousTestProject.studycafe.repository.StudycafeDslRepository;
+import nerds.studiousTestProject.studycafe.repository.StudycafeRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
-@Transactional
-public class StudyCafeService {
+public class StudycafeService {
+    private final StudycafeRepository studycafeRepository;
+    private final ReviewService reviewService;
+    private final RoomService roomService;
+    private final SubPhotoService subPhotoService;
+    private final HashtagService hashtagService;
+    private final ConvenienceService convenienceService;
     private final StudycafeDslRepository studycafeDslRepository;
 
     /**
@@ -25,7 +45,6 @@ public class StudyCafeService {
      * @param pageable 페이지
      * @return 검색 결과
      */
-    @Transactional(readOnly = true)
     public List<SearchResponse> inquire(SearchRequest searchRequest, Pageable pageable) {
 
         // 날짜 선택이 안되었는데 시간을 선택한 경우
@@ -35,10 +54,83 @@ public class StudyCafeService {
 
         // 시작 시간이 끝 시간보다 이후인 경우
         if (searchRequest.getStartTime() != null && searchRequest.getEndTime() != null &&
-        !searchRequest.getStartTime().isBefore(searchRequest.getEndTime())) {
+                !searchRequest.getStartTime().isBefore(searchRequest.getEndTime())) {
             throw new BadRequestException(ErrorCode.START_TIME_AFTER_THAN_END_TIME);
         }
 
         return studycafeDslRepository.searchAll(searchRequest, pageable).getContent();
+    }
+
+    public FindStudycafeResponse findByDate(Long id, LocalDate date, LocalTime startTime, LocalTime endTime, Integer headCount){
+        Studycafe studycafe = studycafeRepository.findById(id).orElseThrow(() -> new RuntimeException("No Such Studycafe"));
+
+        return FindStudycafeResponse.builder()
+                .cafeName(studycafe.getName())
+                .photos(subPhotoService.findCafePhotos(id))
+                .accumResCnt(studycafe.getAccumReserveCount())
+                .duration(studycafe.getDuration())
+                .nearestStation(studycafe.getNearestStation())
+                .hashtags(hashtagService.findHashtags(id))
+                .introduction(studycafe.getIntroduction())
+                .conveniences(convenienceService.getAllCafeConveniences(id))
+                .notification(studycafe.getNotificationInfo())
+                .refundPolicy(studycafe.getRefundPolicyInfo())
+                .notice(getNotice(id))
+                .rooms(roomService.getRooms(date, id))
+                .recommendationRate(reviewService.getAvgRecommendation(id))
+                .cleanliness(reviewService.getAvgCleanliness(id))
+                .deafening(reviewService.getAvgDeafening(id))
+                .fixturesStatus(reviewService.getAvgFixturesStatus(id))
+                .total(studycafe.getTotalGarde())
+                .reviewInfo(reviewService.findAllReviews(studycafe.getId()))
+                .build();
+    }
+
+    public List<MainPageResponse> getRecommendStduycafes(){
+        List<Studycafe> topTenCafeList = studycafeRepository.findTop10ByOrderByTotalGardeDesc();
+        List<MainPageResponse> recommendStudycafeList = new ArrayList<>();
+
+        for (Studycafe studycafe : topTenCafeList) {
+            String[] cafePhotos = subPhotoService.findCafePhotos(studycafe.getId());
+            MainPageResponse foundStudycafe = MainPageResponse.builder()
+                    .cafeName(studycafe.getName())
+                    .photo(cafePhotos[0])
+                    .accumRevCnt(studycafe.getAccumReserveCount())
+                    .distance(studycafe.getDuration())
+                    .grade(studycafe.getTotalGarde())
+                    .hashtags(hashtagService.findHashtags(studycafe.getId()))
+                    .build();
+            recommendStudycafeList.add(foundStudycafe);
+        }
+        return recommendStudycafeList;
+    }
+
+    public List<MainPageResponse> getEventStudycafes(){
+        List<Studycafe> topTenCafeList = studycafeRepository.findTop10ByOrderByCreatedDateDesc();
+        List<MainPageResponse> recommendStudycafeList = new ArrayList<>();
+
+        for (Studycafe studycafe : topTenCafeList) {
+            String[] cafePhotos = subPhotoService.findCafePhotos(studycafe.getId());
+            MainPageResponse foundStudycafe = MainPageResponse.builder()
+                    .cafeName(studycafe.getName())
+                    .photo(cafePhotos[0])
+                    .accumRevCnt(studycafe.getAccumReserveCount())
+                    .distance(studycafe.getDuration())
+                    .grade(studycafe.getTotalGarde())
+                    .hashtags(hashtagService.findHashtags(studycafe.getId()))
+                    .build();
+            recommendStudycafeList.add(foundStudycafe);
+        }
+        return recommendStudycafeList;
+    }
+
+    public String[] getNotice(Long id){
+        Studycafe studycafe = studycafeRepository.findById(id).orElseThrow(() -> new RuntimeException("No Such Studycafe"));
+
+        List<String> noticeList = studycafe.getNotice();
+        Integer arrSize = noticeList.size();
+        String notices[] = noticeList.toArray(new String[arrSize]);
+
+        return notices;
     }
 }
