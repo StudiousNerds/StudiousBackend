606789ff1c1c02eb233a1aca1d95d02e3d908543
kmw2378 <rlarla677@gmail.com>
2023-08-02 01:15:47 +0900

    feat: 계좌 정보, 사업자 정보 검증 및 스터디카페 등록 관련 URI 허용 조건 추가

--- src/main/java/nerds/studiousTestProject/member/config/SecurityConfig.java
+++ src/main/java/nerds/studiousTestProject/member/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 /**
  * Spring Security 관련 설정 사항들
@@ -40,7 +41,9 @@ public class SecurityConfig {
                 .requestMatchers("/studious/studycafes/{cafeId}", "/studious/main").permitAll()
                 .requestMatchers("/studious/members/logout", "/studious/members/reissue").hasAnyRole("USER", "ADMIN", "SUPER_ADMIN")  // 로그아웃, 토큰 재발급
                 .requestMatchers("/studious/mypage/**").hasAnyRole("USER", "ADMIN", "SUPER_ADMIN") // 닉네임, 비밀번호 수정 및 회원 탈퇴
-                .requestMatchers("studious/search/**").permitAll()
+                .requestMatchers("/studious/search/**").permitAll()
+                .requestMatchers("/studious/valid/**").permitAll()   // 임시 허용
+                .requestMatchers("/studious/studycafes/registrations").permitAll()   // 임시 허용
                 .requestMatchers(HttpMethod.POST, "/studious/members/email").permitAll()      // 이메일 찾기
                 .requestMatchers(HttpMethod.POST, "/studious/members/password").permitAll()  // 비밀번호 찾기
                 .requestMatchers("/studious/members/test").hasRole("USER")      // 테스트 용
@@ -70,8 +73,19 @@ public class SecurityConfig {
     }
 
     @Bean
+    public DefaultUriBuilderFactory defaultUriBuilderFactory() {
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+        return factory;
+    }
+
+    /**
+     * 인증 키를 포함 한 URL을 입력 받을 때 인코딩 관련 오류가 발생하므로, WebClient 에서 URL 인코딩을 하지 않도록 하기 위해 DefaultUriBuilderFactory 객체를 추가
+     * @return URL 인코딩이 되지 않는 WebClient
+     */
+    @Bean
     public WebClient webClient() {
-        return WebClient.create();
+        return WebClient.builder().uriBuilderFactory(defaultUriBuilderFactory()).build();
     }
 
     @Bean
