73ec4808d6868c55d6ec8bd489868b53d118533c
kmw2378 <rlarla677@gmail.com>
2023-08-02 01:11:53 +0900

    feat: 계좌 정보, 사업자 정보 검증, 스터디카페 등록 URI 매핑 컨트롤러 추가

--- src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
+++ src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
@@ -9,9 +9,14 @@ import nerds.studiousTestProject.hashtag.entity.HashtagName;
 import nerds.studiousTestProject.studycafe.dto.FindStudycafeRequest;
 import nerds.studiousTestProject.studycafe.dto.FindStudycafeResponse;
 import nerds.studiousTestProject.studycafe.dto.MainPageResponse;
-import nerds.studiousTestProject.studycafe.dto.SearchRequest;
-import nerds.studiousTestProject.studycafe.dto.SearchResponse;
-import nerds.studiousTestProject.studycafe.dto.SortType;
+import nerds.studiousTestProject.studycafe.dto.register.RegisterRequest;
+import nerds.studiousTestProject.studycafe.dto.register.RegisterResponse;
+import nerds.studiousTestProject.studycafe.dto.search.SearchRequest;
+import nerds.studiousTestProject.studycafe.dto.search.SearchResponse;
+import nerds.studiousTestProject.studycafe.dto.search.SortType;
+import nerds.studiousTestProject.studycafe.dto.valid.AccountInfoRequest;
+import nerds.studiousTestProject.studycafe.dto.valid.BusinessInfoRequest;
+import nerds.studiousTestProject.studycafe.dto.valid.ValidResponse;
 import nerds.studiousTestProject.studycafe.service.StudycafeService;
 import nerds.studiousTestProject.studycafe.util.PageRequestConverter;
 import org.hibernate.validator.constraints.Length;
@@ -20,6 +25,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +48,7 @@ public class StudycafeController {
             @RequestParam Integer page,
             @RequestParam(required = false) SortType sortType,       // 정렬 기준
             @RequestParam @Length(min = 2) String keyword,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") @FutureOrPresent LocalDate date,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @FutureOrPresent LocalDate date,
             @RequestParam(required = false) LocalTime startTime,
             @RequestParam(required = false) LocalTime endTime,
             @RequestParam(required = false) @Positive Integer headCount,
@@ -76,5 +82,22 @@ public class StudycafeController {
     @GetMapping("/main")
     public MainPageResponse mainpage() {
         return studycafeService.getMainPage();
+    }
+
+    @PostMapping("/valid/accountInfo")
+    public ValidResponse checkAccountInfo(@RequestBody AccountInfoRequest accountInfoRequest) {
+        return studycafeService.validateAccountInfo(accountInfoRequest);
+    }
+
+    @PostMapping("/valid/businessInfo")
+    public ValidResponse checkBusinessInfo(@RequestBody BusinessInfoRequest businessInfoRequest) {
+        log.info("business = {}", businessInfoRequest);
+        return studycafeService.validateBusinessInfo(businessInfoRequest);
+    }
+
+    @PostMapping("/studycafes/registrations")
+    public RegisterResponse registerStudycafe(@RequestBody RegisterRequest registerRequest) {
+        log.info("request = {}", registerRequest);
+        return studycafeService.register(registerRequest);
     }
 }
