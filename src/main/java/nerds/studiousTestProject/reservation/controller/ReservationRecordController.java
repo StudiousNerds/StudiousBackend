06b758e0b4a96a2bcbdc80b0e7cbbcf08f3e2e5c
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-04 21:10:49 +0900

    refactor: 예약 페이지 조회 기능 추가
    예약 하기를 눌렀을 때 나오는 페이지의 정보를 반환합니다.
    
    Related to: #34

--- src/main/java/nerds/studiousTestProject/reservation/controller/ReservationRecordController.java
+++ src/main/java/nerds/studiousTestProject/reservation/controller/ReservationRecordController.java
@@ -1,10 +1,18 @@
 package nerds.studiousTestProject.reservation.controller;
 
 import lombok.RequiredArgsConstructor;
-import nerds.studiousTestProject.payment.dto.cancel.CancelRequest;
-import nerds.studiousTestProject.payment.dto.cancel.CancelResponse;
+import nerds.studiousTestProject.payment.util.totoss.CancelRequest;
+import nerds.studiousTestProject.payment.dto.cancel.response.CancelResponse;
 import nerds.studiousTestProject.payment.service.PaymentService;
-import org.springframework.web.bind.annotation.*;
+import nerds.studiousTestProject.reservation.dto.reserve.response.ReserveResponse;
+import nerds.studiousTestProject.reservation.service.ReservationRecordService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -14,10 +22,17 @@ import java.util.List;
 public class ReservationRecordController {
 
     private final PaymentService paymentService;
+    private final ReservationRecordService reservationRecordService;
 
     @GetMapping("{reservationRecordId}/cancel")
     public List<CancelResponse> cancelReservation(@PathVariable Long reservationRecordId,
                                                   @RequestBody CancelRequest cancelRequest){
         return paymentService.cancel(cancelRequest, reservationRecordId);
     }
+
+    @GetMapping("/reservations/studycafes/{cafeId}/rooms/{roomId}")
+    public ReserveResponse reserve(@PathVariable Long cafeId, @PathVariable Long roomId, @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
+        return reservationRecordService.reserve(cafeId, roomId, accessToken);
+    }
+
 }
