9d28a9af542b9909e7b936d1e4ec4d584700088b
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-04 22:38:53 +0900

    feat: 추후 환불 계산을 위한 int 형 남은 일수 추가
    확장성을 고려해 int형 추가했습니다.
    
    Related to: #34

--- src/main/java/nerds/studiousTestProject/refundpolicy/entity/RefundDay.java
+++ src/main/java/nerds/studiousTestProject/refundpolicy/entity/RefundDay.java
@@ -2,20 +2,22 @@ package nerds.studiousTestProject.refundpolicy.entity;
 
 public enum RefundDay {
 
-    BEFORE8("이용 8일 전"),
-    BEFORE7("이용 7일 전"),
-    BEFORE6("이용 6일 전"),
-    BEFORE5("이용 5일 전"),
-    BEFORE4("이용 4일 전"),
-    BEFORE3("이용 3일 전"),
-    BEFORE2("이용 2일 전"),
-    BEFORE1("이용 1일 전"),
-    NOW("이용 당일");
+    BEFORE8("이용 8일 전", 8),
+    BEFORE7("이용 7일 전", 7),
+    BEFORE6("이용 6일 전", 6),
+    BEFORE5("이용 5일 전", 5),
+    BEFORE4("이용 4일 전", 4),
+    BEFORE3("이용 3일 전", 3),
+    BEFORE2("이용 2일 전", 2),
+    BEFORE1("이용 1일 전", 1),
+    NOW("이용 당일", 0);
 
     private final String message;
+    private final int remain;
 
-    RefundDay(String message) {
+    RefundDay(String message, int remain) {
         this.message = message;
+        this.remain = remain;
     }
 
     public String getMessage() {
