408722299caea8231554495a48a2e6928ad019ee
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-05 23:49:09 +0900

    feat: 리뷰 등록 메소드 구현

--- src/main/java/nerds/studiousTestProject/review/service/ReviewService.java
+++ src/main/java/nerds/studiousTestProject/review/service/ReviewService.java
@@ -2,27 +2,40 @@ package nerds.studiousTestProject.review.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.common.exception.ErrorCode;
+import nerds.studiousTestProject.common.exception.NotFoundException;
+import nerds.studiousTestProject.hashtag.entity.HashtagName;
+import nerds.studiousTestProject.hashtag.entity.HashtagRecord;
+import nerds.studiousTestProject.photo.entity.SubPhoto;
 import nerds.studiousTestProject.photo.service.SubPhotoService;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import nerds.studiousTestProject.reservation.repository.ReservationRecordRepository;
-import nerds.studiousTestProject.review.dto.FindReviewResponse;
+import nerds.studiousTestProject.review.dto.request.RegisterReviewRequest;
+import nerds.studiousTestProject.review.dto.response.FindReviewResponse;
+import nerds.studiousTestProject.review.dto.response.RegisterReviewResponse;
+import nerds.studiousTestProject.review.entity.Grade;
 import nerds.studiousTestProject.review.entity.Review;
 import nerds.studiousTestProject.review.repository.ReviewRepository;
 import nerds.studiousTestProject.room.entity.Room;
 import nerds.studiousTestProject.room.repository.RoomRepository;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
+import nerds.studiousTestProject.studycafe.repository.StudycafeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Slf4j
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final SubPhotoService subPhotoService;
+    private final StudycafeRepository studycafeRepository;
     private final RoomRepository roomRepository;
     private final ReservationRecordRepository reservationRecordRepository;
 
@@ -36,6 +49,45 @@ public class ReviewService {
             }
         }
         return reviewList;
+    }
+
+    @Transactional
+    public RegisterReviewResponse registerReview(RegisterReviewRequest registerReviewRequest){
+        Grade grade = Grade.builder().cleanliness(registerReviewRequest.getCleanliness())
+                .deafening(registerReviewRequest.getDeafening())
+                .fixturesStatus(registerReviewRequest.getFixtureStatus())
+                .isRecommended(registerReviewRequest.getIsRecommend())
+                .build();
+        grade.addTotal(getTotal(grade.getCleanliness(), grade.getDeafening(), grade.getFixturesStatus()));
+
+        Review review = Review.builder()
+                .grade(grade)
+                .createdDate(LocalDate.now())
+                .detail(registerReviewRequest.getDetail())
+                .build();
+        reviewRepository.save(review);
+
+        List<String> photos = Arrays.stream(registerReviewRequest.getPhotos()).toList();
+        for (String photo : photos) {
+            SubPhoto subPhoto = SubPhoto.builder().review(review).url(photo).build();
+            subPhotoService.savePhoto(subPhoto);
+        }
+
+        List<String> hashtags = Arrays.stream(registerReviewRequest.getHashtags()).toList();
+        Studycafe studycafe = studycafeRepository.findById(registerReviewRequest.getCafeId())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_STUDYCAFE));
+        for (String hashtag : hashtags) {
+            HashtagRecord hashtagRecord = HashtagRecord.builder()
+                    .name(HashtagName.valueOf(hashtag))
+                    .build();
+            hashtagRecord.addCount(1);
+            studycafe.addHashtagRecord(hashtagRecord);
+        }
+
+        Double avgGrade = getAvgGrade(studycafe.getId());
+        studycafe.addTotalGrade(avgGrade);
+
+        return RegisterReviewResponse.builder().reviewId(review.getId()).createdAt(LocalDate.now()).build();
     }
 
     public List<FindReviewResponse> findAllReviews(Long id) {
@@ -115,6 +167,10 @@ public class ReviewService {
         }
 
         return recommend / count * 100;
+    }
+
+    public Double getTotal(Integer cleanliness, Integer deafening, Integer fixtureStatus) {
+        return (cleanliness + deafening + fixtureStatus) / 3.0;
     }
 
     public Double getAvgGrade(Long id) {
