d89328cf45c398158b35efc3b817da67f35a4809
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-05 20:59:35 +0900

    test: 카페 시작 시간을 가져오는 메소드 테스트

---
+++ src/test/java/nerds/studiousTestProject/studycafe/repository/OperationInfoRepositoryTest.java
@@ -0,0 +1,33 @@
+package nerds.studiousTestProject.studycafe.repository;
+
+import nerds.studiousTestProject.studycafe.entity.OperationInfo;
+import nerds.studiousTestProject.studycafe.entity.Week;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class OperationInfoRepositoryTest {
+
+    @Autowired
+    private OperationInfoRepository operationInfoRepository;
+
+    @Test
+    void findStartTime() {
+        // given
+        OperationInfo operation = OperationInfo.builder().startTime(LocalTime.of(8,0,0)).week(Week.SATURDAY).build();
+        operationInfoRepository.save(operation);
+        // when
+        LocalTime startTime = operationInfoRepository.findStartTime(Week.of(LocalDate.now()));
+        // then
+        Assertions.assertThat(startTime.getHour()).isEqualTo(8);
+    }
+}
