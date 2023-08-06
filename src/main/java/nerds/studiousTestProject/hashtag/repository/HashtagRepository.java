8e3641a878f95fc3d5919ee2ba7f2dd6c6cc2c8a
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-06 20:40:14 +0900

    feat: 카페id와 해시태그이름으로 해시태그 기록을 찾는 메소드 구현

--- src/main/java/nerds/studiousTestProject/hashtag/repository/HashtagRepository.java
+++ src/main/java/nerds/studiousTestProject/hashtag/repository/HashtagRepository.java
@@ -1,7 +1,9 @@
 package nerds.studiousTestProject.hashtag.repository;
 
+import nerds.studiousTestProject.hashtag.entity.HashtagName;
 import nerds.studiousTestProject.hashtag.entity.HashtagRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HashtagRepository extends JpaRepository<HashtagRecord, Long> {
+    HashtagRecord findByStudycafeIdAndName(Long studycafeId, HashtagName name);
 }
