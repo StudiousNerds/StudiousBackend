f8607c0ee32c422816c3e42de2e6bd6ef270c681
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-06 17:54:33 +0900

    feat: 리뷰 수정 컨트롤러 구현
    Related to: #30

--- src/main/java/nerds/studiousTestProject/review/controller/ReviewController.java
+++ src/main/java/nerds/studiousTestProject/review/controller/ReviewController.java
@@ -2,9 +2,13 @@ package nerds.studiousTestProject.review.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.review.dto.request.ModifyReviewRequest;
 import nerds.studiousTestProject.review.dto.request.RegisterReviewRequest;
+import nerds.studiousTestProject.review.dto.response.ModifyReviewResponse;
 import nerds.studiousTestProject.review.dto.response.RegisterReviewResponse;
 import nerds.studiousTestProject.review.service.ReviewService;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/studious")
+@RequestMapping("/studious/reviews")
 @Slf4j
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @PostMapping("/reviews")
+    @PostMapping()
     public RegisterReviewResponse registerReview(@RequestBody RegisterReviewRequest registerReviewRequest) {
         return reviewService.registerReview(registerReviewRequest);
+    }
+
+    @PatchMapping("/{reviewId}")
+    public ModifyReviewResponse modifyReview(@PathVariable("reviewId") Long reviewId, @RequestBody ModifyReviewRequest modifyReviewRequest) {
+        return reviewService.modifyReview(reviewId, modifyReviewRequest);
     }
 }
