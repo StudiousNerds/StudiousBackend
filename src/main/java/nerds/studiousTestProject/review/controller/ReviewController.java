028b4fed871b27c95b5d75c4bfa5ec4701188eb0
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-06 21:11:07 +0900

    feat: 리뷰 삭제 컨트롤러 구현
    Related to: #31

--- src/main/java/nerds/studiousTestProject/review/controller/ReviewController.java
+++ src/main/java/nerds/studiousTestProject/review/controller/ReviewController.java
@@ -3,12 +3,15 @@ package nerds.studiousTestProject.review.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.review.dto.request.DeleteReviewRequest;
 import nerds.studiousTestProject.review.dto.request.ModifyReviewRequest;
 import nerds.studiousTestProject.review.dto.request.RegisterReviewRequest;
+import nerds.studiousTestProject.review.dto.response.DeleteReviewResponse;
 import nerds.studiousTestProject.review.dto.response.ModifyReviewResponse;
 import nerds.studiousTestProject.review.dto.response.RegisterReviewResponse;
 import nerds.studiousTestProject.review.service.ReviewService;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +35,10 @@ public class ReviewController {
     @PatchMapping("/{reviewId}")
     public ModifyReviewResponse modifyReview(@PathVariable("reviewId") Long reviewId, @RequestBody @Valid ModifyReviewRequest modifyReviewRequest) {
         return reviewService.modifyReview(reviewId, modifyReviewRequest);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public DeleteReviewResponse deleteReview(@PathVariable("reviewId") Long reviewId, @RequestBody DeleteReviewRequest deleteReviewRequest) {
+        return reviewService.deleteReview(reviewId, deleteReviewRequest);
     }
 }
