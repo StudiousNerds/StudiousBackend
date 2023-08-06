a43c11181d723f7155a1c150b9692c5f2bbfe491
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-06 17:53:29 +0900

    chore: 어노테이션 정리

--- src/main/java/nerds/studiousTestProject/hashtag/repository/HashtagRepository.java
+++ src/main/java/nerds/studiousTestProject/hashtag/repository/HashtagRepository.java
@@ -3,7 +3,5 @@ package nerds.studiousTestProject.hashtag.repository;
 import nerds.studiousTestProject.hashtag.entity.HashtagRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface HashtagRepository extends JpaRepository<HashtagRecord, Long> {
 }
