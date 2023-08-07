c7c55765d488fcb78dfc29d084e93226f581582e
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-07 18:46:33 +0900

    fix: status 값 변경
    돌아댕기다보니 저기를 수정한 거 같은데 반영이 안된건지 암튼 발견해서 고쳐놨습니다!

--- src/main/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepository.java
+++ src/main/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepository.java
@@ -13,7 +13,7 @@ public interface ReservationRecordRepository extends JpaRepository<ReservationRe
     @Query(value = "select r.startTime, r.endTime " +
             "from ReservationRecord r " +
             "where r.date = :date and r.room.id = :roomId " +
-            "and r.status = 'CONFIRMED'")
+            "and r.status = nerds.studiousTestProject.reservation.entity.ReservationStatus.CONFIRMED")
     List<Object[]> findAllReservedTime(@Param("date")LocalDate date, @Param("roomId")Long roomId);
 
     @Query("select r from ReservationRecord r where r.room.id = :roomId")
