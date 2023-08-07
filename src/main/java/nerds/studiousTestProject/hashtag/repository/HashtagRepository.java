704a277f7dbf212a3e0bd5445b904339eb4915ce
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-07 18:32:21 +0900

    refactor: 해시태그 수정 시 삭제 메소드 수정
    reviewId를 통해 해당 리뷰에서 작성한 모든 해시태그를 기록을 삭제하는 메소드를 작성했습니다.
    
    Related to: #30

--- src/main/java/nerds/studiousTestProject/hashtag/repository/HashtagRepository.java
+++ src/main/java/nerds/studiousTestProject/hashtag/repository/HashtagRepository.java
@@ -3,7 +3,11 @@ package nerds.studiousTestProject.hashtag.repository;
 import nerds.studiousTestProject.hashtag.entity.HashtagName;
 import nerds.studiousTestProject.hashtag.entity.HashtagRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface HashtagRepository extends JpaRepository<HashtagRecord, Long> {
-    HashtagRecord findByStudycafeIdAndName(Long studycafeId, HashtagName name);
+    void deleteAllByReviewId(Long reviewId);
 }
