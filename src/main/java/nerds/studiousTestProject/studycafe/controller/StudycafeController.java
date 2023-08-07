3f185605868cfe83a1abef2e293f947e20b4347a
kmw2378 <rlarla677@gmail.com>
2023-08-07 15:19:30 +0900

    feat: 사장님 페이지 - 스터디카페 전체 조회 관련 컨트롤러 메소드 추가

--- src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
+++ src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
@@ -10,6 +10,7 @@ import nerds.studiousTestProject.hashtag.entity.HashtagName;
 import nerds.studiousTestProject.studycafe.dto.FindStudycafeRequest;
 import nerds.studiousTestProject.studycafe.dto.FindStudycafeResponse;
 import nerds.studiousTestProject.studycafe.dto.MainPageResponse;
+import nerds.studiousTestProject.studycafe.dto.manage.ManagedCafeInquireResponse;
 import nerds.studiousTestProject.studycafe.dto.register.request.RegisterRequest;
 import nerds.studiousTestProject.studycafe.dto.register.response.RegisterResponse;
 import nerds.studiousTestProject.studycafe.dto.search.request.SearchRequest;
@@ -23,11 +24,13 @@ import nerds.studiousTestProject.studycafe.util.PageRequestConverter;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,7 +75,7 @@ public class StudycafeController {
                 .build();
 
         log.info("searchRequest = {}", searchRequest);
-        return studycafeService.inquire(searchRequest, PageRequestConverter.of(page));
+        return studycafeService.inquire(searchRequest, PageRequestConverter.of(page, 8));
     }
 
     @GetMapping("/studycafes/{cafeId}")
@@ -97,8 +100,14 @@ public class StudycafeController {
     }
 
     @PostMapping("/studycafes/registrations")
-    public RegisterResponse registerStudycafe(@RequestBody @Valid RegisterRequest registerRequest) {
+    public RegisterResponse register(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @RequestBody @Valid RegisterRequest registerRequest) {
         log.info("request = {}", registerRequest);
-        return studycafeService.register(registerRequest);
+        return studycafeService.register(accessToken, registerRequest);
     }
+
+    @GetMapping("/studycafes/managements")
+    public List<ManagedCafeInquireResponse> viewManagedStudycafe(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @RequestParam Integer page) {
+        return studycafeService.inquireManagedStudycafe(accessToken, PageRequestConverter.of(page, 4));
+    }
+
 }
