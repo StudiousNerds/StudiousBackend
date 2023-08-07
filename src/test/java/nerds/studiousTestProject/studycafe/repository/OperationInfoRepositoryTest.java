f26a635bb0bfe464bb9ec27001bfb9f8ccf514c2
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-07 19:10:54 +0900

    test: Optional 변경 후, 테스트 코드 변경
    이전에 말씀해주셨던 레포지토리 변경사항은 다음 테스트 코드 작성때 다 적용하겠습니다!
    
    Related to: #30

--- src/test/java/nerds/studiousTestProject/studycafe/repository/OperationInfoRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/studycafe/repository/OperationInfoRepositoryTest.java
@@ -1,8 +1,11 @@
 package nerds.studiousTestProject.studycafe.repository;
 
+import nerds.studiousTestProject.common.exception.ErrorCode;
+import nerds.studiousTestProject.common.exception.NotFoundException;
 import nerds.studiousTestProject.studycafe.entity.OperationInfo;
 import nerds.studiousTestProject.studycafe.entity.Week;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -21,23 +24,27 @@ class OperationInfoRepositoryTest {
     private OperationInfoRepository operationInfoRepository;
 
     @Test
+    @DisplayName("시작 시간을 찾아오는 테스트")
     void findStartTime() {
         // given
-        OperationInfo operation = OperationInfo.builder().startTime(LocalTime.of(8,0,0)).week(Week.SATURDAY).build();
+        OperationInfo operation = OperationInfo.builder().startTime(LocalTime.of(8,0,0)).week(Week.MONDAY).build();
         operationInfoRepository.save(operation);
         // when
-        LocalTime startTime = operationInfoRepository.findStartTime(Week.of(LocalDate.now()));
+        LocalTime startTime = operationInfoRepository.findStartTime(Week.of(LocalDate.now()))
+                .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_START_TIME));
         // then
         Assertions.assertThat(startTime.getHour()).isEqualTo(8);
     }
 
     @Test
+    @DisplayName("끝 시간을 찾아오는 테스트")
     void findEndTime() {
         // given
-        OperationInfo operation = OperationInfo.builder().endTime(LocalTime.of(21,0,0)).week(Week.SATURDAY).build();
+        OperationInfo operation = OperationInfo.builder().endTime(LocalTime.of(21,0,0)).week(Week.MONDAY).build();
         operationInfoRepository.save(operation);
         // when
-        LocalTime endTime = operationInfoRepository.findEndTime(Week.of(LocalDate.now()));
+        LocalTime endTime = operationInfoRepository.findEndTime(Week.of(LocalDate.now()))
+                .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_END_TIME));
         // then
         Assertions.assertThat(endTime.getHour()).isEqualTo(21);
     }
