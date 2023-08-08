5b9d307c2f4a07a3d49cca75ee54de1f264ee6ab
kmw2378 <rlarla677@gmail.com>
2023-08-08 15:52:47 +0900

    feat: JWT 토큰 파싱과정에서 토큰 관련 예외 상황을 처리할 핸들러 추가
    - 토큰 관련 예외 상황은 Filter 단에서 처리가 되므로 GlobalExceptionAdvice 에서 처리가 불가능합니다. 따라서, 별도로 예외 핸들링 필터를 만들고 config 파일에 추가하였습니다.

---
+++ src/main/java/nerds/studiousTestProject/common/filter/JwtExceptionHandlerFilter.java
@@ -0,0 +1,35 @@
+package nerds.studiousTestProject.common.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.common.exception.BadRequestException;
+import nerds.studiousTestProject.common.exception.ExceptionResponse;
+import nerds.studiousTestProject.common.exception.NotAuthorizedException;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtExceptionHandlerFilter extends OncePerRequestFilter {
+    private static final String LOG_FORMAT = "Class : {}, Code : {}, Message : {}";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (BadRequestException | NotAuthorizedException e) {
+            int status = e instanceof BadRequestException ? HttpServletResponse.SC_BAD_REQUEST : HttpServletResponse.SC_UNAUTHORIZED;
+            response.setStatus(status);
+            response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            response.getWriter().write(new ObjectMapper().writeValueAsString(ExceptionResponse.from(e)));
+            log.info(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
+        }
+    }
+}
