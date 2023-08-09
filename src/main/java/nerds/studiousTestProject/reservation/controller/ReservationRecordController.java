e648f2ab5c3e995ae5d1269c577fcd22fd4b6d3d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-10 00:45:08 +0900

    refactor: 예약 취소시 예약 관리 uri만 넣어서 반환하도록 변경
    예약 취소시 취소 관련 DTO를 반환하는 것이 아닌 예약 관리 페이지로 리다이렉트 될 수 있도록 location만 담아 반환하도록 변경했습니다.
    
    Related to: #37

--- src/main/java/nerds/studiousTestProject/reservation/controller/ReservationRecordController.java
+++ src/main/java/nerds/studiousTestProject/reservation/controller/ReservationRecordController.java
@@ -2,12 +2,12 @@ package nerds.studiousTestProject.reservation.controller;
 
 import lombok.RequiredArgsConstructor;
 import nerds.studiousTestProject.payment.util.totoss.CancelRequest;
-import nerds.studiousTestProject.payment.dto.cancel.response.CancelResponse;
 import nerds.studiousTestProject.payment.service.PaymentService;
 import nerds.studiousTestProject.reservation.dto.cancel.response.ReservationCancelResponse;
 import nerds.studiousTestProject.reservation.dto.reserve.response.ReserveResponse;
 import nerds.studiousTestProject.reservation.service.ReservationRecordService;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,9 +27,11 @@ public class ReservationRecordController {
     private final ReservationRecordService reservationRecordService;
 
     @PostMapping("/mypage/reservation-settings/{reservationId}/cancellations")
-    public List<CancelResponse> cancelReservation(@PathVariable Long reservationId,
-                                                  @RequestBody CancelRequest cancelRequest){
-        return paymentService.cancel(cancelRequest, reservationId);
+    public ResponseEntity<Void> cancelReservation(@PathVariable Long reservationId,
+                                                  @RequestBody CancelRequest cancelRequest) {
+        paymentService.cancel(cancelRequest, reservationId);
+        return ResponseEntity.created(URI.create("/mypage/reservatioin-settings?tab=cancel"))
+                .build();
     }
 
     @GetMapping("/reservations/studycafes/{cafeId}/rooms/{roomId}")
@@ -38,7 +40,7 @@ public class ReservationRecordController {
     }
 
     @GetMapping("/mypage/reservation-settings/{reservationId}/cancellations")
-    public ReservationCancelResponse cancelReservationInfo(@PathVariable Long reservationId){
+    public ReservationCancelResponse cancelReservationInfo(@PathVariable Long reservationId) {
         return reservationRecordService.cancelInfo(reservationId);
     }
 
