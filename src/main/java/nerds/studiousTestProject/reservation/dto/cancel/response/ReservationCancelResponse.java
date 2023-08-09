ebeec6d6dbbdf8c0fc864218b04fb231d5dd3da4
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-10 01:42:33 +0900

    refactor: DTO 세분화에 따른 변경
    DTO가 세분화 됨에 따라 필드가 변경되었습니다.
    
    Related to: #37

--- src/main/java/nerds/studiousTestProject/reservation/dto/cancel/response/ReservationCancelResponse.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/cancel/response/ReservationCancelResponse.java
@@ -3,26 +3,12 @@ package nerds.studiousTestProject.reservation.dto.cancel.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import nerds.studiousTestProject.reservation.dto.RefundPolicyInResponse;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
 
 @AllArgsConstructor
 @Builder
 @Getter
 public class ReservationCancelResponse {
-    private String studycafeName;
-    private String roomName;
-    private LocalDate reservationDate;
-    private LocalTime reservationStartTime;
-    private LocalTime reservationEndTime;
-    private int reservationDuration;
-    private List<RefundPolicyInResponse> refundPolicy;
-    private String paymentMethod;
-    private int price;
-    private int refundPrice;
-    private int refundFee;
-    private RefundPolicyInResponse refundPolicyOnDay;
+    private ReservationRecordInfo reservationInfo;
+    private PaymentInfo paymentInfo;
+    private RefundPolicyInfo refundPolicyInfo;
 }
