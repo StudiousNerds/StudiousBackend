fecb3d7fe62f98f9b6686c80fa587b9415ba938b
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 18:10:51 +0900

    refactor: 사용하지 않는 import 문 삭제
    사용하지 않는 import문 삭제 입니다.
    
    Related to: #37

--- src/main/java/nerds/studiousTestProject/convenience/service/ConvenienceService.java
+++ src/main/java/nerds/studiousTestProject/convenience/service/ConvenienceService.java
@@ -1,15 +1,10 @@
 package nerds.studiousTestProject.convenience.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nerds.studiousTestProject.convenience.repository.ConvenienceRepository;
-import nerds.studiousTestProject.hashtag.entity.Hashtag;
-import nerds.studiousTestProject.hashtag.entity.HashtagRecord;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
