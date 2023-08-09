e01122b9c4ea09de92911bcd1bc559703ed135b3
kmw2378 <rlarla677@gmail.com>
2023-08-09 13:42:52 +0900

    refactor: Studycafe 컨트롤러에서 검색 uri와 매핑된 메소드(search)에서 쿼리 파라미터값을 @RequestParam 으로 일일히 받는 것을 @ModelAttribute 를 사용하여 객체로 바인딩하도록 수정

--- src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
+++ src/main/java/nerds/studiousTestProject/studycafe/controller/StudycafeController.java
@@ -1,12 +1,8 @@
 package nerds.studiousTestProject.studycafe.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import nerds.studiousTestProject.convenience.entity.ConvenienceName;
-import nerds.studiousTestProject.hashtag.entity.HashtagName;
 import nerds.studiousTestProject.studycafe.dto.FindStudycafeRequest;
 import nerds.studiousTestProject.studycafe.dto.FindStudycafeResponse;
 import nerds.studiousTestProject.studycafe.dto.MainPageResponse;
@@ -16,18 +12,15 @@ import nerds.studiousTestProject.studycafe.dto.register.request.RegisterRequest;
 import nerds.studiousTestProject.studycafe.dto.register.response.RegisterResponse;
 import nerds.studiousTestProject.studycafe.dto.search.request.SearchRequest;
 import nerds.studiousTestProject.studycafe.dto.search.response.SearchResponse;
-import nerds.studiousTestProject.studycafe.dto.search.request.SortType;
 import nerds.studiousTestProject.studycafe.dto.valid.request.AccountInfoRequest;
 import nerds.studiousTestProject.studycafe.dto.valid.request.BusinessInfoRequest;
 import nerds.studiousTestProject.studycafe.dto.valid.response.ValidResponse;
 import nerds.studiousTestProject.studycafe.service.StudycafeService;
 import nerds.studiousTestProject.studycafe.util.PageRequestConverter;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.Range;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,45 +29,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/studious")
-@Validated
 @Slf4j
 public class StudycafeController {
     private final StudycafeService studycafeService;
 
     @GetMapping("/search")
-    public List<SearchResponse> search(
-            @RequestParam Integer page,
-            @RequestParam(required = false) SortType sortType,       // 정렬 기준
-            @RequestParam @Length(min = 2) String keyword,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @FutureOrPresent LocalDate date,
-            @RequestParam(required = false) LocalTime startTime,
-            @RequestParam(required = false) LocalTime endTime,
-            @RequestParam(required = false) @Positive Integer headCount,
-            @RequestParam(required = false) @Range(min = 0, max = 5) Integer minGrade,          // 최소 평점
-            @RequestParam(required = false) Boolean eventInProgress,   // 이벤트 여부
-            @RequestParam(required = false) List<HashtagName> hashtags,     // 해시태그
-            @RequestParam(required = false) List<ConvenienceName> conveniences // 편의 시설
-    ) {
-        SearchRequest searchRequest = SearchRequest.builder()
-                .sortType(sortType)
-                .keyword(keyword)
-                .date(date)
-                .startTime(startTime)
-                .endTime(endTime)
-                .headCount(headCount)
-                .minGrade(minGrade)
-                .eventInProgress(eventInProgress)
-                .hashtags(hashtags)
-                .conveniences(conveniences)
-                .build();
-
+    public List<SearchResponse> search(@RequestParam Integer page, @ModelAttribute @Valid SearchRequest searchRequest) {
         log.info("searchRequest = {}", searchRequest);
         return studycafeService.inquire(searchRequest, PageRequestConverter.of(page, 8));
     }
