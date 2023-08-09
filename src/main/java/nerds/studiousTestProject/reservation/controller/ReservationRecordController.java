481dd4ecaa1acdb05f96ecfc150bd38147c152a1
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-09 19:41:06 +0900

    feat: 예약 취소 페이지 정보 반환 기능 추가
    예약 취소 페이지 정보를 반환하는 기능입니다.
    
    Related to: #37

--- src/main/java/nerds/studiousTestProject/reservation/controller/ReservationRecordController.java
+++ src/main/java/nerds/studiousTestProject/reservation/controller/ReservationRecordController.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import nerds.studiousTestProject.payment.util.totoss.CancelRequest;
 import nerds.studiousTestProject.payment.dto.cancel.response.CancelResponse;
 import nerds.studiousTestProject.payment.service.PaymentService;
+import nerds.studiousTestProject.reservation.dto.cancel.response.ReservationCancelResponse;
 import nerds.studiousTestProject.reservation.dto.reserve.response.ReserveResponse;
 import nerds.studiousTestProject.reservation.service.ReservationRecordService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +26,7 @@ public class ReservationRecordController {
     private final PaymentService paymentService;
     private final ReservationRecordService reservationRecordService;
 
-    @GetMapping("{reservationRecordId}/cancel")
+    @PostMapping("{reservationRecordId}/cancel")
     public List<CancelResponse> cancelReservation(@PathVariable Long reservationRecordId,
                                                   @RequestBody CancelRequest cancelRequest){
         return paymentService.cancel(cancelRequest, reservationRecordId);
@@ -33,6 +35,11 @@ public class ReservationRecordController {
     @GetMapping("/reservations/studycafes/{cafeId}/rooms/{roomId}")
     public ReserveResponse reserve(@PathVariable Long cafeId, @PathVariable Long roomId, @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
         return reservationRecordService.reserve(cafeId, roomId, accessToken);
+    }
+
+    @GetMapping("/mypage/reservation-settings/{reservationId}/cancellations")
+    public ReservationCancelResponse cancelReservation(@PathVariable Long reservationId){
+        return reservationRecordService.cancelInfo(reservationId);
     }
 
 }
