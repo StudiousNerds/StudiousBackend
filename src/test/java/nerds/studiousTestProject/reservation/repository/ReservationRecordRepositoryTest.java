00f5ea1049cadeb5a8d7d0aeb41be58989a8941d
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-10 23:06:23 +0900

    test: 룸 id를 통해 예약 내역을 모두 찾아오는 메소드 테스트
    테스트 결과: 성공🙆🏻‍♀️

--- src/test/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryTest.java
@@ -1,44 +1,35 @@
-//package nerds.studiousTestProject.reservation.repository;
-//
-//import nerds.studiousTestProject.reservation.entity.ReservationRecord;
-//import nerds.studiousTestProject.reservation.entity.ReservationStatus;
-//import nerds.studiousTestProject.room.entity.Room;
-//import nerds.studiousTestProject.room.repository.RoomRepository;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//class ReservationRecordRepositoryTest {
-//
-//    @Autowired
-//    private ReservationRecordRepository reservationRecordRepository;
-//    @Autowired
-//    private RoomRepository roomRepository;
-//
-//    @Test
-//    void findAllReservedTime() {
-//        // given
-//        Room room = roomRepository.save(Room.builder().id(1L).build());
-//        reservationRecordRepository.save(ReservationRecord.builder().room(room).date(LocalDate.now())
-//                .startTime(LocalTime.of(13, 0)).endTime(LocalTime.of(15, 0)).reservationStatus(ReservationStatus.RESERVED).build());
-//        reservationRecordRepository.save(ReservationRecord.builder().room(room).date(LocalDate.now())
-//                .startTime(LocalTime.of(16, 0)).endTime(LocalTime.of(17, 0)).reservationStatus(ReservationStatus.RESERVED).build());
-//        // when
-//        List<Object[]> allReservedTime = reservationRecordRepository.findAllReservedTime(LocalDate.now(), room.getId());
-//        // then
-//        Assertions.assertThat(allReservedTime).hasSize(2);
-//        Assertions.assertThat(allReservedTime.get(0)[0]).isEqualTo(13);
-//        Assertions.assertThat((LocalTime) allReservedTime.get(0)[1]).isEqualTo(15);
-//        Assertions.assertThat((LocalTime) allReservedTime.get(1)[0]).isEqualTo(16);
-//        Assertions.assertThat((LocalTime) allReservedTime.get(1)[1]).isEqualTo(17);
-//    }
-//}
+package nerds.studiousTestProject.reservation.repository;
+
+import nerds.studiousTestProject.RepositoryTest;
+import nerds.studiousTestProject.ReservationRecordFixture;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
+import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.room.repository.RoomRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static nerds.studiousTestProject.RoomFixture.*;
+
+@RepositoryTest
+class ReservationRecordRepositoryTest {
+
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private ReservationRecordRepository reservationRecordRepository;
+
+    @Test
+    void findAllByRoomId() {
+        // given
+        Room room1 = roomRepository.save(ROOM_FOUR_SIX.생성(1L));
+        ReservationRecord save1 = reservationRecordRepository.save(ReservationRecordFixture.FIRST_RESERVATION.룸_생성(room1, 1L));
+        ReservationRecord save2 = reservationRecordRepository.save(ReservationRecordFixture.SECOND_RESERVATION.룸_생성(room1, 2L));
+        // when
+        List<ReservationRecord> reservationRecordList = reservationRecordRepository.findAllByRoomId(1L);
+        // then
+        Assertions.assertThat(reservationRecordList).contains(save1, save2);
+    }
+}
