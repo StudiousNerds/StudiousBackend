0528cabdd4b5aef3872e5ea89911fa6ca471760d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-11 03:48:29 +0900

    test: 예약 내역 목록 페이징 테스트 추가
    예약 내역 목록 전체 페이징 테스트입니다.
    
    Related to: #33

--- src/test/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryTest.java
@@ -1,44 +1,89 @@
-//package nerds.studiousTestProject.reservation.repository;
-//
-//import nerds.studiousTestProject.reservation.entity.ReservationRecord;
-//import nerds.studiousTestProject.reservation.entity.ReservationStatus;
-//import nerds.studiousTestProject.room.entity.Room;
-//import nerds.studiousTestProject.room.repository.RoomRepository;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//class ReservationRecordRepositoryTest {
-//
-//    @Autowired
-//    private ReservationRecordRepository reservationRecordRepository;
-//    @Autowired
-//    private RoomRepository roomRepository;
-//
-//    @Test
-//    void findAllReservedTime() {
-//        // given
-//        Room room = roomRepository.save(Room.builder().id(1L).build());
-//        reservationRecordRepository.save(ReservationRecord.builder().room(room).date(LocalDate.now())
-//                .startTime(LocalTime.of(13, 0)).endTime(LocalTime.of(15, 0)).reservationStatus(ReservationStatus.RESERVED).build());
-//        reservationRecordRepository.save(ReservationRecord.builder().room(room).date(LocalDate.now())
-//                .startTime(LocalTime.of(16, 0)).endTime(LocalTime.of(17, 0)).reservationStatus(ReservationStatus.RESERVED).build());
-//        // when
-//        List<Object[]> allReservedTime = reservationRecordRepository.findAllReservedTime(LocalDate.now(), room.getId());
-//        // then
-//        Assertions.assertThat(allReservedTime).hasSize(2);
-//        Assertions.assertThat(allReservedTime.get(0)[0]).isEqualTo(13);
-//        Assertions.assertThat((LocalTime) allReservedTime.get(0)[1]).isEqualTo(15);
-//        Assertions.assertThat((LocalTime) allReservedTime.get(1)[0]).isEqualTo(16);
-//        Assertions.assertThat((LocalTime) allReservedTime.get(1)[1]).isEqualTo(17);
-//    }
-//}
+package nerds.studiousTestProject.reservation.repository;
+
+import nerds.studiousTestProject.RepositoryTest;
+import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.room.repository.RoomRepository;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
+import nerds.studiousTestProject.support.fixture.RoomFixture;
+import nerds.studiousTestProject.member.entity.member.Member;
+import nerds.studiousTestProject.member.repository.member.MemberRepository;
+import nerds.studiousTestProject.reservation.dto.mypage.response.ReservationSettingsStatus;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
+import nerds.studiousTestProject.studycafe.repository.StudycafeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static nerds.studiousTestProject.support.fixture.MemberFixture.BEAVER;
+import static nerds.studiousTestProject.support.fixture.MemberFixture.MINU;
+import static nerds.studiousTestProject.support.fixture.ReservationRecordFixture.CANCELED_RESERVATION;
+import static nerds.studiousTestProject.support.fixture.ReservationRecordFixture.CONFIRM_RESERVATION;
+import static nerds.studiousTestProject.support.fixture.StudycafeFixture.NERDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RepositoryTest
+class ReservationRecordRepositoryTest {
+
+    @Autowired
+    private ReservationRecordRepository reservationRecordRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private StudycafeRepository studycafeRepository;
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Test
+    @DisplayName(value = "회원이 주어졌을 때 해당 회원의 예약 목록을 페이징해 조회할 수 있다.")
+    void 예약_목록_전체를_페이징하여_조회한다() {
+
+        Member member1 = 멤버_저장(BEAVER.생성(1L));
+        Member member2 = 멤버_저장(MINU.생성(2L));
+        Studycafe studycafe = 스터디카페_저장(NERDS.생성());
+        Room room = 룸_저장(RoomFixture.ROOM_FOUR_SIX.스터디카페_생성(studycafe));
+
+        예약_내역_저장(CANCELED_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(12, 0), member1, room));
+        예약_내역_저장(CONFIRM_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(12, 0), LocalTime.of(14, 0), member1, room));
+        예약_내역_저장(CONFIRM_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(14, 0), LocalTime.of(16, 0), member1, room));
+        예약_내역_저장(CONFIRM_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(16, 0), LocalTime.of(18, 0), member1, room));
+        ReservationRecord reservation1 = 예약_내역_저장(CONFIRM_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(19, 0), LocalTime.of(21, 0), member1, room));
+        ReservationRecord reservation2 = 예약_내역_저장(CONFIRM_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(22, 0), LocalTime.of(23, 0), member1, room));
+        예약_내역_저장(CONFIRM_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(19, 0), LocalTime.of(20, 0), member2, room));
+        예약_내역_저장(CONFIRM_RESERVATION.예약_내역_생성(LocalDate.now(), LocalTime.of(22, 0), LocalTime.of(12, 0), member2, room));
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<ReservationRecord> page = reservationRecordRepository.getReservationRecordsConditions(ReservationSettingsStatus.ALL, null, null, null, member1, pageable);
+        List<ReservationRecord> content = page.getContent();
+        assertAll(
+                ()->assertThat(page.getTotalPages()).isEqualTo(3),
+                ()->assertThat(page.getContent()).containsExactly(reservation2, reservation1)
+        );
+    }
+
+    private Room 룸_저장(Room room) {
+        return roomRepository.save(room);
+    }
+
+    private Member 멤버_저장(Member member) {
+        return memberRepository.save(member);
+    }
+
+    private ReservationRecord 예약_내역_저장(ReservationRecord reservationRecord) {
+        return reservationRecordRepository.save(reservationRecord);
+    }
+
+    private Studycafe 스터디카페_저장(Studycafe studycafe) {
+        return studycafeRepository.save(studycafe);
+    }
+
+
+
+
+}
