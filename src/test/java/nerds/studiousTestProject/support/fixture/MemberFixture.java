61b7250669dd05f0fe51096d9f1e7505f77356c6
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-11 03:44:59 +0900

    feat: fixture 추가
    member, reservationRecord, studycafe fixture 추가입니다.
    
    Related to: #33

---
+++ src/test/java/nerds/studiousTestProject/support/fixture/MemberFixture.java
@@ -0,0 +1,34 @@
+package nerds.studiousTestProject.support.fixture;
+
+import nerds.studiousTestProject.member.entity.member.Member;
+import nerds.studiousTestProject.member.entity.member.Member.MemberBuilder;
+
+public enum MemberFixture {
+
+    BEAVER("길가은", "rlfrkdms@naver.com"),
+    MINU("김민우", "rlaalsdn@naver.com"),
+    POTATO("최보현", "chlqhgus@naver.com");
+
+    private final String name;
+    private final String email;
+
+    MemberFixture(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+    public Member 생성(){
+        return 생성(null);
+    }
+
+    public Member 생성(Long id){
+        return 기본_정보_빌더_생성(id).build();
+    }
+
+    public MemberBuilder 기본_정보_빌더_생성(Long id){
+        return Member.builder()
+                .name(this.name)
+                .email(this.email);
+    }
+}
