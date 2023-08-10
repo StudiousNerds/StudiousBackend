abb5cdd296a6bee0fdf224ec0daef2c97f8ae18d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-10 23:45:31 +0900

    feat: queryDsl을 위한 custom repository 추가
    queryDsl을 사용하기 위해 custom repository를 추가했습니다.
    
    Related to: #33

---
+++ src/main/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryCustom.java
@@ -0,0 +1,13 @@
+package nerds.studiousTestProject.reservation.repository;
+
+import nerds.studiousTestProject.member.entity.member.Member;
+import nerds.studiousTestProject.reservation.dto.mypage.ReservationSettingsStatus;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDate;
+
+public interface ReservationRecordRepositoryCustom {
+
+    Page<ReservationRecord> getReservationRecordsConditions(ReservationSettingsStatus tab, String studycafeName, LocalDate startDate, LocalDate endDate, Member member, Pageable pageable);
+}
