e7d584bedebdd2554696ffa56774e8177d99b7a3
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-10 23:51:22 +0900

    feat: String을 Enum으로 바꾸는 컨버터 추가
    쿼리 파라미터를 Enum으로 받을 수 있는 컨버터 입니다.
    
    Related to: #33

---
+++ src/main/java/nerds/studiousTestProject/common/util/StringToReservationSettingsStatusConverter.java
@@ -0,0 +1,19 @@
+package nerds.studiousTestProject.common.util;
+
+import nerds.studiousTestProject.common.exception.BadRequestException;
+import nerds.studiousTestProject.reservation.dto.mypage.ReservationSettingsStatus;
+import org.springframework.core.convert.converter.Converter;
+
+import static nerds.studiousTestProject.common.exception.ErrorCode.CANT_CONVERT_TO_RESERVATION_SETTING_STATUS;
+
+public class StringToReservationSettingsStatusConverter implements Converter<String, ReservationSettingsStatus> {
+    @Override
+    public ReservationSettingsStatus convert(String status) {
+        if(status == null) return null;
+        try {
+            return ReservationSettingsStatus.valueOf(status);
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException(CANT_CONVERT_TO_RESERVATION_SETTING_STATUS);
+        }
+    }
+}
