e35432c64f33bca7f2a9f2539001eb6ad0095934
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-10 19:52:29 +0900

    feat: 예약 관리 내역 조회 기능 추가
    db에서 예약 관리 탭, 스카명, 날짜 조건에 맞는 예약 내역을 가져옵니다.
    
    Related to: #33

---
+++ src/main/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryCustomImpl.java
@@ -0,0 +1,102 @@
+package nerds.studiousTestProject.reservation.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import nerds.studiousTestProject.member.entity.member.Member;
+import nerds.studiousTestProject.reservation.dto.mypage.ReservationSettingsStatus;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
+import nerds.studiousTestProject.reservation.entity.ReservationStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static nerds.studiousTestProject.reservation.dto.mypage.ReservationSettingsStatus.AFTER_USING;
+import static nerds.studiousTestProject.reservation.dto.mypage.ReservationSettingsStatus.BEFORE_USING;
+import static nerds.studiousTestProject.reservation.dto.mypage.ReservationSettingsStatus.CANCELED;
+import static nerds.studiousTestProject.reservation.dto.mypage.ReservationSettingsStatus.USING;
+import static nerds.studiousTestProject.reservation.entity.QReservationRecord.reservationRecord;
+import static nerds.studiousTestProject.studycafe.entity.QStudycafe.studycafe;
+
+
+@Component
+@RequiredArgsConstructor
+public class ReservationRecordRepositoryCustomImpl implements ReservationRecordRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<ReservationRecord> getReservationRecordsConditions(ReservationSettingsStatus tab, String studycafeName, LocalDate startDate, LocalDate endDate, Member member, Pageable pageable) {
+        List<ReservationRecord> reservationRecordList = jpaQueryFactory.selectFrom(reservationRecord)
+                .innerJoin(reservationRecord.member).on(reservationRecord.member.id.eq(member.getId()))
+                .innerJoin(reservationRecord.room.studycafe, studycafe)
+                .where(
+                        equalToStudycafeName(studycafeName),
+                        handleTab(tab),
+                        afterReservationStartDate(startDate),
+                        beforeReservationEndDate(endDate)
+                )
+                .offset(pageable.getOffset())
+                .fetch();
+    }
+
+    private BooleanExpression equalToStudycafeName(String studycafeName) {
+        return studycafeName == null ? null : studycafe.name.eq(studycafeName);
+    }
+
+    private BooleanExpression afterReservationStartDate(LocalDate startDate) {
+        return startDate == null ? null : reservationRecord.date.after(startDate);
+    }
+
+    private BooleanExpression beforeReservationEndDate(LocalDate endDate) {
+        return endDate == null ? null : reservationRecord.date.before(endDate);
+    }
+
+    private BooleanExpression handleTab(ReservationSettingsStatus tab) {
+        LocalDate nowDate = LocalDate.now();
+        LocalTime nowTime = LocalTime.now();
+        if (tab == CANCELED)
+            return reservationRecord.status.eq(ReservationStatus.CANCELED);
+        if (tab == BEFORE_USING)
+            return conditionOfBeforeUsing(nowDate, nowTime);
+        if (tab == AFTER_USING)
+            return conditionOfAfterUsing(nowDate, nowTime);
+        if (tab == USING)
+            return conditionOfUsing(nowDate, nowTime);
+        return null;
+    }
+
+    private BooleanExpression conditionOfUsing(LocalDate nowDate, LocalTime nowTime) {
+        return reservationRecord.status.eq(ReservationStatus.CONFIRMED)
+                .and(reservationRecord.date.eq(nowDate))
+                .and(reservationRecord.startTime.loe(nowTime))
+                .and(reservationRecord.endTime.goe(nowTime));
+    }
+
+    private BooleanExpression conditionOfAfterUsing(LocalDate nowDate, LocalTime nowTime) {
+        return reservationRecord.status.eq(ReservationStatus.CONFIRMED)
+                .and(
+                        reservationRecord.date.before(nowDate)
+                                .or(
+                                        reservationRecord.date.eq(nowDate)
+                                                .and(reservationRecord.endTime.before(nowTime))
+                                )
+                );
+    }
+
+    private BooleanExpression conditionOfBeforeUsing(LocalDate nowDate, LocalTime nowTime) {
+        return reservationRecord.status.eq(ReservationStatus.CONFIRMED)
+                .and(
+                        reservationRecord.date.after(nowDate)
+                                .or(
+                                        reservationRecord.date.eq(nowDate)
+                                                .and(reservationRecord.startTime.after(nowTime))
+                                )
+                );
+    }
+
+}
