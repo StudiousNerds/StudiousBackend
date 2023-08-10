cdcaf46c27ddfc41ed46a772d8589650d7da3131
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-11 03:55:00 +0900

    test: dsl 관련 config 추가
    query dsl 관련 설정을 추가했습니다.
    
    Related to: #33

--- src/test/java/nerds/studiousTestProject/RepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/RepositoryTest.java
@@ -1,8 +1,10 @@
 package nerds.studiousTestProject;
 
+import nerds.studiousTestProject.common.config.QueryDslConfig;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,7 +13,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
-@DataJpaTest(showSql = false)
+@DataJpaTest(showSql = true)
+@Import(QueryDslConfig.class)
 public @interface RepositoryTest {
 
 }
