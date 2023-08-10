8f003f4a184874f9509522d4152a62f127919db7
kmw2378 <rlarla677@gmail.com>
2023-08-10 15:58:12 +0900

    feat: 시작/마감 시간 필드에 @NotNull 검증 어노테이션 추가

--- src/main/java/nerds/studiousTestProject/studycafe/dto/register/request/OperationInfoRequest.java
+++ src/main/java/nerds/studiousTestProject/studycafe/dto/register/request/OperationInfoRequest.java
@@ -14,9 +14,11 @@ public class OperationInfoRequest {
     @Valid
     private Week week;    // 요일 (추후, 열거체로 리펙토링)
 
+    @NotNull(message = "시작 시간 입력은 필수입니다.")
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     private LocalTime startTime;    // 시작 시간
 
+    @NotNull(message = "마감 시간 입력은 필수입니다.")
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     private LocalTime endTime;  // 마감 시간
 
