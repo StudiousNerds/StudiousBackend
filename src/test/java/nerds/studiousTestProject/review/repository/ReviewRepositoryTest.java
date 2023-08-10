879bec174588a8485d8a3bd74b61d9c5ed0d56fc
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-10 21:20:52 +0900

    test: 예약내역 id리스트로 모든 예약 내역을 가져오는 메소드 테스트
    예약내역 id 리스트를 넘겨주면 생성일자를 내림차순으로 넘겨주는 메소드를 테스트했고 결과는 성공입니다.
    찜찜해서 query날라가는거 확인했는데 의도대로 날라가는 걸 확인했습니다!
    
    Related to: #29

--- src/test/java/nerds/studiousTestProject/review/repository/ReviewRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/review/repository/ReviewRepositoryTest.java
@@ -1,5 +1,6 @@
 package nerds.studiousTestProject.review.repository;
 
+import nerds.studiousTestProject.RepositoryTest;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import nerds.studiousTestProject.reservation.repository.ReservationRecordRepository;
 import nerds.studiousTestProject.review.entity.Review;
@@ -13,11 +14,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+import static nerds.studiousTestProject.ReservationRecordFixture.*;
+import static nerds.studiousTestProject.ReviewFixture.*;
+
+@RepositoryTest
 class ReviewRepositoryTest {
 
     @Autowired
@@ -41,5 +49,20 @@ class ReviewRepositoryTest {
         List<Review> reviewList = reviewRepository.findTop3ByReservationRecordId(studycafe.getId());
         // then
         Assertions.assertThat(reviewList).contains(review);
+    }
+
+    @Test
+    void findAllByReservationRecordIdOrderByCreatedDatedDescIn() {
+        // given
+        Review firstReview = reviewRepository.save(FIRST_REVIEW.생성(1L));
+        Review secondReview = reviewRepository.save(SECOND_REVIEW.생성(2L));
+        Review thirdReview = reviewRepository.save(THIRD_REVIEW.생성(3L));
+        reservationRecordRepository.save(FIRST_RESERVATION.생성(1L)).addReview(firstReview);
+        reservationRecordRepository.save(SECOND_RESERVATION.생성(2L)).addReview(secondReview);
+        reservationRecordRepository.save(THIRD_RESERVATION.생성(3L)).addReview(thirdReview);
+        // when
+        List<Review> reviewList = reviewRepository.findAllByReservationRecordIdInOrderByCreatedDateDesc(Arrays.asList(1L, 2L, 3L));
+        // then
+        Assertions.assertThat(reviewList).containsExactly(firstReview, secondReview, thirdReview);
     }
 }
