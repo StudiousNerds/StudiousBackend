2ebc07cf3a3db355054c9fcf18a33308e0272449
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-11 16:30:47 +0900

    test: 스터디카페 id로 예약 내역을 모두 가져오는 메소드 테스트
    테스트 결과: 성공🙆🏻‍♀️

--- src/test/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepositoryTest.java
@@ -1,12 +1,17 @@
 package nerds.studiousTestProject.reservation.repository;
 
 import nerds.studiousTestProject.RepositoryTest;
+import nerds.studiousTestProject.fixture.ReviewFixture;
+import nerds.studiousTestProject.fixture.StudycafeFixture;
 import nerds.studiousTestProject.member.entity.member.Member;
 import nerds.studiousTestProject.member.repository.member.MemberRepository;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
+import nerds.studiousTestProject.review.entity.Review;
+import nerds.studiousTestProject.review.repository.ReviewRepository;
 import nerds.studiousTestProject.room.entity.Room;
 import nerds.studiousTestProject.room.repository.RoomRepository;
-import org.assertj.core.api.Assertions;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
+import nerds.studiousTestProject.studycafe.repository.StudycafeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,7 +19,10 @@ import java.util.List;
 
 import static nerds.studiousTestProject.fixture.MemberFixture.*;
 import static nerds.studiousTestProject.fixture.ReservationRecordFixture.*;
+import static nerds.studiousTestProject.fixture.ReviewFixture.*;
 import static nerds.studiousTestProject.fixture.RoomFixture.*;
+import static nerds.studiousTestProject.fixture.StudycafeFixture.*;
+import static org.assertj.core.api.Assertions.*;
 
 @RepositoryTest
 class ReservationRecordRepositoryTest {
@@ -25,6 +33,10 @@ class ReservationRecordRepositoryTest {
     private ReservationRecordRepository reservationRecordRepository;
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private StudycafeRepository studycafeRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     @Test
     void findAllByRoomId() {
@@ -35,17 +47,34 @@ class ReservationRecordRepositoryTest {
         // when
         List<ReservationRecord> reservationRecordList = reservationRecordRepository.findAllByRoomId(1L);
         // then
-        Assertions.assertThat(reservationRecordList).contains(save1, save2);
+        assertThat(reservationRecordList).contains(save1, save2);
     }
 
     @Test
     void findAllByMemberId() {
+        // given
         Member member = memberRepository.save(FIRST_MEMBER.생성(1L));
         ReservationRecord save1 = reservationRecordRepository.save(FIRST_RESERVATION.멤버_생성(member, 1L));
         ReservationRecord save2 = reservationRecordRepository.save(FIRST_RESERVATION.멤버_생성(member, 2L));
         // when
         List<ReservationRecord> reservationRecordList = reservationRecordRepository.findAllByMemberId(1L);
         // then
-        Assertions.assertThat(reservationRecordList).contains(save1, save2);
+        assertThat(reservationRecordList).contains(save1, save2);
+    }
+
+    @Test
+    void findAllByStudycafeId() {
+        // given
+        Studycafe studycafe = studycafeRepository.save(FIRST_STUDYCAFE.생성(1L));
+        Room room1 = roomRepository.save(ROOM_FOUR_SIX.생성(1L));
+        studycafe.addRoom(room1);
+        Review review = reviewRepository.save(FIRST_REVIEW.생성(1L));
+        ReservationRecord save1 = reservationRecordRepository.save(FIRST_RESERVATION.룸_생성(room1, 1L));
+        ReservationRecord save2 = reservationRecordRepository.save(SECOND_RESERVATION.룸_생성(room1, 2L));
+        save1.addReview(review);
+        // when
+        List<ReservationRecord> reservationRecordList = reservationRecordRepository.findAllByStudycafeId(studycafe.getId());
+        // then
+        assertThat(reservationRecordList).contains(save1);
     }
 }
