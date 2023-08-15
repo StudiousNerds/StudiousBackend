365cb6c8311bc3aa744adf2f3ae3f7064a7b8781
kmw2378 <rlarla677@gmail.com>
2023-08-15 20:03:02 +0900

    refactor: Address 클래스에 @Column 속성 추가 및 이하 DTO 클래스에서 반영
    Related to: #39

--- src/main/java/nerds/studiousTestProject/studycafe/entity/Address.java
+++ src/main/java/nerds/studiousTestProject/studycafe/entity/Address.java
@@ -1,5 +1,6 @@
 package nerds.studiousTestProject.studycafe.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -10,8 +11,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
+    @Column(name = "address_basic", nullable = false)
     private String addressBasic;   // 기본 주소
+
+    @Column(name = "address_detail", nullable = false)
     private String addressDetail;  // 상세 주소
+
+    @Column(name = "address_zipcode", nullable = false)
     private String addressZipcode; // 우편 번호
 
     public String getEntryAddress() {
