a1bafae0c19bcd5491901a9bb2f4081ca4c5e521
kmw2378 <rlarla677@gmail.com>
2023-08-15 19:11:12 +0900

    feat: Role 엔티티 추가
    Related to: #39

---
+++ src/main/java/nerds/studiousTestProject/member/entity/member/Role.java
@@ -0,0 +1,39 @@
+package nerds.studiousTestProject.member.entity.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole value;
+
+    public void setMember(Member member) {
+        if (member != null) {
+            this.member = member;
+        }
+    }
+}
