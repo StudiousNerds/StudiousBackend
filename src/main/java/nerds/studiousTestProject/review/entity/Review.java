99780f8bba6df11c710bac94f7bedbd2e9ad93af
kmw2378 <rlarla677@gmail.com>
2023-08-15 19:25:21 +0900

    refactor: 사용되지 않는 import 문 제거 및 ReservationRecord 의 @JoinColumn 명 수정 사용되지 않는 import 문을 제거 후 ReservationRecord의 @JoinColumn 의 name 속성 값을 reservation_record_id 로 수정하였습니다.
    Related to: #39

--- src/main/java/nerds/studiousTestProject/review/entity/Review.java
+++ src/main/java/nerds/studiousTestProject/review/entity/Review.java
@@ -2,26 +2,20 @@ package nerds.studiousTestProject.review.entity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import nerds.studiousTestProject.hashtag.entity.HashtagRecord;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
-import org.apache.catalina.LifecycleState;
-import org.bouncycastle.pqc.crypto.newhope.NHOtherInfoGenerator;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -36,7 +30,7 @@ public class Review {
     private Long id;
 
     @OneToOne
-    @JoinColumn(name = "reservation_id")
+    @JoinColumn(name = "reservation_record_id")
     private ReservationRecord reservationRecord;
 
     @OneToMany(mappedBy = "review",
