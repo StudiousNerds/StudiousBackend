494364d74c9b2d0ae6f23e73accc7a44362a37b7
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-18 20:03:58 +0900

    refactor: 해시태그 가져오는 로직 수정
    우선 리뷰에서 다 가져오는 방식으로 만들어 두었습니다~
    
    Related to: #43

---
+++ src/main/java/nerds/studiousTestProject/hashtag/service/HashtagRecordService.java
@@ -0,0 +1,28 @@
+package nerds.studiousTestProject.hashtag.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.hashtag.entity.HashtagName;
+import nerds.studiousTestProject.hashtag.repository.HashtagRecordRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+public class HashtagRecordService {
+    private final HashtagRecordRepository hashtagRecordRepository;
+
+    public List<HashtagName> findStudycafeHashtag(Long studycafeId) {
+        List<HashtagName> hashtagNames = hashtagRecordRepository.findHashtagRecordByStudycafeId(studycafeId);
+        List<HashtagName> hashtagNameList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            hashtagNameList.add(hashtagNames.get(i));
+        }
+        return hashtagNameList;
+    }
+}
