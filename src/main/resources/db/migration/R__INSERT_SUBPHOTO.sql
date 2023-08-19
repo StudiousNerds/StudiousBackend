c6fd331bcadcfdf16fcd5f159e8b7cd2e2d66d8d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-19 12:31:30 +0900

    feat: 서브 포토 최소 사진 수 맞추도록 더미데이터 추가
    Related to: #38

--- src/main/resources/db/migration/R__INSERT_SUBPHOTO.sql
+++ src/main/resources/db/migration/R__INSERT_SUBPHOTO.sql
@@ -2,50 +2,69 @@
 ------------------- 스카 1 -----------------------
 -- 룸 사진 4장인 경우
 INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 1);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 1);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 1);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 1); --스카 1
+INSERT IGNORE INTO `sub_photo` VALUES (2, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 1);
+INSERT IGNORE INTO `sub_photo` VALUES (3, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 1);
+INSERT IGNORE INTO `sub_photo` VALUES (4, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 1); --스카 1
 -- 룸 사진 2장인 경우
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 2);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 2); -- 스카 1
+INSERT IGNORE INTO `sub_photo` VALUES (5, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 2);
+INSERT IGNORE INTO `sub_photo` VALUES (6, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 2); -- 스카 1
 -- 룸 사진 3장인 경
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 3);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 3);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 3); -- 스카 1
+INSERT IGNORE INTO `sub_photo` VALUES (7, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 3);
+INSERT IGNORE INTO `sub_photo` VALUES (8, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 3);
+INSERT IGNORE INTO `sub_photo` VALUES (9, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 3); -- 스카 1
 -- 룸 사진 1장인 경우
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 4); -- 스카 1
+INSERT IGNORE INTO `sub_photo` VALUES (10, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 4); -- 스카 1
 -------------------------------------------------
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 5); -- 스카 2
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 6);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 6); -- 스카 3
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 7); -- 스카 3
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 8); -- 스카 4
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 9); -- 스카 4
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 10); -- 스카 5
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 11); -- 스카 5
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 12); -- 스카 6
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (11, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 5); -- 스카 2
+INSERT IGNORE INTO `sub_photo` VALUES (12, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 6);
+INSERT IGNORE INTO `sub_photo` VALUES (13, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 6); -- 스카 3
+INSERT IGNORE INTO `sub_photo` VALUES (14, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 7); -- 스카 3
+INSERT IGNORE INTO `sub_photo` VALUES (15, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 8); -- 스카 4
+INSERT IGNORE INTO `sub_photo` VALUES (16, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 9); -- 스카 4
+INSERT IGNORE INTO `sub_photo` VALUES (17, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 10); -- 스카 5
+INSERT IGNORE INTO `sub_photo` VALUES (18, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 11); -- 스카 5
+INSERT IGNORE INTO `sub_photo` VALUES (19, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 12); -- 스카 6
+INSERT IGNORE INTO `sub_photo` VALUES (20, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (21, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (22, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (23, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 1, null, null);
 -- 스카 사진 2장인 경우
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 2, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (24, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 2, null, null);
 -- 스카 사진 3장인 경우
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 3, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 3, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (25, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 3, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (26, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 3, null, null);
 -- 스카 사진 4장인 경우
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 4, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 4, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 4, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (27, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 4, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (28, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 4, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (29, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 4, null, null);
 -- 스카 사진 5장인 경우
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (30, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (31, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (32, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (33, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 5, null, null);
 -- 스카 사진 7장인 경우
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
-INSERT IGNORE INTO `sub_photo` VALUES (1, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (34, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (35, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (36, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (37, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (38, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 6, null, null);
+
+INSERT IGNORE INTO `sub_photo` VALUES (39, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 13);
+INSERT IGNORE INTO `sub_photo` VALUES (40, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 14);
+INSERT IGNORE INTO `sub_photo` VALUES (41, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 15);
+INSERT IGNORE INTO `sub_photo` VALUES (42, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 16);
+INSERT IGNORE INTO `sub_photo` VALUES (43, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 17);
+INSERT IGNORE INTO `sub_photo` VALUES (44, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 18);
+INSERT IGNORE INTO `sub_photo` VALUES (45, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 19);
+INSERT IGNORE INTO `sub_photo` VALUES (46, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 20);
+INSERT IGNORE INTO `sub_photo` VALUES (47, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 21);
+INSERT IGNORE INTO `sub_photo` VALUES (48, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'ROOM', null, null, 22);
+INSERT IGNORE INTO `sub_photo` VALUES (49, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 7, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (50, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 8, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (51, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 9, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (52, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 10, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (53, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 11, null, null);
+INSERT IGNORE INTO `sub_photo` VALUES (54, 'https://studiousbucket.s3.ap-northeast-2.amazonaws.com/aced27ec-c691-426c-a02f-36a0fab429f4.png', 'STUDYCAFE', 12, null, null);
+
+
 
