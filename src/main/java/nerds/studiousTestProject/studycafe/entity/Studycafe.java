2399fda8253b69be810ea61e562d46af3abbc693
kmw2378 <rlarla677@gmail.com>
2023-08-21 21:50:34 +0900

    refactor: Member와 연관관계 에서 fetch 속성을 LAZY로 설정 Studycafe 조회 시 연관된 엔티티 조인 쿼리를 나가지 않게 하기 위해 fetch 속성을 LAZY로 수정하였습니다.
    Related to: #42

--- src/main/java/nerds/studiousTestProject/studycafe/entity/Studycafe.java
+++ src/main/java/nerds/studiousTestProject/studycafe/entity/Studycafe.java
@@ -2,10 +2,9 @@ package nerds.studiousTestProject.studycafe.entity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,7 +34,7 @@ public class Studycafe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
