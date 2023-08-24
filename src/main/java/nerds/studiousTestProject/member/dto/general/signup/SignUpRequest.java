7061461be806c7cdf37e7549f8abb9973990a98f
kmw2378 <rlarla677@gmail.com>
2023-08-24 22:45:41 +0900

    feat: @SignUpTypeCheck 어노테이션 추가 회원가입 시 소셜/일반 회원에 따라 필요한 값들을 검증하는 어노테이션을 추가하였습니다.
    Related to: #46

--- src/main/java/nerds/studiousTestProject/member/dto/general/signup/SignUpRequest.java
+++ src/main/java/nerds/studiousTestProject/member/dto/general/signup/SignUpRequest.java
@@ -6,12 +6,14 @@ import nerds.studiousTestProject.member.entity.member.Member;
 import nerds.studiousTestProject.member.entity.member.MemberRole;
 import nerds.studiousTestProject.member.entity.member.MemberType;
 import nerds.studiousTestProject.member.entity.member.Role;
+import nerds.studiousTestProject.member.validator.register.SignUpTypeCheck;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @Builder
+@SignUpTypeCheck
 public class SignUpRequest {
     private String email;
     private String password;
