d69b3de29383ffaa76767f2bd94010826498139b
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-25 00:26:30 +0900

    feat: 정적 메소드 추가
    페이지네이션 정보를 반환하는 dto에 정적 메소드를 추가했습니다.
    
    Related to: #43

--- src/main/java/nerds/studiousTestProject/review/dto/find/response/PageResponse.java
+++ src/main/java/nerds/studiousTestProject/review/dto/find/response/PageResponse.java
@@ -2,10 +2,19 @@ package nerds.studiousTestProject.review.dto.find.response;
 
 import lombok.Builder;
 import lombok.Data;
+import nerds.studiousTestProject.review.entity.Review;
+import org.springframework.data.domain.Page;
 
 @Builder
 @Data
 public class PageResponse {
     private Integer totalPage;
     private Integer currentPage;
+
+    public static PageResponse of(Page<Review> reviews) {
+        return PageResponse.builder()
+                .currentPage(reviews.getNumber())
+                .totalPage(reviews.getTotalPages())
+                .build();
+    }
 }
