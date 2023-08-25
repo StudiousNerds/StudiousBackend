1d0f0b68ddd14c20e2349971680b67b7472bcb86
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-25 22:52:40 +0900

    refactor: 로직 수정 및 리팩토링
    convenience에는 room의 편의시설이 들어가도록 수정,
    paidConveniences에는 룸과 스터디카페 중 유료인 것 들어가도록 수정했습니다.
    stream을 사용한 것은 모두 메서드 추출했습니다.
    
    Related to:

--- src/main/java/nerds/studiousTestProject/reservation/dto/reserve/response/ReserveResponse.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/reserve/response/ReserveResponse.java
@@ -3,6 +3,7 @@ package nerds.studiousTestProject.reservation.dto.reserve.response;
 import lombok.Builder;
 import lombok.Getter;
 import nerds.studiousTestProject.convenience.entity.Convenience;
+import nerds.studiousTestProject.convenience.entity.ConvenienceUsage;
 import nerds.studiousTestProject.member.entity.member.Member;
 import nerds.studiousTestProject.reservation.dto.RefundPolicyInResponse;
 import nerds.studiousTestProject.room.entity.Room;
@@ -25,29 +26,34 @@ public class ReserveResponse {
     private String userPhoneNumber;
     private List<RefundPolicyInResponse> refundPolicy;
 
-    public static ReserveResponse of(Member member, Room room, Studycafe studycafe) {
-        List<Convenience> convenienceLists = studycafe.getConveniences();
-        List<String> convenienceList = new ArrayList<>();
-        List<PaidConvenience> paidConvenienceList = new ArrayList<>();
-        for (Convenience convenience : convenienceLists) {
-            convenienceList.add(convenience.getName().name());
-            if (!convenience.isFree()) {
-                paidConvenienceList.add(PaidConvenience.from(convenience));
-            }
-        }
+    public static ReserveResponse of(Member member, Room room, Studycafe studycafe, List<Convenience> conveniences) {
         return ReserveResponse.builder()
-                .conveniences(convenienceList)
-                .paidConveniences(paidConvenienceList)
+                .conveniences(getRoomConvenienceList(conveniences))
+                .paidConveniences(getPaidConvenienceList(conveniences))
                 .cafeName(studycafe.getName())
                 .roomName(room.getName())
                 .studycafePhoto(studycafe.getPhoto())
                 .username(member.getName())
                 .userPhoneNumber(member.getPhoneNumber())
-                .refundPolicy(
-                        studycafe.getRefundPolicies().stream()
-                        .map(RefundPolicyInResponse::from)
-                        .collect(Collectors.toList()))
+                .refundPolicy(getRefundPolicyInResponses(studycafe))
                 .build();
+    }
+
+    private static List<RefundPolicyInResponse> getRefundPolicyInResponses(Studycafe studycafe) {
+        return studycafe.getRefundPolicies().stream()
+                .map(RefundPolicyInResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    private static List<PaidConvenience> getPaidConvenienceList(List<Convenience> conveniences) {
+        List<PaidConvenience> paidConvenienceList = conveniences.stream().filter(convenience -> !convenience.isFree()).map(PaidConvenience::from).toList();
+        return paidConvenienceList;
+    }
+
+    private static List<String> getRoomConvenienceList(List<Convenience> conveniences) {
+        List<String> roomConvenienceList = conveniences.stream().filter(convenience -> convenience.getUsage().equals(ConvenienceUsage.ROOM))
+                .map(convenience -> convenience.getName().name()).toList();
+        return roomConvenienceList;
     }
 
 
