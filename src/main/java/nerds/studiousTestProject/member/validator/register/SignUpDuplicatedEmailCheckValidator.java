7ff0b949bae7eae0d69cadb0fd399681c0f32fcb
kmw2378 <rlarla677@gmail.com>
2023-08-25 15:26:24 +0900

    feat: 회원가입 시 이메일 중복 검증기 추가 회원가입 시 요청데이터 이메일이 이미 존재하는지를 검증하는 검증기를 추가하였습니다.
    Related to: #46

---
+++ src/main/java/nerds/studiousTestProject/member/validator/register/SignUpDuplicatedEmailCheckValidator.java
@@ -0,0 +1,34 @@
+package nerds.studiousTestProject.member.validator.register;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import nerds.studiousTestProject.member.dto.general.signup.SignUpRequest;
+import nerds.studiousTestProject.member.entity.member.MemberType;
+import nerds.studiousTestProject.member.repository.MemberRepository;
+
+@RequiredArgsConstructor
+public class SignUpDuplicatedEmailCheckValidator implements ConstraintValidator<SignUpDuplicatedEmailCheck, SignUpRequest> {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void initialize(SignUpDuplicatedEmailCheck constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(SignUpRequest signUpRequest, ConstraintValidatorContext context) {
+        Long providerId = signUpRequest.getProviderId();
+        MemberType type = signUpRequest.getType();
+
+        if (providerId != null && memberRepository.existsByProviderIdAndType(providerId, type)) {
+            return false;
+        }
+
+        if (memberRepository.existsByEmailAndType(signUpRequest.getEmail(), type)) {
+            return false;
+        }
+
+        return true;
+    }
+}
