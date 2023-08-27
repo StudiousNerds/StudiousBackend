63bb082d20dc1dc0ac20381997f74c8d69c25993
rlfrkdms1 <rlfrkdms1@naver.com>
2023-08-27 23:49:25 +0900

    refactor: DTO에 valid 추가
    Related to:

--- src/main/java/nerds/studiousTestProject/payment/dto/request/request/PaymentRequest.java
+++ src/main/java/nerds/studiousTestProject/payment/dto/request/request/PaymentRequest.java
@@ -1,13 +1,23 @@
 package nerds.studiousTestProject.payment.dto.request.request;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class PaymentRequest {
 
+    @NotNull
     private ReserveUser user;
+
+    @NotNull
     private ReservationInfo reservation;
+
+    @Nullable
+    private List<PaidConvenience> paidConveniences;
 
 }
