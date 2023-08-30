a7ff3e3131e66ad18df690f30d50e7df6be60d09
kmw2378 <rlarla677@gmail.com>
2023-08-30 14:47:20 +0900

    refactor: DTO 검증 어노테이션 추가 필드 값 검증 어노테이션을 추가하였습니다.
    Related to: #49

--- src/main/java/nerds/studiousTestProject/member/dto/general/find/FindEmailRequest.java
+++ src/main/java/nerds/studiousTestProject/member/dto/general/find/FindEmailRequest.java
@@ -1,8 +1,10 @@
 package nerds.studiousTestProject.member.dto.general.find;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class FindEmailRequest {
+    @NotBlank(message = "전화번호는 공백일 수 없습니다.")
     private String phoneNumber;
 }
--- src/main/java/nerds/studiousTestProject/member/dto/general/find/FindPasswordRequest.java
+++ src/main/java/nerds/studiousTestProject/member/dto/general/find/FindPasswordRequest.java
@@ -1,9 +1,14 @@
 package nerds.studiousTestProject.member.dto.general.find;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class FindPasswordRequest {
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
     private String phoneNumber;
 }
