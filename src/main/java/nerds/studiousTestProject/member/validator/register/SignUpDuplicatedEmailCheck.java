37d0a6466e7ae3611814a45e9387c4fee50ed641
kmw2378 <rlarla677@gmail.com>
2023-09-03 19:28:49 +0900

    refactor: 대거 수정 예정인 Custom Validator 주석 처리 추후, 사용될 가능성이 있어 삭제하지 않고 일단 주석처리 하였습니다.
    Related to: #49

--- src/main/java/nerds/studiousTestProject/member/validator/register/SignUpDuplicatedEmailCheck.java
+++ src/main/java/nerds/studiousTestProject/member/validator/register/SignUpDuplicatedEmailCheck.java
@@ -1,18 +1,18 @@
-package nerds.studiousTestProject.member.validator.register;
-
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = SignUpDuplicatedEmailCheckValidator.class)
-public @interface SignUpDuplicatedEmailCheck {
-    String message() default "이미 존재하는 이메일입니다.";
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
-}
+//package nerds.studiousTestProject.member.validator.register;
+//
+//import jakarta.validation.Constraint;
+//import jakarta.validation.Payload;
+//
+//import java.lang.annotation.ElementType;
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.RetentionPolicy;
+//import java.lang.annotation.Target;
+//
+//@Target(ElementType.TYPE)
+//@Retention(RetentionPolicy.RUNTIME)
+//@Constraint(validatedBy = SignUpDuplicatedEmailCheckValidator.class)
+//public @interface SignUpDuplicatedEmailCheck {
+//    String message() default "이미 존재하는 이메일입니다.";
+//    Class<?>[] groups() default {};
+//    Class<? extends Payload>[] payload() default {};
+//}
