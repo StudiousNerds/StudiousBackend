f209f02c2cf8d9626339f9ef33eb387d9a6f777c
kmw2378 <rlarla677@gmail.com>
2023-09-03 23:52:34 +0900

    refactor: phoneNumber 필드 검증 메시지 수정 검증 메시지를 올바르게 수정하였습니다.
    Related to: #49

--- src/main/java/nerds/studiousTestProject/member/dto/find/FindPasswordRequest.java
+++ src/main/java/nerds/studiousTestProject/member/dto/find/FindPasswordRequest.java
@@ -9,6 +9,6 @@ public class FindPasswordRequest {
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 
-    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+    @NotBlank(message = "전화번호는 공백일 수 없습니다.")
     private String phoneNumber;
 }
