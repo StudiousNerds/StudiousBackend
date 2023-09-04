5962a8a7998ff00adb29919e02ec2f987b73d162
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-04 22:37:41 +0900

    feat: 예약 내역 페이지네이션 정보 추가
    원래 정적메소드로 review 내역을 받아서 하는게 있었는데 지금 필요한게 예약 내역들을 받아서 그를 토대로 페이지네이션을 하다보니 한 클래스안에서 이름이 겹치게 되었습니다..
    그래서 이름을 우선 저렇게 만들어보았습니다.. 혹시 더 좋은 방법이 있으시면 알려주시면 감사하겠습니다..!
    
    Related to: #54

--- src/main/java/nerds/studiousTestProject/review/dto/find/response/PageInfo.java
+++ src/main/java/nerds/studiousTestProject/review/dto/find/response/PageInfo.java
@@ -2,6 +2,7 @@ package nerds.studiousTestProject.review.dto.find.response;
 
 import lombok.Builder;
 import lombok.Data;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import nerds.studiousTestProject.review.entity.Review;
 import org.springframework.data.domain.Page;
 
@@ -15,6 +16,13 @@ public class PageInfo {
         return PageInfo.builder()
                 .currentPage(reviews.getNumber() + 1)
                 .totalPage(reviews.getTotalPages())
+                .build();
+    }
+
+    public static PageInfo getPageInfo(Page<ReservationRecord> reservationRecords) {
+        return PageInfo.builder()
+                .currentPage(reservationRecords.getNumber() + 1)
+                .totalPage(reservationRecords.getTotalPages())
                 .build();
     }
 }
