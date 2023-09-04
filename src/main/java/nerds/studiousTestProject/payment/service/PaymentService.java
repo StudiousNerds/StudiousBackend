b379900d340e9dcf384e5123cacc32396f0c63d0
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-04 18:18:37 +0900

    refactor: 코드 리팩토링
    1. 접근 제어자 변경
    2. 메서드 명 변경
    3. 누락된 어노테이션 추가
    4. 메서드 추출
    
    Related to:

--- src/main/java/nerds/studiousTestProject/payment/service/PaymentService.java
+++ src/main/java/nerds/studiousTestProject/payment/service/PaymentService.java
@@ -9,23 +9,17 @@ import nerds.studiousTestProject.payment.util.totoss.CancelRequest;
 import nerds.studiousTestProject.payment.dto.cancel.response.CancelResponse;
 import nerds.studiousTestProject.payment.dto.confirm.response.ConfirmFailResponse;
 import nerds.studiousTestProject.payment.dto.confirm.response.ConfirmSuccessResponse;
-import nerds.studiousTestProject.payment.dto.confirm.response.ReservationInfo;
-import nerds.studiousTestProject.payment.dto.confirm.response.ReserveUserInfo;
-import nerds.studiousTestProject.payment.dto.request.request.PaymentRequest;
-import nerds.studiousTestProject.payment.dto.request.response.PaymentResponse;
 import nerds.studiousTestProject.payment.entity.Payment;
 import nerds.studiousTestProject.payment.repository.PaymentRepository;
 import nerds.studiousTestProject.payment.util.PaymentGenerator;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
-import nerds.studiousTestProject.reservation.service.ReservationRecordService;
+import nerds.studiousTestProject.reservation.repository.ReservationRecordRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
 import java.util.List;
-
 import static nerds.studiousTestProject.common.exception.ErrorCode.NOT_FOUND_PAYMENT;
+import static nerds.studiousTestProject.common.exception.ErrorCode.NOT_FOUND_RESERVATION_RECORD;
 
 
 @RequiredArgsConstructor
@@ -34,41 +28,26 @@ import static nerds.studiousTestProject.common.exception.ErrorCode.NOT_FOUND_PAY
 @Transactional(readOnly = true)
 public class PaymentService {
 
-    private final ReservationRecordService reservationRecordService;
     private final PaymentRepository paymentRepository;
     private final PaymentGenerator paymentGenerator;
+    private final ReservationRecordRepository reservationRecordRepository;
 
-    private static final String REQUEST_SUCCESS_URI = "http://localhost:8080/studious/payments/success";
-    private static final String REQUEST_FAIL_URI = "http://localhost:8080/studious/payments/fail";
     private static final String CONFIRM_URI = "https://api.tosspayments.com/v1/payments/confirm";
     private static final String CANCEL_URI = "https://api.tosspayments.com/v1/payments/%s/cancel";
 
-    public PaymentResponse createPaymentResponse(PaymentRequest paymentRequest, String orderId) {
-        return PaymentResponse.builder()
-                .amount(paymentRequest.getReservation().getPrice())
-                .orderId(orderId)
-                .orderName(paymentRequest.getUser().getName())
-                .successUrl(REQUEST_SUCCESS_URI)
-                .failUrl(REQUEST_FAIL_URI)
-                .build();
-    }
 
     @Transactional
     public ConfirmSuccessResponse confirmPayToToss(String orderId, String paymentKey, Integer amount) {
-        ConfirmSuccessRequest request = ConfirmSuccessRequest.of(orderId,amount,paymentKey);
-        PaymentResponseFromToss responseFromToss = paymentGenerator.requestToToss(request, CONFIRM_URI);
+        PaymentResponseFromToss responseFromToss = paymentGenerator.requestToToss(ConfirmSuccessRequest.of(orderId,amount,paymentKey), CONFIRM_URI);
         Payment payment = paymentRepository.save(responseFromToss.toPayment());
         log.info("success payment ! payment status is {} and method is {}", responseFromToss.getStatus(), responseFromToss.getMethod());
-        reservationRecordService.findByOrderId(orderId).completePay(payment);//결제 완료로 상태 변경
-        return createPaymentConfirmResponse(responseFromToss);
+        ReservationRecord reservationRecord = findReservationRecordByOrderId(orderId);
+        reservationRecord.completePay(payment);//결제 완료로 상태 변경
+        return ConfirmSuccessResponse.from(reservationRecord);
     }
 
-    public ConfirmSuccessResponse createPaymentConfirmResponse(PaymentResponseFromToss responseFromToss){
-        ReservationRecord reservationRecord = reservationRecordService.findByOrderId(responseFromToss.getOrderId());
-        return ConfirmSuccessResponse.builder()
-                .reservationInfo(ReservationInfo.of(reservationRecord))
-                .reserveUserInfo(ReserveUserInfo.of(reservationRecord))
-                .build();
+    private ReservationRecord findReservationRecordByOrderId(String orderId) {
+        return reservationRecordRepository.findByOrderId(orderId).orElseThrow(() -> new NotFoundException(NOT_FOUND_RESERVATION_RECORD));
     }
 
     /*
@@ -76,33 +55,37 @@ public class PaymentService {
      */
     @Transactional
     public ConfirmFailResponse confirmFail(String message, String orderId){
-        reservationRecordService.deleteByOrderId(orderId);
+        reservationRecordRepository.delete(findReservationRecordByOrderId(orderId));
         return ConfirmFailResponse.builder()
                 .message(message)
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .build();
     }
 
-    public void cancel(CancelRequest cancelRequest, Long reservationId){
-        ReservationRecord reservationRecord = reservationRecordService.findById(reservationId);
-        requestCancelToToss(cancelRequest, reservationRecord.getPayment().getPaymentKey());
-        reservationRecordService.cancel(reservationId); //결제 취소 상태로 변경
-    }
     @Transactional
-    public List<CancelResponse> requestCancelToToss(CancelRequest cancelRequest, String paymentKey){
+    public void cancel(CancelRequest cancelRequest, Long reservationId){
+        ReservationRecord reservationRecord = findReservationById(reservationId);
+        requestCancelToToss(cancelRequest, reservationRecord.getPayment().getPaymentKey());
+        reservationRecord.canceled(); //결제 취소 상태로 변경
+    }
+
+    private List<CancelResponse> requestCancelToToss(CancelRequest cancelRequest, String paymentKey){
         PaymentResponseFromToss responseFromToss = paymentGenerator.requestToToss(cancelRequest, String.format(CANCEL_URI, paymentKey));
-        List<CancelResponse> cancelResponses = new ArrayList<>();
-        responseFromToss.getCancels().stream().forEach(cancel -> cancelResponses.add(CancelResponse.of(cancel)));
-        cancelPaymentByCancel(responseFromToss);
+        List<CancelResponse> cancelResponses = responseFromToss.getCancels().stream().map(CancelResponse::of).toList();
+        cancelPayment(responseFromToss);
         return cancelResponses;
     }
 
-    private void cancelPaymentByCancel(PaymentResponseFromToss responseFromToss) {
+    private void cancelPayment(PaymentResponseFromToss responseFromToss) {
         Payment payment = paymentRepository.findByPaymentKeyAndOrderId(
                         responseFromToss.getPaymentKey(),
                         responseFromToss.getOrderId())
                 .orElseThrow(() -> new NotFoundException(NOT_FOUND_PAYMENT));
         payment.canceled(responseFromToss);
+    }
+
+    private ReservationRecord findReservationById(Long reservationId) {
+        return reservationRecordRepository.findById(reservationId).orElseThrow(()-> new NotFoundException(NOT_FOUND_RESERVATION_RECORD));
     }
 
 }
