488f1fde7e7700275f369a2e666b54d71146d0d8
kmw2378 <rlarla677@gmail.com>
2023-09-05 23:59:46 +0900

    refactor: V1_init.sql 컬럼 변경사항 반영 password not null 조건 해제, review 테이블에 grade_id 컬럼을 추가하였습니다.
    Related to: #53

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -25,7 +25,6 @@ CREATE TABLE IF NOT EXISTS `grade` (
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE IF NOT EXISTS `member` (
     `id`            bigint       NOT NULL AUTO_INCREMENT,
     `resigned_date` DATE,
