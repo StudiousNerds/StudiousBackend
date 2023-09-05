b4a0f0dd6c57ee70500e627e7a903a0ea78a98e5
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-06 00:19:49 +0900

    refactor: 필요성이 없어져서 삭제
    Related to: #50

--- src/main/java/nerds/studiousTestProject/convenience/service/ConvenienceService.java
+++ src/main/java/nerds/studiousTestProject/convenience/service/ConvenienceService.java
@@ -2,12 +2,9 @@ package nerds.studiousTestProject.convenience.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import nerds.studiousTestProject.convenience.entity.Convenience;
 import nerds.studiousTestProject.convenience.repository.ConvenienceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -19,10 +16,5 @@ public class ConvenienceService {
     @Transactional
     public void deleteRoomConveniences(Long roomId) {
         convenienceRepository.deleteAllByRoomId(roomId);
-    }
-
-    @Transactional
-    public void saveRoomConveniences(List<Convenience> convenienceList) {
-        convenienceRepository.saveAll(convenienceList);
     }
 }
