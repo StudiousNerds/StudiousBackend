6bbb83fdbcfcb9ceb297b2f419668b3c7827f310
kmw2378 <rlarla677@gmail.com>
2023-09-05 23:19:17 +0900

    feat: studycafeId 에 해당하는 모든 Review 엔티티를 페이징 처리하여 가져오는 메소드 추가 ReservationRecord, Room 과 fetch join을 @Query를 사용하여 구현하였습니다.
    Related to: #53

--- src/main/java/nerds/studiousTestProject/review/repository/ReviewRepository.java
+++ src/main/java/nerds/studiousTestProject/review/repository/ReviewRepository.java
@@ -1,9 +1,11 @@
 package nerds.studiousTestProject.review.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import nerds.studiousTestProject.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,4 +14,14 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByIdInOrderByCreatedDateDesc(List<Long> reviewIds);
 
     Page<Review> findAllByIdIn(List<Long> reviewIds, Pageable pageable);
+
+    @Query(
+            value = "select r from Review r " +
+                    "join fetch ReservationRecord r1 on r1.review.id = r.id " +
+                    "join fetch Room r2 on r2.id = r1.room.id and r2.studycafe.id = :studycafeId",
+            countQuery = "select count(r) from Review r " +
+                    "join ReservationRecord r1 on r1.review.id = r.id " +
+                    "join Room r2 on r2.id = r1.room.id and r2.studycafe.id = :studycafeId"
+    )
+    Page<Review> findAllByStudycafeId(@Param("studycafeId") Long studycafeId, Pageable pageable);
 }
