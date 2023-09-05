de02f25abab3a32a2bc7c8bcdf13242c46158ddf
kmw2378 <rlarla677@gmail.com>
2023-09-05 23:24:17 +0900

    feat: 리뷰 및 댓글 관리 관련 Service 클래스 추가 리뷰 및 댓글 관리 관련 로직을 담당하는 Service 클래스를 추가하였습니다.
    Related to: #53

---
+++ src/main/java/nerds/studiousTestProject/review/service/AdminReviewService.java
@@ -0,0 +1,62 @@
+package nerds.studiousTestProject.review.service;
+
+import lombok.RequiredArgsConstructor;
+import nerds.studiousTestProject.common.exception.NotFoundException;
+import nerds.studiousTestProject.common.service.TokenService;
+import nerds.studiousTestProject.member.entity.member.Member;
+import nerds.studiousTestProject.photo.repository.SubPhotoRepository;
+import nerds.studiousTestProject.review.dto.manage.inquire.ReviewInfoResponse;
+import nerds.studiousTestProject.review.dto.manage.modify.ModifyCommentRequest;
+import nerds.studiousTestProject.review.dto.manage.register.RegisterCommentRequest;
+import nerds.studiousTestProject.review.entity.Review;
+import nerds.studiousTestProject.review.repository.ReviewRepository;
+import nerds.studiousTestProject.studycafe.repository.StudycafeRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static nerds.studiousTestProject.common.exception.ErrorCode.MISMATCH_MEMBER_AND_STUDYCAFE;
+import static nerds.studiousTestProject.common.exception.ErrorCode.NOT_FOUND_REVIEW;
+
+@RequiredArgsConstructor
+@Service
+public class AdminReviewService {
+    private final ReviewRepository reviewRepository;
+    private final StudycafeRepository studycafeRepository;
+    private final SubPhotoRepository subPhotoRepository;
+    private final TokenService tokenService;
+
+    public List<ReviewInfoResponse> getWrittenReviews(Long studycafeId, String accessToken, Pageable pageable) {
+        Member member = tokenService.getMemberFromAccessToken(accessToken);
+        if (!matchStudycafeAndMember(studycafeId, member)) {
+            throw new NotFoundException(MISMATCH_MEMBER_AND_STUDYCAFE);
+        }
+
+        List<Review> reviews = reviewRepository.findAllByStudycafeId(studycafeId, pageable).getContent();
+        return reviews.stream().map(r -> ReviewInfoResponse.from(r, subPhotoRepository.findAllByReviewId(r.getId()))).toList();
+    }
+
+    private boolean matchStudycafeAndMember(Long studycafeId, Member member) {
+        return studycafeRepository.existsByIdAndMember(studycafeId, member);
+    }
+
+    @Transactional
+    public void registerComment(Long reviewId, RegisterCommentRequest registerCommentRequest) {
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new NotFoundException(NOT_FOUND_REVIEW));
+        review.updateComment(registerCommentRequest.getComment());
+    }
+
+    @Transactional
+    public void modifyComment(Long reviewId, ModifyCommentRequest modifyCommentRequest) {
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new NotFoundException(NOT_FOUND_REVIEW));
+        review.updateComment(modifyCommentRequest.getComment());
+    }
+
+    @Transactional
+    public void deleteComment(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new NotFoundException(NOT_FOUND_REVIEW));
+        review.deleteComment();
+    }
+}
