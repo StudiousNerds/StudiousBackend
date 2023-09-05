5a10debc83396dbee449f53e688a114de8ddec51
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-05 23:50:10 +0900

    feat: 편의시설 수정 요청 정보를 담아오는 방식 변경
    편의시설 이름과 가격만 넘어올 것이라고 생각하여 수정했습니다.
    그리고 밑에 엔티티는 받아온 정보를 기반으로 유료와 무료를 구별합니다.
    
    Related to: #50

---
+++ src/main/java/nerds/studiousTestProject/room/dto/modify/request/ModifyConvenienceRequest.java
@@ -0,0 +1,35 @@
+package nerds.studiousTestProject.room.dto.modify.request;
+
+import jakarta.validation.constraints.Min;
+import lombok.Builder;
+import lombok.Data;
+import nerds.studiousTestProject.convenience.entity.Convenience;
+import nerds.studiousTestProject.convenience.entity.ConvenienceName;
+import nerds.studiousTestProject.room.entity.Room;
+
+@Builder
+@Data
+public class ModifyConvenienceRequest {
+    private ConvenienceName name;
+
+    @Min(value = 0, message = "가격은 0 이상입니다.")
+    private Integer price;
+
+    public Convenience toEntity() {
+        if (price == 0) {
+            return Convenience.builder()
+                    .name(name)
+                    .price(price)
+                    .isFree(price == 0)
+                    .build();
+        }
+        if (price != 0) {
+            return Convenience.builder()
+                    .name(name)
+                    .price(price)
+                    .isFree(price != 0)
+                    .build();
+        }
+        return null;
+    }
+}
--- src/main/java/nerds/studiousTestProject/room/dto/modify/request/ModifyRoomRequest.java
+++ src/main/java/nerds/studiousTestProject/room/dto/modify/request/ModifyRoomRequest.java
@@ -2,7 +2,6 @@ package nerds.studiousTestProject.room.dto.modify.request;
 
 import lombok.Builder;
 import lombok.Data;
-import nerds.studiousTestProject.reservation.dto.reserve.response.PaidConvenience;
 import nerds.studiousTestProject.room.entity.PriceType;
 import nerds.studiousTestProject.room.entity.Room;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
@@ -18,8 +17,7 @@ public class ModifyRoomRequest {
     private Integer price;
     private String type;
     private Integer minUsingTime;
-    private List<String> conveniences;
-    private List<PaidConvenience> paidConveniences;
+    private List<ModifyConvenienceRequest> conveniences;
 
     public static Room toEntity(Studycafe studycafe, Long roomId, ModifyRoomRequest modifyRoomRequest) {
         return Room.builder()
