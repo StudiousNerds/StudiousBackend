3f63f28c93416adc223f26ae5f5754fdb7ec89d7
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-05 22:48:55 +0900

    refactor: 페이지네이션 정보 반환 방식 변경
    pageInfo로 따로 빼지 않는 방향으로 수정했습니다!
    
    Related to: #54

--- src/main/java/nerds/studiousTestProject/review/dto/available/response/AvailableReviewResponse.java
+++ src/main/java/nerds/studiousTestProject/review/dto/available/response/AvailableReviewResponse.java
@@ -2,13 +2,13 @@ package nerds.studiousTestProject.review.dto.available.response;
 
 import lombok.Builder;
 import lombok.Data;
-import nerds.studiousTestProject.review.dto.find.response.PageInfo;
 
 import java.util.List;
 
 @Builder
 @Data
 public class AvailableReviewResponse {
-    private PageInfo pageInfo;
+    private Integer totalPage;
+    private Integer currentPage;
     private List<AvailableReviewInfo> availableReviewInfo;
 }
--- src/main/java/nerds/studiousTestProject/review/dto/written/response/WrittenReviewResponse.java
+++ src/main/java/nerds/studiousTestProject/review/dto/written/response/WrittenReviewResponse.java
@@ -2,13 +2,13 @@ package nerds.studiousTestProject.review.dto.written.response;
 
 import lombok.Builder;
 import lombok.Data;
-import nerds.studiousTestProject.review.dto.find.response.PageInfo;
 
 import java.util.List;
 
 @Builder
 @Data
 public class WrittenReviewResponse {
-    private PageInfo pageInfo;
+    private Integer totalPage;
+    private Integer currentPage;
     private List<WrittenReviewInfo> writtenReviewInfos;
 }
