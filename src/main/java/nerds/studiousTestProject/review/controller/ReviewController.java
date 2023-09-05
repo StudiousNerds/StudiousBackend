1b003e522de14f95678e01b0030153dba4e90a5a
kmw2378 <rlarla677@gmail.com>
2023-09-05 23:23:28 +0900

    feat: 리뷰 및 댓글 관리 관련 uri 매핑 메소드 추가 ReviewController에 리뷰 및 댓글 관련 uri를 매핑하는 메소드를 추가하였습니다.
    Related to: #53

--- src/main/java/nerds/studiousTestProject/review/controller/ReviewController.java
+++ src/main/java/nerds/studiousTestProject/review/controller/ReviewController.java
@@ -3,6 +3,10 @@ package nerds.studiousTestProject.review.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nerds.studiousTestProject.member.entity.member.MemberRole;
+import nerds.studiousTestProject.review.dto.manage.inquire.ReviewInfoResponse;
+import nerds.studiousTestProject.review.dto.manage.modify.ModifyCommentRequest;
+import nerds.studiousTestProject.review.dto.manage.register.RegisterCommentRequest;
 import nerds.studiousTestProject.review.dto.modify.request.ModifyReviewRequest;
 import nerds.studiousTestProject.review.dto.register.request.RegisterReviewRequest;
 import nerds.studiousTestProject.review.dto.available.response.AvailableReviewResponse;
@@ -11,15 +15,19 @@ import nerds.studiousTestProject.review.dto.find.response.FindReviewSortedRespon
 import nerds.studiousTestProject.review.dto.modify.response.ModifyReviewResponse;
 import nerds.studiousTestProject.review.dto.register.response.RegisterReviewResponse;
 import nerds.studiousTestProject.review.dto.written.response.WrittenReviewResponse;
+import nerds.studiousTestProject.review.service.AdminReviewService;
 import nerds.studiousTestProject.review.service.ReviewService;
+import nerds.studiousTestProject.studycafe.util.PageRequestConverter;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +45,7 @@ import java.util.List;
 @Validated
 public class ReviewController {
     private final ReviewService reviewService;
+    private final AdminReviewService adminReviewService;
 
     @PostMapping("/reviews")
     public RegisterReviewResponse registerReview(@RequestPart("registerReviewRequest") @Valid RegisterReviewRequest registerReviewRequest,
@@ -75,5 +84,29 @@ public class ReviewController {
     @GetMapping("/studycafes/{studycafeId}/rooms/{roomId}/reviews")
     public FindReviewSortedResponse findRoomReviews(@PathVariable("studycafeId") Long studycafeId, @PathVariable("roomId") Long roomId, Pageable pageable) {
         return reviewService.findRoomReviews(studycafeId, roomId, pageable);
+    }
+
+    @GetMapping("/studycafes/{studycafeId}/reviews/managements")
+    @Secured(value = MemberRole.ROLES.ADMIN)
+    public List<ReviewInfoResponse> inquireStudycafeReviews(@PathVariable("studycafeId") Long studycafeId, @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @RequestParam Integer page) {
+        return adminReviewService.getWrittenReviews(studycafeId, accessToken, PageRequestConverter.of(page, 3));
+    }
+
+    @PostMapping("/reviews/managements/{reviewId}")
+    @Secured(value = MemberRole.ROLES.ADMIN)
+    public void registerComment(@PathVariable("reviewId") Long reviewId, @RequestBody @Valid RegisterCommentRequest registerCommentRequest) {
+        adminReviewService.registerComment(reviewId, registerCommentRequest);
+    }
+
+    @PatchMapping("/reviews/managements/{reviewId}")
+    @Secured(value = MemberRole.ROLES.ADMIN)
+    public void modifyComment(@PathVariable("reviewId") Long reviewId, @RequestBody @Valid ModifyCommentRequest modifyCommentRequest) {
+        adminReviewService.modifyComment(reviewId, modifyCommentRequest);
+    }
+
+    @DeleteMapping("/reviews/managements/{reviewId}")
+    @Secured(value = MemberRole.ROLES.ADMIN)
+    public void deleteComment(@PathVariable("reviewId") Long reviewId) {
+        adminReviewService.deleteComment(reviewId);
     }
 }
