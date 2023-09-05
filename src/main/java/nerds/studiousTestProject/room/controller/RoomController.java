242436ce9258e230ee7c2dfba3b41d207b523cdd
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-06 00:21:41 +0900

    feat: 사장님 스터디룸 삭제 컨트롤러 구현
    - 삭제 컨트롤러 구현
    - `PathVariable`을 사용해야 하는데 `RequestParam`으로 잘못 사용해서 전반적 수정
    
    Related to: #50

--- src/main/java/nerds/studiousTestProject/room/controller/RoomController.java
+++ src/main/java/nerds/studiousTestProject/room/controller/RoomController.java
@@ -7,10 +7,12 @@ import nerds.studiousTestProject.room.dto.find.response.FindAllRoomResponse;
 import nerds.studiousTestProject.room.dto.modify.request.ModifyRoomRequest;
 import nerds.studiousTestProject.room.dto.modify.response.ModifyRoomResponse;
 import nerds.studiousTestProject.room.service.RoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,14 +28,20 @@ public class RoomController {
     private final RoomService roomService;
 
     @GetMapping("/studycafes/managements/{studycafeId}/rooms/{roomId}")
-    public FindAllRoomResponse inquireModifyRoom(@RequestParam Long studycafeId, @RequestParam Long roomId) {
+    public FindAllRoomResponse inquireModifyRoom(@PathVariable Long studycafeId, @PathVariable Long roomId) {
         return roomService.getAllRooms(studycafeId, roomId);
     }
 
     @PatchMapping("/studycafes/managements/{studycafeId}/rooms/{roomId}")
-    public ModifyRoomResponse modifyRoom(@RequestParam Long studycafeId, @RequestParam Long roomId,
+    public ModifyRoomResponse modifyRoom(@PathVariable Long studycafeId, @PathVariable Long roomId,
                                          @RequestPart("modifyRoomReuqest") @Valid ModifyRoomRequest modifyRoomRequest,
                                          @RequestPart(value = "file", required = false) List<MultipartFile> files) {
         return roomService.modifyRoom(studycafeId, roomId, modifyRoomRequest, files);
+    }
+
+    @DeleteMapping("/studycafes/managements/{studycafeId}/rooms/{roomId}")
+    public ResponseEntity<Void> deleteRoom(@PathVariable Long studycafeId, @PathVariable Long roomId) {
+        roomService.deleteRoom(roomId);
+        return ResponseEntity.noContent().build();
     }
 }
