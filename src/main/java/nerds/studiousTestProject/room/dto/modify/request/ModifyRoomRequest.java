4198f7e85c7c0707e470b94c4f27e91805d5237b
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-05 22:28:49 +0900

    feat: 필드값 수정 및 toEntity 추가
    - 수정을 위해 필요한 toEntity 추가
    - 인원수 필드명 수정
    - 사진은 MultipartFile로 받아오기 위해 DTO에서 삭제
    
    Related to: #50

--- src/main/java/nerds/studiousTestProject/room/dto/modify/request/ModifyRoomRequest.java
+++ src/main/java/nerds/studiousTestProject/room/dto/modify/request/ModifyRoomRequest.java
@@ -3,20 +3,34 @@ package nerds.studiousTestProject.room.dto.modify.request;
 import lombok.Builder;
 import lombok.Data;
 import nerds.studiousTestProject.reservation.dto.reserve.response.PaidConvenience;
+import nerds.studiousTestProject.room.entity.PriceType;
+import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
 
 import java.util.List;
 
 @Builder
 @Data
 public class ModifyRoomRequest {
-    private Long roomId;
     private String roomName;
-    private Integer minCount;
-    private Integer maxCount;
+    private Integer minHeadCount;
+    private Integer maxHeadCount;
     private Integer price;
     private String type;
     private Integer minUsingTime;
-    private List<String> photos;
     private List<String> conveniences;
     private List<PaidConvenience> paidConveniences;
+
+    public static Room toEntity(Studycafe studycafe, Long roomId, ModifyRoomRequest modifyRoomRequest) {
+        return Room.builder()
+                .id(roomId)
+                .name(modifyRoomRequest.getRoomName())
+                .minHeadCount(modifyRoomRequest.getMinHeadCount())
+                .maxHeadCount(modifyRoomRequest.getMaxHeadCount())
+                .price(modifyRoomRequest.getPrice())
+                .type(PriceType.valueOf(modifyRoomRequest.getType()))
+                .minUsingTime(modifyRoomRequest.getMinUsingTime())
+                .studycafe(studycafe)
+                .build();
+    }
 }
