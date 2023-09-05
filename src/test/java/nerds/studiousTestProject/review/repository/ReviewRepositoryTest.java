4aef8f6a858ae9aa1d52f810ae55a48a20aef91c
kmw2378 <rlarla677@gmail.com>
2023-09-05 23:29:29 +0900

    test: 주석 수정 및 reviewRepository의 findAllByStudycafeId 테스트 코드 추가
    Related to: #53

--- src/test/java/nerds/studiousTestProject/review/repository/ReviewRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/review/repository/ReviewRepositoryTest.java
@@ -1,38 +1,44 @@
-//package nerds.studiousTestProject.review.repository;
-//import nerds.studiousTestProject.support.RepositoryTest;
-//import nerds.studiousTestProject.hashtag.entity.HashtagRecord;
-//import nerds.studiousTestProject.hashtag.repository.HashtagRecordRepository;
-//import nerds.studiousTestProject.reservation.repository.ReservationRecordRepository;
-//import nerds.studiousTestProject.review.entity.Grade;
-//import nerds.studiousTestProject.review.entity.Review;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Sort;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import static nerds.studiousTestProject.support.fixture.HashtagFixture.*;
-//import static nerds.studiousTestProject.support.fixture.ReservationRecordFixture.*;
-//import static nerds.studiousTestProject.support.fixture.ReviewFixture.*;
-//
-//@RepositoryTest
-//class ReviewRepositoryTest {
-//
-//    @Autowired
-//    private ReviewRepository reviewRepository;
-//    @Autowired
-//    private ReservationRecordRepository reservationRecordRepository;
-//    @Autowired
-//    private GradeRepository gradeRepository;
-//    @Autowired
-//    private HashtagRecordRepository hashtagRecordRepository;
-//
+package nerds.studiousTestProject.review.repository;
+import nerds.studiousTestProject.member.entity.member.Member;
+import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
+import nerds.studiousTestProject.support.RepositoryTest;
+import nerds.studiousTestProject.hashtag.repository.HashtagRecordRepository;
+import nerds.studiousTestProject.reservation.repository.ReservationRecordRepository;
+import nerds.studiousTestProject.review.entity.Review;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static nerds.studiousTestProject.support.EntitySaveProvider.룸_저장;
+import static nerds.studiousTestProject.support.EntitySaveProvider.리뷰_저장;
+import static nerds.studiousTestProject.support.EntitySaveProvider.스터디카페_저장;
+import static nerds.studiousTestProject.support.EntitySaveProvider.예약_정보_저장;
+import static nerds.studiousTestProject.support.EntitySaveProvider.회원_저장;
+import static nerds.studiousTestProject.support.fixture.MemberFixture.*;
+import static nerds.studiousTestProject.support.fixture.ReservationRecordFixture.*;
+import static nerds.studiousTestProject.support.fixture.ReviewFixture.*;
+import static nerds.studiousTestProject.support.fixture.RoleFixture.*;
+import static nerds.studiousTestProject.support.fixture.RoomFixture.*;
+import static nerds.studiousTestProject.support.fixture.StudycafeFixture.*;
+
+@RepositoryTest
+class ReviewRepositoryTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private ReservationRecordRepository reservationRecordRepository;
+    @Autowired
+    private GradeRepository gradeRepository;
+    @Autowired
+    private HashtagRecordRepository hashtagRecordRepository;
+
 //    @Test
 //    @DisplayName("예약 id가 주어졌을 때 생성날짜를 기준으로 내림차순 정렬하여 그에 해당하는 리뷰들을 모두 다 가져오는지 테스트")
 //    void findAllByReservationRecordIdInOrderByCreatedDatedDesc() {
@@ -108,4 +114,72 @@
 //        Assertions.assertThat(gradeList.size()).isEqualTo(1);
 //        Assertions.assertThat(hashtagRecordList.size()).isEqualTo(1);
 //    }
-//}
+
+    @Test
+    @DisplayName("특정 스터디카페의 모든 리뷰를 조회하는지 테스트")
+    public void 특정_스터디카페_모든_리뷰_조회() throws Exception {
+
+        // given
+        Member admin = 회원_저장(BEAVER.생성());
+        ADMIN.멤버_생성(admin);
+
+        Member member = 회원_저장(BEAVER.생성());
+        USER.멤버_생성(member);
+
+        Studycafe studycafe1 = 스터디카페_저장(NERDS.멤버_생성(admin));
+        Room room1 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe1));
+        Room room2 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe1));
+        Room room3 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe1));
+
+        LocalDate currentReserveDate1 = LocalDate.of(2023, 7, 1);
+        LocalTime startTime1 = LocalTime.of(17, 0);
+        LocalTime endTime1 = LocalTime.of(19, 0);
+
+        for (long i = 0L; i < 31L; i++) {
+            Review review = 리뷰_저장(FIRST_REVIEW.평점_정보_생성(1, 1, 1, 1.0));
+            예약_정보_저장(CONFIRM_RESERVATION.예약_내역_생성(currentReserveDate1.plusDays(i), startTime1, endTime1, member, room1, review));
+        }
+
+        for (long i = 0L; i < 31L; i++) {
+            Review review = 리뷰_저장(FIRST_REVIEW.평점_정보_생성(1, 1, 1, 1.0));
+            예약_정보_저장(CONFIRM_RESERVATION.예약_내역_생성(currentReserveDate1.plusDays(i), startTime1, endTime1, member, room2, review));
+        }
+
+        for (long i = 0L; i < 31L; i++) {
+            Review review = 리뷰_저장(FIRST_REVIEW.평점_정보_생성(1, 1, 1, 1.0));
+            예약_정보_저장(CONFIRM_RESERVATION.예약_내역_생성(currentReserveDate1.plusDays(i), startTime1, endTime1, member, room3, review));
+        }
+
+        Studycafe studycafe2 = 스터디카페_저장(NERDS.멤버_생성(admin));
+        Room room4 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe2));
+        Room room5 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe2));
+        Room room6 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe2));
+
+        LocalDate currentReserveDate2 = LocalDate.of(2023, 6, 1);
+        LocalTime startTime2 = LocalTime.of(17, 0);
+        LocalTime endTime2 = LocalTime.of(19, 0);
+
+        for (long i = 0L; i < 30L; i++) {
+            Review review = 리뷰_저장(FIRST_REVIEW.평점_정보_생성(1, 1, 1, 1.0));
+            예약_정보_저장(CONFIRM_RESERVATION.예약_내역_생성(currentReserveDate2.plusDays(i), startTime2, endTime2, member, room4, review));
+        }
+
+        for (long i = 0L; i < 30L; i++) {
+            Review review = 리뷰_저장(FIRST_REVIEW.평점_정보_생성(1, 1, 1, 1.0));
+            예약_정보_저장(CONFIRM_RESERVATION.예약_내역_생성(currentReserveDate2.plusDays(i), startTime2, endTime2, member, room5, review));
+        }
+
+        for (long i = 0L; i < 30L; i++) {
+            Review review = 리뷰_저장(FIRST_REVIEW.평점_정보_생성(1, 1, 1, 1.0));
+            예약_정보_저장(CONFIRM_RESERVATION.예약_내역_생성(currentReserveDate2.plusDays(i), startTime2, endTime2, member, room6, review));
+        }
+
+        // when
+        List<Review> reviews1 = reviewRepository.findAllByStudycafeId(studycafe1.getId(), null).getContent();
+        List<Review> reviews2 = reviewRepository.findAllByStudycafeId(studycafe2.getId(), null).getContent();
+
+        // then
+        Assertions.assertThat(reviews1.size()).isEqualTo(93);
+        Assertions.assertThat(reviews2.size()).isEqualTo(90);
+    }
+}
