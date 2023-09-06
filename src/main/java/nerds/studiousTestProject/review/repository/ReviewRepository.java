a346dedd127a58aa4e8f8e36ec846cc98d3d98a8
kmw2378 <rlarla677@gmail.com>
2023-09-06 23:08:40 +0900

    feat: QueryDSL이 사용된 ReviewRepositoryCustom 인터페이스 상속 관계 추가 OCP 원칙을 지키기 위해 인터페이스 상속을 추가하였습니다. 따라서, 서비스 레이어에서 ReviewRepository를 의존하여 두 개의 구현체의 메소드를 모두 사용할 수 있습니다.
    Related to: #53

--- src/main/java/nerds/studiousTestProject/review/repository/ReviewRepository.java
+++ src/main/java/nerds/studiousTestProject/review/repository/ReviewRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
     // 스터디카페의 평균 청결도, 방음도를 가져올 때 필요한 메소드인데, 그 때는 Pageable를 받지 않아서 따로 구분 했습니다!
     List<Review> findAllByIdInOrderByCreatedDateDesc(List<Long> reviewIds);
 
