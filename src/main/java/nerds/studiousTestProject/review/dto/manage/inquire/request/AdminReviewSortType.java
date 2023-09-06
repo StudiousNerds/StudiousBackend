5332398bf5123cde7abdca0ad1dbdabda91c465b
kmw2378 <rlarla677@gmail.com>
2023-09-06 23:49:30 +0900

    refactor: AdminReviewSortType 값 이름 수정 Studycafe의 정렬 조건 값과 통일성 있도록 DATE를 빼서 CREATED_DESC, CREATED_ASC로 수정하였습니다.
    Related to: #53

--- src/main/java/nerds/studiousTestProject/review/dto/manage/inquire/request/AdminReviewSortType.java
+++ src/main/java/nerds/studiousTestProject/review/dto/manage/inquire/request/AdminReviewSortType.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Sort;
 
 @Getter
 public enum AdminReviewSortType {
-    CREATED_DATE_DESC(Sort.by(Sort.Direction.DESC, "createdDate")),
-    CREATED_DATE_ASC(Sort.by(Sort.Direction.ASC, "createdDate"));
+    CREATED_DESC(Sort.by(Sort.Direction.DESC, "createdDate")),
+    CREATED_ASC(Sort.by(Sort.Direction.ASC, "createdDate"));
 
     private final Sort sort;
 
@@ -15,6 +15,6 @@ public enum AdminReviewSortType {
     }
 
     public static class Names {
-        public static final String CREATED_DATE_DESC = "CREATED_DATE_DESC";
+        public static final String CREATED_DATE_DESC = "CREATED_DESC";
     }
 }
