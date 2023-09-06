69256eb1c14e803b683c5f43c45d5ec44ef16a2a
kmw2378 <rlarla677@gmail.com>
2023-09-06 23:03:49 +0900

    feat: 사장님 페이지 리뷰 조회 시 사용되는 QueryDSL을 도입한 Repository 인터페이스 추가 JPQL 로 작성하기 힘든 쿼리문을 작성하기 위해 QueryDSL을 도입하였습니다.
    Related to: #53

---
+++ src/main/java/nerds/studiousTestProject/review/repository/ReviewRepositoryCustom.java
@@ -0,0 +1,10 @@
+package nerds.studiousTestProject.review.repository;
+
+import nerds.studiousTestProject.review.dto.manage.inquire.request.AdminReviewType;
+import nerds.studiousTestProject.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ReviewRepositoryCustom {
+    Page<Review> getPagedReviewsByStudycafeId(Long studycafeId, AdminReviewType reviewType, Pageable pageable);
+}
