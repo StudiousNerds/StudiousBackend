f49f01a373a1795b994b2273f3bbd72d4b4525df
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-08 16:30:06 +0900

    feat: 페이지 관련 예외 추가
    쿼리 파라미터로 넘어온 page가 음수이거나 null 일 경우의 예외 입니다.
    
    Related to: #57

--- src/main/java/nerds/studiousTestProject/common/exception/ErrorCode.java
+++ src/main/java/nerds/studiousTestProject/common/exception/ErrorCode.java
@@ -52,7 +52,8 @@ public enum ErrorCode {
     MISMATCH_PAYMENT_METHOD("결제 수단이 일치하지 않습니다."),
     INVALID_PAYMENT_SECRET("올바른 시크릿 값이 아닙니다."),
     INVALID_REFUND_RECEIVE_ACCOUNT_INFO("가상계좌 환불 시 환불 계좌는 필수입니다."),
-    MISMATCH_MEMBER_AND_STUDYCAFE("회원 정보와 일치하는 스터디카페를 찾을 수 없습니다");
+    MISMATCH_MEMBER_AND_STUDYCAFE("회원 정보와 일치하는 스터디카페를 찾을 수 없습니다"),
+    INVALID_PAGE_NUMBER("잘못된 페이지 번호입니다.");
 
     private final String message;
     ErrorCode(String message) {
