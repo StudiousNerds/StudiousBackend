9d7824192ce9e9051dc50a8edf16cb425670c350
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-10 13:03:17 +0900

    refactor: DTO 클래스명 변경
    RefundPolicyInfo 이름이 겹쳐 RefundPolicyInfoWithOnDay로 변경하였습니다.
    
    Related to: #57

--- src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepository.java
+++ src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepository.java
@@ -2,11 +2,9 @@ package nerds.studiousTestProject.convenience.repository;
 
 import nerds.studiousTestProject.convenience.entity.Convenience;
 import nerds.studiousTestProject.room.entity.Room;
-import nerds.studiousTestProject.studycafe.entity.Studycafe;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface ConvenienceRepository extends JpaRepository<Convenience, Long> {
-    List<Convenience> findAllByRoomOrStudycafe(Room room, Studycafe studycafe);
+    List<Convenience> findAllByRoom(Room room);
 }
---
+++ src/main/java/nerds/studiousTestProject/reservation/dto/cancel/response/RefundPolicyInfoWithOnDay.java
@@ -0,0 +1,25 @@
+package nerds.studiousTestProject.reservation.dto.cancel.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import nerds.studiousTestProject.refundpolicy.entity.RefundPolicy;
+import nerds.studiousTestProject.reservation.dto.RefundPolicyInfo;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class RefundPolicyInfoWithOnDay {
+
+    private List<RefundPolicyInfo> refundPolicy;
+    private RefundPolicyInfo refundPolicyOnDay;
+
+    public static RefundPolicyInfoWithOnDay of(List<RefundPolicy> refundPolicies, RefundPolicy refundPolicyOnDay) {
+        return RefundPolicyInfoWithOnDay.builder()
+                .refundPolicy(refundPolicies.stream().map(RefundPolicyInfo::from).collect(Collectors.toList()))
+                .refundPolicyOnDay(RefundPolicyInfo.from(refundPolicyOnDay))
+                .build();
+    }
+}
