45124859e4ed1a160bc02b7aea45394b7ea4af1b
kmw2378 <rlarla677@gmail.com>
2023-09-18 13:03:36 +0900

    refactor: WebClient 관련 설정사항을 별도로 분리 WebClient의 Bean 등록 메소드를 WebClientConfig.java에 추가하였습니다,
    Related to: #60

--- src/main/java/nerds/studiousTestProject/common/config/SecurityConfig.java
+++ src/main/java/nerds/studiousTestProject/common/config/SecurityConfig.java
@@ -20,8 +20,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 /**
  * Spring Security 관련 설정 사항들
@@ -69,22 +67,6 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-
-    @Bean
-    public DefaultUriBuilderFactory defaultUriBuilderFactory() {
-        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
-        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
-        return factory;
-    }
-
-    /**
-     * 인증 키를 포함 한 URL을 입력 받을 때 인코딩 관련 오류가 발생하므로, WebClient 에서 URL 인코딩을 하지 않도록 하기 위해 DefaultUriBuilderFactory 객체를 추가
-     * @return URL 인코딩이 되지 않는 WebClient
-     */
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder().uriBuilderFactory(defaultUriBuilderFactory()).build();
     }
 
     @Bean
