a332169371810923acea66b24a27df176838d1ed
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-18 15:20:37 +0900

    feat: reservation으로 편의시설 내역찾는 기능 추가
    Related to: #61

--- src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRecordRepository.java
+++ src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRecordRepository.java
@@ -1,7 +1,13 @@
 package nerds.studiousTestProject.convenience.repository;
 
 import nerds.studiousTestProject.convenience.entity.ConvenienceRecord;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ConvenienceRecordRepository extends JpaRepository<ConvenienceRecord, Long> {
+
+    Optional<ConvenienceRecord> findByReservationRecord(ReservationRecord reservationRecord);
+
 }
