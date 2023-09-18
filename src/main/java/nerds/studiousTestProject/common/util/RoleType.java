db4198bdb5484ae5c780df9ecf7f895465745955
kmw2378 <rlarla677@gmail.com>
2023-09-18 13:00:56 +0900

    feat: @Secured 어노테이션 속성값으로 사용되는 클래스 추가 속성 값에 사용되는 상수들을 정의한 클래스를 추가하였습니다.
    Related to: #60

---
+++ src/main/java/nerds/studiousTestProject/common/util/RoleType.java
@@ -0,0 +1,7 @@
+package nerds.studiousTestProject.common.util;
+
+public class RoleType {
+    public static final String USER = "ROLE_USER";
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String SUPER_ADMIN = "ROLE_SUPER_ADMIN";
+}
