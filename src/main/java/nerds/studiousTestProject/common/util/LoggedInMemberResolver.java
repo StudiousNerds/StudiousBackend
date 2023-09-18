6242a07ad199c46e16d38406ca70771fc6b34db4
kmw2378 <rlarla677@gmail.com>
2023-09-18 13:10:55 +0900

    feat: 요청 헤더의 토큰 값을 파싱하여 member pk값을 가져오는 어노테이션 및 리졸버 추기 기존 tokenService를 주입받아 토큰을 파싱하지 않고 어노테이션을 통하여 별도의 의존관계없이 바로 pk값을 가져오도록 하였습니다.
    Related to: #60

---
+++ src/main/java/nerds/studiousTestProject/common/util/LoggedInMemberResolver.java
@@ -0,0 +1,35 @@
+package nerds.studiousTestProject.common.util;
+
+import jakarta.annotation.Nullable;
+import nerds.studiousTestProject.common.exception.NotAuthorizedException;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.security.Principal;
+
+import static nerds.studiousTestProject.common.exception.ErrorCode.NOT_AUTHORIZE_ACCESS;
+
+public class LoggedInMemberResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoggedInMember.class);
+    }
+
+    @Override
+    public Long resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Principal principal = webRequest.getUserPrincipal();
+
+        try {
+            return Long.parseLong(principal.getName());
+        } catch (NullPointerException e) {
+            if (parameter.hasParameterAnnotation(Nullable.class)) {
+                return null;
+            }
+
+            throw new NotAuthorizedException(NOT_AUTHORIZE_ACCESS);
+        }
+    }
+}
