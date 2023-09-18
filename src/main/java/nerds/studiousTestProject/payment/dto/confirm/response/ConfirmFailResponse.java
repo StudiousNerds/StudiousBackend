80e65ed97dbb788d4d1de6f2c6956e1acbacdf17
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-18 15:20:17 +0900

    feat: 정적 팩토리 메서드 추가
    Related to: #61

--- src/main/java/nerds/studiousTestProject/payment/dto/confirm/response/ConfirmFailResponse.java
+++ src/main/java/nerds/studiousTestProject/payment/dto/confirm/response/ConfirmFailResponse.java
@@ -4,13 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Builder
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ConfirmFailResponse {
     private String message;
     private int statusCode;
+
+    public static ConfirmFailResponse of(String message) {
+        return ConfirmFailResponse.builder()
+                .message(message)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+    }
+
 }
