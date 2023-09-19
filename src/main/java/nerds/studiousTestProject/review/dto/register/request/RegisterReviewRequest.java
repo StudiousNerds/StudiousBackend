a12ed32d0caece445e2ca62a80574302032c492f
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-20 04:29:51 +0900

    feat: dto단에서 최대값 검증
    Related to: #62

--- src/main/java/nerds/studiousTestProject/review/dto/register/request/RegisterReviewRequest.java
+++ src/main/java/nerds/studiousTestProject/review/dto/register/request/RegisterReviewRequest.java
@@ -1,8 +1,10 @@
 package nerds.studiousTestProject.review.dto.register.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import nerds.studiousTestProject.review.entity.Grade;
@@ -14,16 +16,20 @@ import java.util.List;
 public class RegisterReviewRequest {
     private Long reservationId;
     @NotNull(message = "청결도 평점은 필수입니다.")
+    @Max(value = 5, message = "최대 5점까지 가능합니다.")
     private Integer cleanliness;
     @NotNull(message = "방음 평점은 필수입니다.")
+    @Max(value = 5, message = "최대 5점까지 가능합니다.")
     private Integer deafening;
     @NotNull(message = "비품 상태 평점은 필수입니다.")
+    @Max(value = 5, message = "최대 5점까지 가능합니다.")
     private Integer fixtureStatus;
     @NotNull(message = "추천 여부는 필수입니다.")
     private Boolean isRecommend;
     @NotEmpty(message = "해시태그는 필수입니다.")
     private List<String> hashtags;
     @NotBlank(message = "리뷰는 필수입니다.")
+    @Size(max = 200, message = "최대 200자까지 작성가능합니다.")
     private String detail;
 
     public static Grade toGrade(RegisterReviewRequest registerReviewRequest) {
