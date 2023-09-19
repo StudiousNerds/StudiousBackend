aea055ca7477145aacda418cd3fe8a45a5cefb22
kmw2378 <rlarla677@gmail.com>
2023-09-19 15:17:07 +0900

    remove: @Secured 속성 값에 사용되는 클래스 삭제 @Secured 를 사용하지 않아 삭제하였습니다.
    Related to: #60

--- src/main/java/nerds/studiousTestProject/common/util/RoleType.java
+++
@@ -1,7 +0,0 @@
-package nerds.studiousTestProject.common.util;
-
-public class RoleType {
-    public static final String USER = "ROLE_USER";
-    public static final String ADMIN = "ROLE_ADMIN";
-    public static final String SUPER_ADMIN = "ROLE_SUPER_ADMIN";
-}
