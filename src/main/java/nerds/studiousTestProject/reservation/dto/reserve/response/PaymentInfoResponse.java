6ee30eb4216ee1e6ed35230335dad30d5c2fc59b
rlfrkdms1 <rlfrkdms1@naver.com>
2023-09-20 00:26:31 +0900

    refactor: 성공/실패 url을 클라이언트에서 지정하도록 변경
    결제 수단(가상계좌 결제)의 추가로 인해 수단이 달라질 경우 성공 url이 달라져야 하는데 서버에선 url을 지정하기 전엔 결제 수단을 알 수 없습니다. 따라서 결제 수단에 따라 클라이언트에서 url을 지정하도록 변경합니다.
    
    Related to: #61

--- src/main/java/nerds/studiousTestProject/reservation/dto/reserve/response/PaymentInfoResponse.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/reserve/response/PaymentInfoResponse.java
@@ -19,19 +19,11 @@ public class PaymentInfoResponse {
 
     private String orderName;
 
-    private String successUrl;
-
-    private String failUrl;
-
-    private static final String REQUEST_SUCCESS_URI = "http://localhost:8080/studious/payments/success";
-    private static final String REQUEST_FAIL_URI = "http://localhost:8080/studious/payments/fail";
     public static PaymentInfoResponse of(Payment payment, String orderName) {
         return PaymentInfoResponse.builder()
                 .amount(payment.getPrice())
                 .orderId(payment.getOrderId())
                 .orderName(orderName)
-                .successUrl(REQUEST_SUCCESS_URI)
-                .failUrl(REQUEST_FAIL_URI)
                 .build();
     }
 
