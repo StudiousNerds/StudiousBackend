ffd8a2850ae6508f4ffc87e12627152c0411a413
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-20 03:21:46 +0900

    test: 스터디카페 id로 모든 룸 찾는 테스트
    오류가 나는 부분들 전부 수정 완료
    결과: 성공
    
    Related to: #62

--- src/test/java/nerds/studiousTestProject/room/repository/RoomRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/room/repository/RoomRepositoryTest.java
@@ -1,5 +1,6 @@
 package nerds.studiousTestProject.room.repository;
 
+import nerds.studiousTestProject.member.entity.member.Member;
 import nerds.studiousTestProject.support.RepositoryTest;
 import nerds.studiousTestProject.room.entity.Room;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
@@ -10,8 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.Optional;
 
-import static nerds.studiousTestProject.support.fixture.StudycafeFixture.*;
+import static nerds.studiousTestProject.support.EntitySaveProvider.룸_저장;
+import static nerds.studiousTestProject.support.EntitySaveProvider.스터디카페_저장;
+import static nerds.studiousTestProject.support.EntitySaveProvider.회원_저장;
+import static nerds.studiousTestProject.support.fixture.MemberFixture.DEFAULT_USER;
+import static nerds.studiousTestProject.support.fixture.RoleFixture.ADMIN;
 import static nerds.studiousTestProject.support.fixture.RoomFixture.ROOM_FOUR_SIX;
+import static nerds.studiousTestProject.support.fixture.StudycafeFixture.NERDS;
 
 @RepositoryTest
 class RoomRepositoryTest {
@@ -24,13 +30,17 @@ class RoomRepositoryTest {
     @Test
     void findAllByStudycafeId() {
         // given
-        Studycafe studycafe = studycafeRepository.save(FIRST_STUDYCAFE.생성(1L));
-        Room room = roomRepository.save(ROOM_FOUR_SIX.스터디카페_생성(studycafe, 1L));
-        Room room1 = roomRepository.save(ROOM_FOUR_SIX.스터디카페_생성(studycafe,2L));
+        Member admin = 회원_저장(DEFAULT_USER.생성());
+        ADMIN.멤버_생성(admin);
+
+        Studycafe studycafe = 스터디카페_저장(NERDS.멤버_생성(admin));
+        Room room1 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe));
+        Room room2 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe));
+        Room room3 = 룸_저장(ROOM_FOUR_SIX.스터디카페_생성(studycafe));
         // when
         List<Room> roomList = roomRepository.findAllByStudycafeId(studycafe.getId());
         // then
-        Assertions.assertThat(roomList).contains(room, room1);
+        Assertions.assertThat(roomList).hasSize(3);
     }
 
     @Test
