a42d9cb03572237f88655e7bb5f9c306388354a9
kmw2378 <rlarla677@gmail.com>
2023-09-21 01:12:12 +0900

    !HOTFIX: 스터디카페 검색 시 해시테그값이 없는 문제 해결 DTO 클래스에 해시테그 필드가 누락되어 추가했습니다.

--- src/main/java/nerds/studiousTestProject/studycafe/dto/search/response/SearchResponse.java
+++ src/main/java/nerds/studiousTestProject/studycafe/dto/search/response/SearchResponse.java
@@ -1,9 +1,13 @@
 package nerds.studiousTestProject.studycafe.dto.search.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Builder
 @Data
 public class SearchResponse {
@@ -13,20 +17,11 @@ public class SearchResponse {
     private Integer accumRevCnt;
     private Integer walkingTime;
     private String nearestStation;
+    private List<String> hashtagNames;
     private Double grade;
 
-    public SearchResponse(Long id, String name, String photo, Integer accumRevCnt, Integer walkingTime, String nearestStation, Double grade) {
-        this.id = id;
-        this.name = name;
-        this.photo = photo;
-        this.accumRevCnt = accumRevCnt;
-        this.walkingTime = walkingTime;
-        this.nearestStation = nearestStation;
-        this.grade = grade;
-    }
-
     public static SearchResponse from(Studycafe studycafe) {
-        return SearchResponse.builder()
+        SearchResponse searchResponse = SearchResponse.builder()
                 .id(studycafe.getId())
                 .name(studycafe.getName())
                 .photo(studycafe.getPhoto())
@@ -35,5 +30,11 @@ public class SearchResponse {
                 .walkingTime(studycafe.getWalkingTime())
                 .nearestStation(studycafe.getNearestStation())
                 .build();
+
+        List<String> hashtagNames = studycafe.getAccumHashtagHistories().stream().map(e -> e.getName().name()).toList();
+        searchResponse.setHashtagNames(hashtagNames);
+        System.out.println(hashtagNames);
+
+        return searchResponse;
     }
 }
