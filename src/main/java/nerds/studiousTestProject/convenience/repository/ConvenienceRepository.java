d1ee07eef41c2f56a6616446fbdb17523df0c12d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-03 23:02:45 +0900

    feat: 유료 편의시설 조회 기능 추가
    Related to: #61

--- src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepository.java
+++ src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepository.java
@@ -1,11 +1,17 @@
 package nerds.studiousTestProject.convenience.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import nerds.studiousTestProject.convenience.entity.Convenience;
 import nerds.studiousTestProject.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface ConvenienceRepository extends JpaRepository<Convenience, Long> {
     List<Convenience> findAllByRoom(Room room);
     void deleteAllByRoomId(Long roomId);
+
+    @Query("select c from Convenience  c where c.room.id = :roomId and c.isFree = false ")
+    List<Convenience> findAllByRoomIdWherePaid(@Param("roomId") Long roomId);
 }
