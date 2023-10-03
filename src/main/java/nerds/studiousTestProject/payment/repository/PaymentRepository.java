c9711f2202b26df4a050062b9e8a1f3f27008014
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-03 23:03:34 +0900

    feat: reservation으로 모든 결제 조회, 존재 여부 기능 추가
    Related to: #61

--- src/main/java/nerds/studiousTestProject/payment/repository/PaymentRepository.java
+++ src/main/java/nerds/studiousTestProject/payment/repository/PaymentRepository.java
@@ -3,10 +3,15 @@ package nerds.studiousTestProject.payment.repository;
 import nerds.studiousTestProject.payment.entity.Payment;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     Optional<Payment> findByReservationRecord(ReservationRecord reservationRecord);
     Optional<Payment> findByOrderId(String orderId);
+
+    List<Payment> findAllByReservationRecord(ReservationRecord reservationRecord);
+
+    boolean existsByReservationRecord(ReservationRecord reservationRecord);
 }
