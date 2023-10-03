094c8c49c885441d3916f7c6cfbace4da266a05a
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-03 23:01:31 +0900

    refactor: all로 변경
    예약 한건에 여러 편의시설이 존재하므로 all로 변경했습니다.
    
    Related to: #61

--- src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRecordRepository.java
+++ src/main/java/nerds/studiousTestProject/convenience/repository/ConvenienceRecordRepository.java
@@ -4,10 +4,10 @@ import nerds.studiousTestProject.convenience.entity.ConvenienceRecord;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ConvenienceRecordRepository extends JpaRepository<ConvenienceRecord, Long> {
 
-    Optional<ConvenienceRecord> findByReservationRecord(ReservationRecord reservationRecord);
+    List<ConvenienceRecord> findAllByReservationRecord(ReservationRecord reservationRecord);
 
 }
