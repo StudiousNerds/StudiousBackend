0a29666a64057a8b84aa9ff636f6cff19a337faf
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-07 19:01:35 +0900

    feat: 정적팩토리 메서드 추가
    오버로딩하여 두가지 타입에서 사용가능하도록 추가했습니다.
    
    Related to: #61

--- src/main/java/nerds/studiousTestProject/reservation/dto/change/response/PaidConvenienceInfo.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/change/response/PaidConvenienceInfo.java
@@ -1,16 +1,23 @@
 package nerds.studiousTestProject.reservation.dto.change.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import nerds.studiousTestProject.convenience.entity.Convenience;
+import nerds.studiousTestProject.convenience.entity.ConvenienceRecord;
 
 @AllArgsConstructor
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PaidConvenienceInfo {
 
     private String name;
     private int price;
+
+    public static PaidConvenienceInfo from(Convenience convenience) {
+        return new PaidConvenienceInfo(convenience.getName().name(), convenience.getPrice());
+    }
+
+    public static PaidConvenienceInfo from(ConvenienceRecord convenienceRecord) {
+        return new PaidConvenienceInfo(convenienceRecord.getConvenienceName(), convenienceRecord.getPrice());
+    }
 
 }
