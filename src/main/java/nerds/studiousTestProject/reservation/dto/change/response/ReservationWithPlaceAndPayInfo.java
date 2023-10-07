d9a9def1370aaff8a4a69f7220947ac1096e285d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-07 19:02:46 +0900

    refactor: 결제 수단 삭제
    결제 수단을 삭제하면서 payment를 객체로 받지 않고 결제 금액만 받아오는것으로 변경했습니다.
    
    Related to: #61

--- src/main/java/nerds/studiousTestProject/reservation/dto/change/response/ReservationWithPlaceAndPayInfo.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/change/response/ReservationWithPlaceAndPayInfo.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import nerds.studiousTestProject.payment.entity.Payments;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import nerds.studiousTestProject.room.entity.Room;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
@@ -21,21 +20,19 @@ public class ReservationWithPlaceAndPayInfo {
     private String studycafePhoto;
     private String studycafeName;
     private String roomName;
-    private String paymentMethod;
     private int price;
     private LocalDate reserveDate;
     private LocalTime startTime;
     private LocalTime endTime;
 
-    public static ReservationWithPlaceAndPayInfo of(Payments payments, ReservationRecord reservationRecord) {
+    public static ReservationWithPlaceAndPayInfo of(int price, ReservationRecord reservationRecord) {
         Room room = reservationRecord.getRoom();
         Studycafe studycafe = room.getStudycafe();
         return ReservationWithPlaceAndPayInfo.builder()
                 .studycafeName(studycafe.getName())
                 .studycafePhoto(studycafe.getPhoto())
                 .roomName(room.getName())
-                .paymentMethod(payments.getMethods())
-                .price(payments.getTotalPrice())
+                .price(price)
                 .reserveDate(reservationRecord.getDate())
                 .startTime(reservationRecord.getStartTime())
                 .endTime(reservationRecord.getEndTime())
