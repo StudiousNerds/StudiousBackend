099d37f2377d38bf644dd45926f44a6a5280c762
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-09 04:51:41 +0900

    feat: 사장님 예약 취소 기능 추가
    Related to: #66

--- src/main/java/nerds/studiousTestProject/reservation/controller/AdminReservationRecordController.java
+++ src/main/java/nerds/studiousTestProject/reservation/controller/AdminReservationRecordController.java
@@ -2,13 +2,17 @@ package nerds.studiousTestProject.reservation.controller;
 
 import lombok.RequiredArgsConstructor;
 import nerds.studiousTestProject.common.util.LoggedInMember;
+import nerds.studiousTestProject.payment.service.PaymentService;
+import nerds.studiousTestProject.payment.util.totoss.CancelRequest;
 import nerds.studiousTestProject.reservation.dto.admin.ShowAdminCancelResponse;
 import nerds.studiousTestProject.reservation.service.ReservationRecordService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/admin/reservations")
@@ -17,10 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminReservationRecordController {
 
     private final ReservationRecordService reservationRecordService;
+    private final PaymentService paymentService;
 
     @GetMapping("/{reservationId}/cancel")
     public ShowAdminCancelResponse showCancel(@PathVariable Long reservationId) {
         return reservationRecordService.showAdminCancel(reservationId);
+    }
+
+    @PostMapping("/{reservationId}/cancel")
+    public ResponseEntity<Void> cancel(@PathVariable Long reservationId, @RequestBody CancelRequest request) {
+        paymentService.adminCancel(reservationId, request);
+        return ResponseEntity.noContent().build();
     }
 
 }
