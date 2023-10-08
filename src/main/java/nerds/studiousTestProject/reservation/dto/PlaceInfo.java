5f8adc4122f49b92f2ce9f729fe6ab31a01d8d6c
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-08 12:08:21 +0900

    refactor: 장소와 예약을 분리 및 PlaceInfo 공통으로 쓰도록 수정
    Related to: #61

---
+++ src/main/java/nerds/studiousTestProject/reservation/dto/PlaceInfo.java
@@ -0,0 +1,24 @@
+package nerds.studiousTestProject.reservation.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import nerds.studiousTestProject.room.entity.Room;
+import nerds.studiousTestProject.studycafe.entity.Studycafe;
+
+@Getter
+@Builder
+public class PlaceInfo {
+
+    private String studycafeName;
+    private String roomName;
+    private String address;
+
+    public static PlaceInfo of(Studycafe studycafe, Room room) {
+        return PlaceInfo.builder()
+                .studycafeName(studycafe.getName())
+                .roomName(room.getName())
+                .address(studycafe.getAddress().getEntryAddress())
+                .build();
+    }
+
+}
--- src/main/java/nerds/studiousTestProject/reservation/dto/cancel/response/PaymentInfoWithRefund.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/cancel/response/PaymentInfoWithRefund.java
@@ -3,14 +3,16 @@ package nerds.studiousTestProject.reservation.dto.cancel.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import nerds.studiousTestProject.reservation.dto.PaymentInfo;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
 @Builder
 public class PaymentInfoWithRefund {
-    private String paymentMethod;
-    private int price;
+
     private int refundPrice;
     private int refundFee;
+    List<PaymentInfo> payment;
 
 }
