352ecfb1e6f8658c2dbc3c5cd4c5f8165868f59b
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-09 01:23:38 +0900

    feat: 예약 변경 요청 DTO 추가
    Related to: #65

---
+++ src/main/java/nerds/studiousTestProject/reservation/dto/change/request/ChangeReservationRequest.java
@@ -0,0 +1,24 @@
+package nerds.studiousTestProject.reservation.dto.change.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nerds.studiousTestProject.reservation.dto.change.response.PaidConvenienceInfo;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChangeReservationRequest {
+
+    @NotNull(message = "추가 결제 총 가격은 필수입니다.")
+    private Integer price;
+    private Integer headCount;
+    private List<PaidConvenienceInfo> conveniences;
+
+}
