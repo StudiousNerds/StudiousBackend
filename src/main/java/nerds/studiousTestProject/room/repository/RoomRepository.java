8e244de983d7e0887fef1b401610e2a24c337de9
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-08 12:22:21 +0900

    refactor: id로 room 조회 기능에 페치 조인 기능 추가
    Related to: #61

--- src/main/java/nerds/studiousTestProject/room/repository/RoomRepository.java
+++ src/main/java/nerds/studiousTestProject/room/repository/RoomRepository.java
@@ -1,10 +1,16 @@
 package nerds.studiousTestProject.room.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import nerds.studiousTestProject.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     List<Room> findAllByStudycafeId(Long id);
+
+    @Query("select r from Room r join fetch r.studycafe where r.id = :roomId")
+    Optional<Room> findByIdWithStudycafe(@Param("roomId") Long roomId);
 }
