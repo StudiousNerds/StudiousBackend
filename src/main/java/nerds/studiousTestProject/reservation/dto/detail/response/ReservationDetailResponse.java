0f4903426009a9d913a448c5bb0de0162c3feaa1
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-08 12:09:49 +0900

    refactor: place와 reservation 분리
    Related to: #61

--- src/main/java/nerds/studiousTestProject/reservation/dto/detail/response/ReservationDetailResponse.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/detail/response/ReservationDetailResponse.java
@@ -3,6 +3,9 @@ package nerds.studiousTestProject.reservation.dto.detail.response;
 import lombok.Builder;
 import lombok.Getter;
 import nerds.studiousTestProject.payment.entity.Payment;
+import nerds.studiousTestProject.reservation.dto.PaymentInfo;
+import nerds.studiousTestProject.reservation.dto.PlaceInfo;
+import nerds.studiousTestProject.reservation.dto.ReservationInfo;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import nerds.studiousTestProject.room.entity.Room;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
@@ -11,15 +14,19 @@ import nerds.studiousTestProject.studycafe.entity.Studycafe;
 @Builder
 public class ReservationDetailResponse {
 
-    private PlaceInfo placeInfo;
-    private PaymentInfo paymentInfo;
-    private ReserveUserInfo reserveUserInfo;
+    private PlaceInfo place;
+    private ReservationInfo reservation;
+    private PaymentInfo payment;
+    private ReserveUserInfo reserveUser;
 
-    public static ReservationDetailResponse of(ReservationRecord reservationRecord, Studycafe studycafe, Room room, Payment payment) {
+    public static ReservationDetailResponse of(ReservationRecord reservationRecord, Payment payment) {
+        Room room = reservationRecord.getRoom();
+        Studycafe studycafe = room.getStudycafe();
         return ReservationDetailResponse.builder()
-                .placeInfo(PlaceInfo.of(studycafe, room, reservationRecord))
-                .paymentInfo(PaymentInfo.from(payment))
-                .reserveUserInfo(ReserveUserInfo.from(reservationRecord))
+                .place(PlaceInfo.of(studycafe, room))
+                .reservation(ReservationInfo.from(reservationRecord))
+                .payment(PaymentInfo.from(payment))
+                .reserveUser(ReserveUserInfo.from(reservationRecord))
                 .build();
     }
 }
