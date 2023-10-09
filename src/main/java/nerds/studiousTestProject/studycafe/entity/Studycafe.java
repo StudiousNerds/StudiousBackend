663d9697e88f39999c9327c23005efc9f59a7089
kmw2378 <rlarla677@gmail.com>
2023-10-09 15:07:43 +0900

    refactor: Studycafe 필드 및 생성자 수정 기존 totalGrade 삭제 후 gradeSum. gradeCount 를 추가하였습니다. 생성자에서 gradeSum, gradeCount, createdDate 등 초깃값이 명확한 필드는 파라미터로 받지않고 생성자 내에서 초기화하도록 수정하였습니다.
    Related to: #64

--- src/main/java/nerds/studiousTestProject/studycafe/entity/Studycafe.java
+++ src/main/java/nerds/studiousTestProject/studycafe/entity/Studycafe.java
@@ -69,8 +69,11 @@ public class Studycafe {
     @CreatedDate
     private LocalDateTime createdDate;
 
-    @Column(name = "total_grade", nullable = true)
-    private Double totalGrade;
+    @Column(name = "grade_sum", nullable = true)
+    private Double gradeSum;
+
+    @Column(name = "grade_count", nullable = true)
+    private Integer gradeCount;
 
     @OneToMany(mappedBy = "studycafe", cascade = CascadeType.ALL)
     private List<Announcement> announcements = new ArrayList<>();
@@ -169,10 +172,6 @@ public class Studycafe {
         }
     }
 
-    public void addTotalGrade(Double totalGrade) {
-        this.totalGrade = totalGrade;
-    }
-
     @Builder
     public Studycafe(Long id, Member member, String name, Address address, String photo, String tel, Integer walkingTime, String nearestStation, String introduction) {
         this.id = id;
@@ -186,6 +185,7 @@ public class Studycafe {
         this.accumReserveCount = 0;
         this.introduction = introduction;
         this.createdDate = LocalDateTime.now();
-        this.totalGrade = 0.;
+        this.gradeSum = 0.;
+        this.gradeCount = 0;
     }
 }
