3abbf730cbe16d455c53c747b9ac97b4c2b99613
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-10-15 21:36:29 +0900

    test: 권한 저장을 하도록 수정
    Related to: #62

--- src/test/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepositoryTest.java
+++ src/test/java/nerds/studiousTestProject/convenience/repository/ConvenienceRepositoryTest.java
@@ -11,12 +11,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static nerds.studiousTestProject.support.EntitySaveProvider.권한_저장;
 import static nerds.studiousTestProject.support.EntitySaveProvider.스터디카페_저장;
 import static nerds.studiousTestProject.support.EntitySaveProvider.편의시설_저장;
 import static nerds.studiousTestProject.support.EntitySaveProvider.회원_저장;
 import static nerds.studiousTestProject.support.fixture.ConvenienceFixture.ROOM_HDMI_FREE;
-import static nerds.studiousTestProject.support.fixture.MemberFixture.DEFAULT_USER;
-import static nerds.studiousTestProject.support.fixture.RoleFixture.USER;
+import static nerds.studiousTestProject.support.fixture.MemberFixture.KAKAO_USER;
+import static nerds.studiousTestProject.support.fixture.RoleFixture.ADMIN;
 import static nerds.studiousTestProject.support.fixture.RoomFixture.ROOM_FOUR_SIX;
 import static nerds.studiousTestProject.support.fixture.StudycafeFixture.NERDS;
 
@@ -34,10 +35,10 @@ class ConvenienceRepositoryTest {
     @DisplayName(value = "룸 id를 통해 (룸)편의시설을 삭제할 수 있다.")
     void deleteAllByRoomId() {
         // given
-        Member member = 회원_저장(DEFAULT_USER.생성());
-        USER.멤버_생성(member);
-        Studycafe studycafe = 스터디카페_저장(NERDS.멤버_생성(member));
-        Room room = roomRepository.save(ROOM_FOUR_SIX.스터디카페_생성(studycafe, 1L));
+        Member admin = 회원_저장(KAKAO_USER.생성());
+        권한_저장(ADMIN.멤버_생성(admin));
+        Studycafe studycafe = 스터디카페_저장(NERDS.멤버_생성(admin));
+        Room room = roomRepository.save(ROOM_FOUR_SIX.스터디카페_생성(studycafe));
         편의시설_저장(ROOM_HDMI_FREE.룸_생성(room, null));
         // when
         convenienceRepository.deleteAllByRoomId(room.getId());
