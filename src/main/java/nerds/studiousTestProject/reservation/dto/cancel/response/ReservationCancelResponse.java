f44cd14db3fbd4d5d324981a58efed285c641510
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-18 03:08:34 +0900

    refactor: 필드 분리 및 정적 팩토리 메서드 추가
    Related to: #65

--- src/main/java/nerds/studiousTestProject/reservation/dto/cancel/response/ReservationCancelResponse.java
+++ src/main/java/nerds/studiousTestProject/reservation/dto/cancel/response/ReservationCancelResponse.java
@@ -3,14 +3,30 @@ package nerds.studiousTestProject.reservation.dto.cancel.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import nerds.studiousTestProject.refundpolicy.entity.RefundPolicy;
+import nerds.studiousTestProject.reservation.dto.ReservationInfo;
+import nerds.studiousTestProject.reservation.entity.ReservationRecord;
+import nerds.studiousTestProject.studycafe.dto.PlaceInfo;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Builder
 @Getter
 public class ReservationCancelResponse {
 
-    private ReservationRecordInfoWithPlace reservation;
-    private PaymentInfoWithRefund paymentRecord;
+    private ReservationInfo reservation;
+    private PlaceInfo place;
+    private PaymentInfoWithRefund payment;
     private RefundPolicyInfoWithOnDay refundPolicy;
 
+    public static ReservationCancelResponse of(ReservationRecord reservationRecord, PaymentInfoWithRefund paymentInfoWithRefund, List<RefundPolicy> refundPolicies, RefundPolicy refundPolicyOnDay) {
+        return ReservationCancelResponse.builder()
+                .reservation(ReservationInfo.from(reservationRecord))
+                .place(PlaceInfo.from(reservationRecord))
+                .payment(paymentInfoWithRefund)
+                .refundPolicy(RefundPolicyInfoWithOnDay.of(refundPolicies, refundPolicyOnDay))
+                .build();
+
+    }
 }
