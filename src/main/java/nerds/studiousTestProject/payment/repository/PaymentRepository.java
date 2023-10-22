b75f45749a33f13d8831fb3c63f227a08992a809
rlfrkdms1 <rlfrkdms1@naver.com>
2023-10-23 00:12:44 +0900

    stlye: import 문 정리
    Related to: #65

--- src/main/java/nerds/studiousTestProject/payment/repository/PaymentRepository.java
+++ src/main/java/nerds/studiousTestProject/payment/repository/PaymentRepository.java
@@ -1,10 +1,7 @@
 package nerds.studiousTestProject.payment.repository;
 
 import nerds.studiousTestProject.payment.entity.Payment;
-import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
