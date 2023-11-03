2a0c6b925d0201e9ec5ca33f00a65a49815d5821
kmw2378 <rlarla677@gmail.com>
2023-11-04 01:17:27 +0900

    refactor: 기존 특정 날짜의 예약 정보를 가져오던 메소드를 특정 날짜 이후 예약 정보를 반환하도록 수정 룸 예약정보 조회시 쿼리 수를 줄이기 위해 특정 날짜 이후 모든 예약 정보를 한 번에 가져온 후 서비스 레이어에서 이를 파싱하도록 하였습니다.
    Related to: #70

--- src/main/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepository.java
+++ src/main/java/nerds/studiousTestProject/reservation/repository/ReservationRecordRepository.java
@@ -1,6 +1,7 @@
 package nerds.studiousTestProject.reservation.repository;
 
 import nerds.studiousTestProject.member.entity.member.Member;
+import nerds.studiousTestProject.payment.entity.Payment;
 import nerds.studiousTestProject.reservation.entity.ReservationRecord;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,22 +9,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRecordRepository extends JpaRepository<ReservationRecord, Long>, ReservationRecordRepositoryCustom {
-    @Query(value = "select r.startTime, r.endTime " +
-            "from ReservationRecord r " +
-            "where r.date = :date and r.room.id = :roomId " +
-            "and r.status = nerds.studiousTestProject.reservation.entity.ReservationStatus.CONFIRMED")
-    List<List<LocalTime>> findAllReservedTime(@Param("date")LocalDate date, @Param("roomId")Long roomId);
-
-    @Query(value = "select r.id, date, using_time, start_time, end_time, head_count, user_name, order_id, user_phone_number, request, status, member_id, room_id, review_id " +
-            "from reservation_record r " +
-            "where r.date >= :date and r.id = :roomId " +
-            "and r.status = 'CONFIRMED' " +
-            "group by r.date", nativeQuery = true)
+    @Query(value = "select * from reservation_record " +
+            "where date >= :date and room_id = :roomId " +
+            "and status = 'CONFIRMED' " +
+            "group by id, date", nativeQuery = true)
     List<ReservationRecord> findAllByFutureReservedTimeGroupingByDate(@Param("date")LocalDate date, @Param("roomId")Long roomId);
 
     List<ReservationRecord> findAllByRoomId(Long roomId);
@@ -32,8 +25,6 @@ public interface ReservationRecordRepository extends JpaRepository<ReservationRe
             "where r.room.id in (select ro.id from Room ro where ro.studycafe.id = :studycafeId) " +
             "and r.review.id is not null")
     List<ReservationRecord> findAllByStudycafeId(@Param("studycafeId") Long studycafeId);
-
-    List<ReservationRecord> findAllByRoomIdAndDate(Long roomId, LocalDate date);
 
     Page<ReservationRecord> findAllByMember(Pageable pageable, Member member);
 
