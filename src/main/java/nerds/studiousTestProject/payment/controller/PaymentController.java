7173396cb898c81e4d3e744f6531336040b9020c
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-19 18:14:15 +0900

    refactor: 예약 취소와 관련된 컨트롤러 모두 payment로 이동

--- src/main/java/nerds/studiousTestProject/payment/controller/PaymentController.java
+++ src/main/java/nerds/studiousTestProject/payment/controller/PaymentController.java
@@ -8,8 +8,12 @@ import nerds.studiousTestProject.payment.dto.confirm.response.ConfirmFailRespons
 import nerds.studiousTestProject.payment.dto.confirm.response.SuccessPayResponse;
 import nerds.studiousTestProject.payment.dto.virtual.response.VirtualAccountResponse;
 import nerds.studiousTestProject.payment.service.PaymentService;
+import nerds.studiousTestProject.payment.util.totoss.CancelRequest;
 import nerds.studiousTestProject.payment.util.totoss.ConfirmSuccessRequest;
+import nerds.studiousTestProject.reservation.dto.cancel.response.ReservationCancelResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,35 +21,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/payments")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class PaymentController {
 
     private final PaymentService paymentService;
 
-    @PostMapping("/success")
+    @PostMapping("/payments/success")
     public SuccessPayResponse payConfirmSuccess(@RequestBody @Valid ConfirmSuccessRequest request) {
         return paymentService.confirmSuccess(request);
     }
 
-    @PostMapping("/fail")
+    @PostMapping("/payments/fail")
     public ConfirmFailResponse payConfirmFail(@RequestParam String code,
                                               @RequestParam String message,
                                               @RequestParam String orderId) {
         return paymentService.confirmFail(message, orderId);
     }
 
-    @PostMapping("/virtual/success")
+    @PostMapping("/paymentsvirtual/success")
     public VirtualAccountResponse confirmVirtualAccount(@RequestBody @Valid ConfirmSuccessRequest request) {
         return paymentService.virtualAccount(request);
     }
 
 
-    @PostMapping("/deposit-callback")
+    @PostMapping("/payments/deposit-callback")
     public ResponseEntity<Void> depositCallback(@RequestBody DepositCallbackRequest depositCallbackRequest) {
         paymentService.processDepositByStatus(depositCallbackRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/reservations/{reservationId}/cancellations")
+    public ReservationCancelResponse showCancel(@PathVariable Long reservationId) {
+        return paymentService.getCancel(reservationId);
+    }
+
+    @PostMapping("/reservations/{reservationId}/cancellations")
+    public ResponseEntity<Void> cancel(@PathVariable Long reservationId,
+                                       @RequestBody @Valid CancelRequest cancelRequest) {
+        paymentService.userCancel(cancelRequest, reservationId);
+        return ResponseEntity.noContent().build();
     }
 
 }
