71b96d4c42b3241ec0fe59dccd9c93b39a9c2e6d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-19 18:17:14 +0900

    faet: 해당 일자의 취소 수수료를 반환하는 기능

--- src/main/java/nerds/studiousTestProject/refundpolicy/repository/RefundPolicyRepository.java
+++ src/main/java/nerds/studiousTestProject/refundpolicy/repository/RefundPolicyRepository.java
@@ -1,11 +1,16 @@
 package nerds.studiousTestProject.refundpolicy.repository;
 
 import nerds.studiousTestProject.refundpolicy.entity.RefundPolicy;
+import nerds.studiousTestProject.refundpolicy.entity.Remaining;
 import nerds.studiousTestProject.studycafe.entity.Studycafe;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface RefundPolicyRepository extends JpaRepository<RefundPolicy, Long> {
     List<RefundPolicy> findAllByStudycafe(Studycafe studycafe);
+
+    @Query("select r.rate from RefundPolicy r where r.type = 'STUDYCAFE' and r.studycafe = :studycafe and r.remaining = :remaining")
+    int findStudycafeRefundRateOnDay(@Param("studycafe") Studycafe studycafe, @Param("remaining") Remaining remaining);
 }
